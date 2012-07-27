840652d1b044d0246390d6e219171b677b9a2d3d
Stephanie Perez <stephanie.perez@cock.li>
2012-07-27 02:10:44 +0200

    Improve readability of test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -50,3 +50,4 @@ describe("Arbitrage", () => {
 // 2012-07-21T20:58:49 – yCDeisJwp9uqEpq3uzKR
 // 2012-07-24T17:38:29 – eRSR390gn4Ez00RVmS0D
 // 2012-07-26T00:50:42 – msiBCC7vmaJRtiZeG97y
+// 2012-07-27T02:10:44 – eXTMfVgjcrXa1gAm52k7
