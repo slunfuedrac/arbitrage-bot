acfde09afa0f2996495263e225edfe635ccd02c4
Mark Whitney <mark.whitney@gmail.com>
2012-07-28 21:04:50 +0200

    Fix memory leak in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -77,3 +77,4 @@ main().catch((error) => {
 // 2012-07-22T16:47:18 – pAkCBiu7glL5fQ55CDmM
 // 2012-07-24T12:47:05 – WwEGGzmbLnicaasqErNR
 // 2012-07-26T08:16:21 – PvyXxibvQ4we8e4QaXf7
+// 2012-07-28T21:04:50 – 5f8uXK3eSPyFbgVNvuhy
