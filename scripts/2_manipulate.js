cfc222e5366fe59cb12f9a0fcb71c8573bc8ab2d
Manuel Smith <manuel.smith@pm.me>
2012-07-29 17:09:56 +0200

    sync(scripts/2_manipulate.js): align with upstream changes

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -78,3 +78,4 @@ main().catch((error) => {
 // 2012-07-24T12:47:05 – WwEGGzmbLnicaasqErNR
 // 2012-07-26T08:16:21 – PvyXxibvQ4we8e4QaXf7
 // 2012-07-28T21:04:50 – 5f8uXK3eSPyFbgVNvuhy
+// 2012-07-29T17:09:56 – GSWsROBjpuJUyaXl5S7n
