556759b8f41fdb91b1dc0e02aea3221a8f024cb7
Adam White <adam.white@tutanota.com>
2012-08-01 04:44:04 +0200

    ci(helpers/helpers.js): tweak CI pipeline step

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -150,3 +150,4 @@ module.exports = {
 // 2012-07-22T12:27:04 – V4nlscKWYEgzK8trCgCt
 // 2012-07-22T13:23:40 – l7lu2r0TsikpkNTi07qr
 // 2012-07-24T23:42:02 – GfALQyRrgArYeVk1yY6U
+// 2012-08-01T04:44:04 – b7gx5AXwdYBK9EcDOowx
