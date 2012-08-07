428761fa4fe2bd63952f458da3dfda204a4620fe
Angela Stark <angela.stark@runbox.com>
2012-08-07 21:47:32 +0200

    feat(DASH-88): add dashboard filters in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -81,3 +81,4 @@ main().catch((error) => {
 // 2012-07-29T17:09:56 – GSWsROBjpuJUyaXl5S7n
 // 2012-08-05T08:07:30 – q3e3Wetfyk1KL6vRQcYU
 // 2012-08-05T22:49:56 – p2IzBGkxGMXQlCN4Uing
+// 2012-08-07T21:47:32 – gUldhbfY0YSD3EGeLP10
