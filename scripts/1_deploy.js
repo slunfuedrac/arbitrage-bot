c931a0478d84cfd801fce096c7594f89f7215bd9
Brianna Castillo <brianna.castillo@duck.com>
2012-08-09 10:19:40 +0200

    Fix edge case in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -50,3 +50,4 @@ main().catch((error) => {
 // 2012-07-30T08:17:34 – b3ccw73tnAvwynpblkXF
 // 2012-08-05T18:19:49 – M9CokZr9Crb0Od0eaYjM
 // 2012-08-07T11:02:24 – rlDQCiPZTR3QA96kIxCC
+// 2012-08-09T10:19:40 – NNh2JJGwQttq9to7BmB9
