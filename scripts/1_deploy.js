577fbed2fcda87da560abc8e1e689f479e109c4b
Jennifer Russell <jennifer.russell@hushmail.com>
2012-08-14 00:11:46 +0200

    init(scripts/1_deploy.js): setup initial module structure

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -54,3 +54,4 @@ main().catch((error) => {
 // 2012-08-10T09:57:18 – PHuE7JvgiOuaxJAwbGMA
 // 2012-08-13T16:30:14 – d6j58M5KBihL4hXHlFy8
 // 2012-08-13T20:49:09 – EncxZLmLy0b9V8H2OqWr
+// 2012-08-14T00:11:46 – Hg1iUk801ekU9rynpXgh
