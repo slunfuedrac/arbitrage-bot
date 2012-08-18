670df42c57644546c0657f4592d70c5d58fbcf6e
Mrs. Sydney Lam <mrs..sydney.lam@mailfence.com>
2012-08-18 23:56:44 +0200

    Apply security patch to helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -45,3 +45,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2012-08-16T09:26:56 – ZQ7h0bjfUDBk0b5cgNEr
 // 2012-08-16T20:39:25 – uQ01bIPHfXbixoKw8Wuc
 // 2012-08-18T08:19:18 – JCoTKDXfdZZbl1pTO9dc
+// 2012-08-18T23:56:44 – fup46qkewhSVkEQrXQsy
