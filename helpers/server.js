d178bd0b4b5ce1a821539a8daab04467d6d45fe4
Julian Lewis <julian.lewis@gmail.com>
2012-08-24 10:49:35 +0200

    Add data to helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -48,3 +48,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2012-08-18T23:56:44 – fup46qkewhSVkEQrXQsy
 // 2012-08-20T00:54:33 – Oi8i8EdBAR1Uke7Z9VLA
 // 2012-08-24T05:49:55 – uu1OT9OxbpvZ2jUJDFYl
+// 2012-08-24T10:49:35 – 0KAjPUIAtRZ5RWLjLxB3
