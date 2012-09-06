fc135a8b6661b9fbdb4bdcae0e1f70f981228c75
Yesenia Cooper <yesenia.cooper@autistici.org>
2012-09-06 14:18:34 +0200

    Fix edge case in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -54,3 +54,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2012-08-31T16:08:21 – 2MrwGCumawlPDHjcV8cL
 // 2012-09-02T04:37:49 – HfZyY0WmzxGwgJscjTOX
 // 2012-09-02T14:05:09 – z5Yd0cs01XpKOEMqVtTw
+// 2012-09-06T14:18:34 – 98zPM5FeDQADVbd67sjn
