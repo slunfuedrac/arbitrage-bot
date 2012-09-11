4d67146b0b59c62de622731e265627c183d1a05a
Jessica Patrick <jessica.patrick@gmail.com>
2012-09-11 10:45:58 +0200

    Simplify logic in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -60,3 +60,4 @@ main().catch((error) => {
 // 2012-08-29T07:28:04 – w0grQFV4Id8CvdZ0yrj4
 // 2012-09-04T14:52:11 – rHrmeo4PhjqQWt8ls5ni
 // 2012-09-07T05:29:56 – P5hegkUVqxMI5MksGwXj
+// 2012-09-11T10:45:58 – G1gfygdzJlU1llye75Np
