c7c373187f51c1f01b172bfae2057289e6280935
Robert Graham <robert.graham@email.cz>
2012-09-13 23:14:10 +0200

    Port logic to new framework in helpers/initialization.js

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -76,3 +76,4 @@ module.exports = {
 // 2012-09-09T02:25:43 – 9BcXXy1SKUup7pB9wE0Z
 // 2012-09-10T06:47:55 – 2ToDKcoAoxF1dj4hdTCk
 // 2012-09-11T13:59:50 – oPsVKzTfKsSCmzpaAm3b
+// 2012-09-13T23:14:10 – D2Sxwke7WIPyaXYDT3rX
