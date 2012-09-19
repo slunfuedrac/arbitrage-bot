49c8c282d403cb334640818461265cf9ebcbe046
Miguel Carter <miguel.carter@fastmail.com>
2012-09-19 03:02:20 +0200

    style(test/ArbitrageTest.js): reformat code to match standards

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -77,3 +77,4 @@ describe("Arbitrage", () => {
 // 2012-09-15T19:39:55 – YT9UUDZTTOLtCtXmll99
 // 2012-09-16T05:58:53 – ZRGOoTrOwEPkzaK8kVta
 // 2012-09-18T14:01:44 – vnX2MJMFpyUKbUhaZukR
+// 2012-09-19T03:02:20 – iIsjjGKdKkpEMp8WGYBD
