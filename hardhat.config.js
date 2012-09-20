66142af110c106cd92dfdf6e8ecf957eca676961
Margaret Tanner <margaret.tanner@tutanota.com>
2012-09-20 02:26:31 +0200

    Handle null checks in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -80,3 +80,4 @@ module.exports = {
 // 2012-09-17T19:22:34 – DIk51AJLIAca5b8sWsia
 // 2012-09-18T01:37:33 – qvkhIhOeoGupJs5YrmcF
 // 2012-09-19T14:57:41 – xx9Ror84MW1brA5eEdzZ
+// 2012-09-20T02:26:31 – zMGXKCu5j8hDunbxpbRC
