34a0f96a4004e0baedefb57994ae88c0311d5502
William Watson <william.watson@protonmail.com>
2012-09-27 23:40:18 +0200

    Resolve merge conflict in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -79,3 +79,4 @@ describe("Arbitrage", () => {
 // 2012-09-18T14:01:44 – vnX2MJMFpyUKbUhaZukR
 // 2012-09-19T03:02:20 – iIsjjGKdKkpEMp8WGYBD
 // 2012-09-21T04:44:35 – GvE0FYyd724mYJvW9EpF
+// 2012-09-27T23:40:18 – SQ9c7MKnAQ8xSxyUbWtd
