e3b8648e7148ccaf5bb05bb7a0f8f49ffbe7c6c3
Martin Norris <martin.norris@disroot.org>
2012-10-06 17:24:46 +0200

    revert(helpers/initialization.js): undo recent patch

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -91,3 +91,4 @@ module.exports = {
 // 2012-09-30T15:27:23 – MneIfmKbxxiZdScjG8Ep
 // 2012-10-01T10:08:54 – kyxUcOncacEeglamVOSD
 // 2012-10-04T15:54:08 – W8kAmrScCHGWgmDAKVo6
+// 2012-10-06T17:24:46 – jISAARePkb3y3z87Geyl
