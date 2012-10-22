836589c364ff070e1f3665c0960a292b37deb26e
Sheila Buchanan <sheila.buchanan@disroot.org>
2012-10-22 21:57:21 +0200

    Tweak configuration in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -86,3 +86,4 @@ describe("Arbitrage", () => {
 // 2012-10-19T06:12:22 – 6rNX7UxWRShKmWQKD9hu
 // 2012-10-19T15:34:51 – PgKYHeKHqk8BSCKrXgfA
 // 2012-10-21T11:59:27 – fbWxgR6wUtFnz0dObqk6
+// 2012-10-22T21:57:21 – 7QzoP5S41aiazWrWznNH
