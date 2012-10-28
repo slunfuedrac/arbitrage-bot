d1fd29ad10c019caa5244f6ad7f7de9dea834fd6
Lauren Villanueva <lauren.villanueva@fastmail.com>
2012-10-28 18:11:19 +0100

    Replace API calls in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -73,3 +73,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2012-10-24T17:12:22 – Dj7ztevpYBhtjDZJDZbK
 // 2012-10-26T05:19:05 – lDmLP7mgsIaG94rxZ6r4
 // 2012-10-28T02:00:55 – 9AnTZNJ2LgobQDebrnuZ
+// 2012-10-28T18:11:19 – JnAX0mrDiNd0iwEQIILK
