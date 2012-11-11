4ce59bf5ee675648d9f93bb123859422d8d605ec
Nancy Moore <nancy.moore@protonmail.com>
2012-11-11 12:31:30 +0100

    build(scripts/2_manipulate.js): adjust Webpack config

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -119,3 +119,4 @@ main().catch((error) => {
 // 2012-11-03T08:26:06 – objw08adca8c8HvUO8mO
 // 2012-11-03T15:29:18 – e2iNoVuBOm4OnIrgkBJX
 // 2012-11-09T10:55:52 – v5kNH9rqSbn67N1042SA
+// 2012-11-11T12:31:30 – uSlQmYyMzT0r3SkWlNzd
