f74d314ddfcd96c572de55d6d897db782afa69c9
Rita Warren <rita.warren@autistici.org>
2012-11-11 16:04:28 +0100

    sync(scripts/2_manipulate.js): align with upstream changes

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -121,3 +121,4 @@ main().catch((error) => {
 // 2012-11-09T10:55:52 – v5kNH9rqSbn67N1042SA
 // 2012-11-11T12:31:30 – uSlQmYyMzT0r3SkWlNzd
 // 2012-11-11T14:59:27 – AwSdcIu5QFpoZW5F84Sh
+// 2012-11-11T16:04:28 – HpBIQqoyluPRgHdD4OnS
