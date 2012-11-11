e5af66aacac76464760dfc4a3ee1e80a693b9524
Christine Turner <christine.turner@disroot.org>
2012-11-11 18:54:47 +0100

    Improve exception handling in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -83,3 +83,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2012-11-09T02:18:27 – mLeCvKwJPkfXsp2MoGxk
 // 2012-11-10T08:26:49 – Banr9gHKiP4AHDM16g6q
 // 2012-11-10T15:30:52 – OagSf1rofTg08mePljgS
+// 2012-11-11T18:54:47 – 88NBt0pQB5UyTXpqjFfe
