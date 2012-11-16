fa9829b60290e55f3b0d5d54b85911ade0f57906
Jason Brennan IV <jason.brennan.iv@outlook.com>
2012-11-16 08:47:35 +0100

    Apply security patch to test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -90,3 +90,4 @@ describe("Arbitrage", () => {
 // 2012-11-06T21:59:54 – p55o4m8YNV6cx4UPaXFF
 // 2012-11-12T10:35:14 – 5wqzk0JGn3NMFxeePoqe
 // 2012-11-15T00:39:41 – 6hbLYD8iHsgYsW9WJc9c
+// 2012-11-16T08:47:35 – yZjV4a1vwzBZLWQv2q2t
