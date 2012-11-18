7ee65ba659d9ed8019d9ee2c3ec125ddca7c6798
Jeremy Wilkins <jeremy.wilkins@icloud.com>
2012-11-18 03:24:01 +0100

    Fix typo in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -80,3 +80,4 @@ main().catch((error) => {
 // 2012-11-10T22:16:46 – JiyT2o3ELh6ivq99DIty
 // 2012-11-15T04:44:43 – hvig2bIjEEyxqJHJI9R6
 // 2012-11-16T04:19:47 – tUCvaxmTZhZn5smezUge
+// 2012-11-18T03:24:01 – bT2di8qDW8YjCDv7fT3y
