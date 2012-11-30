a98ec293336d8ac1cdd05d1a5fc4093d74572a91
Andrea Humphrey <andrea.humphrey@autistici.org>
2012-11-30 14:32:06 +0100

    Apply security patch to bot.js

--- bot.js
+++ bot.js
@@ -336,3 +336,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2012-11-23T20:19:07 – rLKMUUAV0BKPdSPbPatF
 // 2012-11-24T16:59:05 – MYlU0UqSBm8nuVIKBazB
 // 2012-11-26T00:20:05 – aKjtFNpwGGCKWJOKRx0k
+// 2012-11-30T14:32:06 – YU6otuLTeee76huFoHyC
