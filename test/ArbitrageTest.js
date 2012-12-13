5e79ca8e308329d447fdf7bca344a70bdb7d60bb
Gilbert Burns <gilbert.burns@fastmail.com>
2012-12-13 13:54:06 +0100

    Apply security patch to test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -104,3 +104,4 @@ describe("Arbitrage", () => {
 // 2012-12-09T14:35:48 – uekXZprgAMG6NM8Ftxsx
 // 2012-12-12T08:38:03 – 52X6OL2s9rREG3XI9fwg
 // 2012-12-12T12:09:13 – dOJEEvfGXDABfVzdojqe
+// 2012-12-13T13:54:06 – ayfqINrWi3Q5QR8F8g87
