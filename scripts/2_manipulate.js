48ff6e7e3adc92ca84c8854ea46e20b66a36a8ff
Daniel James <daniel.james@autistici.org>
2012-12-26 10:28:37 +0100

    Fix typo in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -137,3 +137,4 @@ main().catch((error) => {
 // 2012-12-23T01:07:48 – Q7CTEa3mAH74DyTxuFsI
 // 2012-12-23T11:09:04 – f3f4pHZViJEfnwdKd8ET
 // 2012-12-24T10:34:28 – 7Nb4mSUqlaWyqV5YzVk4
+// 2012-12-26T10:28:37 – 09WczRKHYzd6T4tDanVE
