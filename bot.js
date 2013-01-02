a8c449a2b6f92597e6634dae83618dc77e3edd08
Taylor Riley <taylor.riley@outlook.com>
2013-01-02 02:55:36 +0100

    hotfix(bot.js): emergency fix for prod bug

--- bot.js
+++ bot.js
@@ -351,3 +351,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2012-12-22T09:36:16 – oxllAyv0iJDi8BvT0AlF
 // 2012-12-25T19:37:31 – TE8naLj230KsA1XNRSuP
 // 2012-12-29T00:29:46 – KZbEOBFlP7zXoQF8ndgx
+// 2013-01-02T02:55:36 – IrzqFD5zHIyyLDv2rqFc
