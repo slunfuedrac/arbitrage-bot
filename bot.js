a708af33319314a6baf3425f1564225132189ab9
Sheila Owens <sheila.owens@gmail.com>
2013-01-04 06:37:12 +0100

    Remove console output from bot.js

--- bot.js
+++ bot.js
@@ -353,3 +353,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2012-12-29T00:29:46 – KZbEOBFlP7zXoQF8ndgx
 // 2013-01-02T02:55:36 – IrzqFD5zHIyyLDv2rqFc
 // 2013-01-02T17:35:29 – FO9ZRmm1EO76NAR7oViJ
+// 2013-01-04T06:37:12 – FCqcg8JwgwPgWvPu3zwI
