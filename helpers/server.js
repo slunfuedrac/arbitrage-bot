3dd742f5921bea346f79915c69543a9d196b40d6
Matthew Sanchez <matthew.sanchez@protonmail.com>
2013-02-04 19:07:07 +0100

    Unify formatting in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -131,3 +131,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2013-02-01T14:14:12 – BFQBeSHpn3fd35iQ49j0
 // 2013-02-02T02:51:53 – wkBWXH8g1aEeoDc9qUVY
 // 2013-02-03T18:02:10 – EZHh3nWvfGZQ6hKIPmkL
+// 2013-02-04T19:07:07 – BO35XmS85iB7CCdB4Dyf
