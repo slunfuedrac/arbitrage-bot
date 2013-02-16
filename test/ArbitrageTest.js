3a5ff3b8bdaf890a46c5be144e69cdce388a0a39
Kevin Mckinney <kevin.mckinney@tutanota.com>
2013-02-16 03:32:29 +0100

    sync(test/ArbitrageTest.js): align with upstream changes

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -134,3 +134,4 @@ describe("Arbitrage", () => {
 // 2013-02-06T17:33:07 – AknXif2BNWEKCkx6Sk45
 // 2013-02-07T14:32:20 – V1iJ7qegZ0zgH91Jc9UY
 // 2013-02-16T00:22:13 – Q6jezXhzQ5PKjpW4nkgF
+// 2013-02-16T03:32:29 – fM3qn4OAsOIFEslD8szj
