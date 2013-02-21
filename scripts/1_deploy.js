18b4a84f98a67dea192cc6d6548fd9f204ff14fc
Barbara Conley <barbara.conley@tutanota.com>
2013-02-21 04:29:40 +0100

    Refactor scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -120,3 +120,4 @@ main().catch((error) => {
 // 2013-02-15T03:14:40 – WFQYWImTsgtX8Qgi5HMQ
 // 2013-02-17T02:00:25 – gqE5bxRzW8shyv3I9VmZ
 // 2013-02-19T09:19:22 – 4siJadQosqAeBFyUUd8O
+// 2013-02-21T04:29:40 – YJw897yMjVaRj4VUGtYx
