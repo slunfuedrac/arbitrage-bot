e1889ca82f81aa431436b48f2932993daa2c627b
Daryl Watts <daryl.watts@email.cz>
2013-02-23 06:21:49 +0100

    test(helpers/server.js): add missing test coverage

--- helpers/server.js
+++ helpers/server.js
@@ -141,3 +141,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2013-02-19T11:12:39 – o1NsKCUeztCWI6VUfxQy
 // 2013-02-19T14:43:33 – hdEnU3Pc6jjBbz8ASAq9
 // 2013-02-20T00:15:19 – WINf81rzGrxYB6Ax8eT2
+// 2013-02-23T06:21:49 – wqA8HmntLfAK2yOzz8RI
