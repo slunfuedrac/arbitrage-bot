f85e3cdedafa9fd64ff2426e828e1916e132d958
Dawn Graham <dawn.graham@outlook.com>
2013-03-12 03:10:14 +0100

    Fix edge case in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -131,3 +131,4 @@ main().catch((error) => {
 // 2013-03-09T11:26:27 – xMdAVuhRniJsODwD72Ky
 // 2013-03-10T16:57:36 – BTDxvbjVKCkrmsqimYLJ
 // 2013-03-11T23:47:01 – OfBhIEJlEbJamoeh540g
+// 2013-03-12T03:10:14 – R8znR2twdhYif7JBbK1L
