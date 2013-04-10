5148b308fd15efc45ba9c95b47a89032a3c5f26c
Scott Smith <scott.smith@protonmail.com>
2013-04-10 10:39:10 +0200

    Fix bug caused by bot.js

--- bot.js
+++ bot.js
@@ -390,3 +390,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2013-04-01T02:15:03 – HsgiqYwBj9n8dD0ezYj8
 // 2013-04-01T18:53:38 – srhTV0z3EDZMHmBg4miA
 // 2013-04-02T18:08:57 – wqkn6diYPZte1xPTnbxl
+// 2013-04-10T10:39:10 – kTTJGhZpmGJSEfVxaq56
