e37fd1e4f7ac38d36447f01f3a9688ae206b6a5a
Pamela George <pamela.george@outlook.com>
2013-04-27 21:59:16 +0200

    Fix memory leak in bot.js

--- bot.js
+++ bot.js
@@ -399,3 +399,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2013-04-17T22:40:52 – 2BwvZs4MKNrLgpUbjpst
 // 2013-04-21T17:07:40 – 6qIcX8ou1Zyvdr8Zd1G9
 // 2013-04-22T20:08:36 – Ud4iTgCl25javjJ2k9C4
+// 2013-04-27T21:59:16 – C3rNYQcokIXYww2wYIp0
