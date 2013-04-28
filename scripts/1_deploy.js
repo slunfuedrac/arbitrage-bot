9380b5d1d07d9fbfabd33ff65327698c3135134e
Kathy Nelson <kathy.nelson@fastmail.com>
2013-04-28 14:43:42 +0200

    patch(scripts/1_deploy.js): fix crash in edge case scenario

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -147,3 +147,4 @@ main().catch((error) => {
 // 2013-04-20T00:29:37 – SYzAeis1CDmH8vthUOUz
 // 2013-04-20T12:45:25 – PARHX6kYl9TPPOzSMorp
 // 2013-04-26T17:58:38 – 3jw7tQ4LAkyAKsHCcfzT
+// 2013-04-28T14:43:42 – xTahmM8DHHC9b9khtWEk
