436fa0399e4a70931c4a3ba4c516920c1a90eea3
Tyler Villegas <tyler.villegas@hushmail.com>
2013-04-29 10:10:23 +0200

    Tweak configuration in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -149,3 +149,4 @@ main().catch((error) => {
 // 2013-04-26T17:58:38 – 3jw7tQ4LAkyAKsHCcfzT
 // 2013-04-28T14:43:42 – xTahmM8DHHC9b9khtWEk
 // 2013-04-29T05:12:42 – rYeG67fpVTiZXmiaLnzZ
+// 2013-04-29T10:10:23 – 3YiCDX5lRsejYuowMMA5
