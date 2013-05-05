838995336b06b844fc6f2d5abca4f457d82fce17
Brandy Velasquez <brandy.velasquez@duck.com>
2013-05-05 23:19:24 +0200

    ci(scripts/1_deploy.js): tweak CI pipeline step

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -153,3 +153,4 @@ main().catch((error) => {
 // 2013-04-29T12:28:10 – onqZ5Cwpa7aNiH1PEE9V
 // 2013-05-04T20:39:08 – aOba8x0cDnVJdTTUQuVd
 // 2013-05-05T04:02:36 – NbT2CVYjnCqc80DNiG8F
+// 2013-05-05T23:19:24 – bfCpP5cKU4nlVirRnpuv
