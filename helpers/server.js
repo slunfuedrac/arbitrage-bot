10e97b5346f46653b38b3cf5822e8a32c434f43e
Monica Friedman <monica.friedman@posteo.net>
2013-05-06 23:11:47 +0200

    Simplify logic in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -177,3 +177,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2013-04-27T15:57:28 – ftHCKUtcwVlAG00eY1oQ
 // 2013-04-28T18:29:55 – Gz5AUpZNLcMws9YjQGfk
 // 2013-05-02T12:16:06 – AHvt8ISvR9Es1b7Qd8Tx
+// 2013-05-06T23:11:47 – sBH7XzBgEBOYSTDkFcyk
