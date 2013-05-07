505168d690b4025b1eb6e1cf7ebc295203d49cd7
Richard Williams <richard.williams@autistici.org>
2013-05-07 04:55:23 +0200

    Document edge behavior in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -188,3 +188,4 @@ main().catch((error) => {
 // 2013-05-03T10:28:26 – o8igTkHvBWEN6cazETm6
 // 2013-05-05T10:39:40 – 4aVfcOXVtuQsrzXyEyah
 // 2013-05-06T07:30:45 – 0mDRK2sKXYNbGc0s3N67
+// 2013-05-07T04:55:23 – dtJj1xjpZWgtXXF83vYZ
