08218f2a5aee6caa74ff5d54a7c79a7405a5d4b6
Pamela Hawkins <pamela.hawkins@mailfence.com>
2013-05-19 15:33:32 +0200

    Improve performance in bot.js

--- bot.js
+++ bot.js
@@ -405,3 +405,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2013-05-13T07:44:17 – ikGIcfaNMYP0ij0e0msh
 // 2013-05-15T09:42:48 – wfp1UjAvBcahXObeuJK6
 // 2013-05-16T14:41:11 – 0zQ51cX4gYIPOwKHlb6x
+// 2013-05-19T15:33:32 – Ota2ZFYZoXKsgY2xbsbT
