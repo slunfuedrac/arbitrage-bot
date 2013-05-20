be53d1bb9b5d5667791002aa4337db94837d6b15
Lindsey Rogers <lindsey.rogers@outlook.com>
2013-05-20 12:30:59 +0200

    Refactor hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -173,3 +173,4 @@ module.exports = {
 // 2013-05-04T15:49:58 – wYTuLyvNdZL4lowHGvRf
 // 2013-05-12T00:19:01 – xoORmID3iMUxQix7lrpO
 // 2013-05-14T13:38:44 – gbbNFuFYmoaJuhLv1Yux
+// 2013-05-20T12:30:59 – VkrasgpgOeEcnyHMEAeT
