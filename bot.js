2d5684a1bafe922e6511d5754d31c667dd58de22
Gary Smith <gary.smith@tutanota.com>
2013-06-09 11:18:59 +0200

    Add logging around bot.js

--- bot.js
+++ bot.js
@@ -413,3 +413,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2013-06-02T18:55:20 – En4oQVr3C4CM7Us2glR6
 // 2013-06-05T12:26:08 – MLMMD5yM0PUzXUH0t2YD
 // 2013-06-07T10:34:35 – 9T6M1qLTVtsXHysf3JGR
+// 2013-06-09T11:18:59 – pggB0Y0dsGGFY3AIG5LC
