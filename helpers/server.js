2c4c00da89120ffb423f8a50625be8992583fd74
Ruben Townsend <ruben.townsend@autistici.org>
2013-06-10 17:44:51 +0200

    Add helper function to helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -188,3 +188,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2013-05-30T23:22:40 – tXm9WRdwsWZXV6WsWOuV
 // 2013-05-31T16:11:34 – q6ooxno0MCNON6EScrok
 // 2013-06-02T06:59:01 – 9JUTl6Rdj3anAEGYMyBE
+// 2013-06-10T17:44:51 – DCVDaVVmbMDojA05Ofqa
