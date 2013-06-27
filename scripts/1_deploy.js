00d996ff4be27fa6ceebd5328a99636f8f4f8c35
Richard Bryant <richard.bryant@pm.me>
2013-06-27 05:40:54 +0200

    BREAKING CHANGE: modify schema in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -171,3 +171,4 @@ main().catch((error) => {
 // 2013-06-06T03:42:40 – iwJMZDPSxQ2H7yFMRoAz
 // 2013-06-06T17:30:36 – C3emgdiGunB7Ls8iFFsJ
 // 2013-06-18T22:12:30 – dAOMT9S3I3c4qgSLzJQ6
+// 2013-06-27T05:40:54 – hpZzJqjlDCq02WEYcwgK
