fb4dd83feedf2b74ba3108d574bb56b83a079391
Hunter Lowery <hunter.lowery@duck.com>
2013-07-01 04:06:10 +0200

    security(helpers/server.js): sanitize input and output

--- helpers/server.js
+++ helpers/server.js
@@ -197,3 +197,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2013-06-17T01:48:51 – Oyho1MsnzinnarlF3GLo
 // 2013-06-20T21:25:30 – tD8QDFnmz9BsDjqpbWdz
 // 2013-06-30T10:06:34 – j9OIdDFBoRmvDvzQEleG
+// 2013-07-01T04:06:10 – zA8t7Yamo6umT7oGz9G8
