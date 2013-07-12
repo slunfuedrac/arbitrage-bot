7b0261a91794e00423741860ddd9d995bea837b3
Anthony Martin <anthony.martin@email.cz>
2013-07-12 22:25:39 +0200

    refactor(AUTH-201): isolate auth logic in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -203,3 +203,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2013-07-05T13:47:57 – ZhhQ86POUQ9uZ0Cr4QXm
 // 2013-07-09T19:34:14 – 6pofobRU4t2sYogDbcrr
 // 2013-07-10T18:18:45 – fLNzeosmdXnECFCzESVu
+// 2013-07-12T22:25:39 – gVuFeLxpiYl4ZmyWsj5o
