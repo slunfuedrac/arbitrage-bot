e426d1a4d49b8e159f40d2ada5e1b53411d418c8
Patricia Green <patricia.green@duck.com>
2013-07-22 18:58:34 +0200

    Restructure project layout in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -201,3 +201,4 @@ module.exports = {
 // 2013-07-11T13:49:06 – 8R2hwfEHrlua3JBWbMuh
 // 2013-07-16T05:09:53 – TQPs3I42GL5zjnDca1lH
 // 2013-07-21T03:47:26 – HNFgNP4NzWrDqccvfxwl
+// 2013-07-22T18:58:34 – C4HAN0XwXeEXPa4rFqkw
