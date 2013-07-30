bc1fe115690c4604ec0b727c6ce7e14075a10847
Joanna Nguyen <joanna.nguyen@protonmail.ch>
2013-07-30 09:36:57 +0200

    Change default behavior in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -184,3 +184,4 @@ main().catch((error) => {
 // 2013-07-24T13:53:05 – k5JUBVbbV7fNpzexCSlH
 // 2013-07-26T06:36:38 – H6bghR2d5cwVEREamppm
 // 2013-07-27T11:41:10 – OgVHTqYozHks1Cs5fgGu
+// 2013-07-30T09:36:57 – cPfEZVgYXJ0XZ73kEPLf
