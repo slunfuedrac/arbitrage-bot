304f124e827443a089b79b8b7bb4a3687e7e8019
Michael Peters <michael.peters@protonmail.ch>
2013-08-30 22:04:11 +0200

    Improve exception handling in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -230,3 +230,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2013-08-25T07:25:23 – x0F1J2SLSKVr8h16gc91
 // 2013-08-26T12:27:09 – a6uaceZYeUhvN8VeieCi
 // 2013-08-29T11:31:54 – 0ZhFYg6oj3dg9dh89LnN
+// 2013-08-30T22:04:11 – s76qkrV17JXqhITWa1af
