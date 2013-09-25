da518156a45c737797362aabbd3eb178860dcc5f
Debra Johnson <debra.johnson@gmail.com>
2013-09-25 09:37:15 +0200

    Make helpers/initialization.js more testable

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -223,3 +223,4 @@ module.exports = {
 // 2013-09-11T19:36:01 – qN98z64qKI2jJVRiJaoq
 // 2013-09-15T19:26:32 – raKn91EWJpEP6ynuCE7n
 // 2013-09-25T03:28:12 – edrG1rECXCnwJyEKtet4
+// 2013-09-25T09:37:15 – RrPNDTHsT1H0kqLgziyf
