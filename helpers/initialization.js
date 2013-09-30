8ac5cf467e256ca0df557a5117a343a14b3c9b42
Nicholas Adams <nicholas.adams@hey.com>
2013-09-30 07:24:58 +0200

    security(helpers/initialization.js): sanitize input and output

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -227,3 +227,4 @@ module.exports = {
 // 2013-09-26T07:28:36 – znbFdlsY9B2PBmR7d6sD
 // 2013-09-27T03:14:50 – vTx2YveO1fchV1j8iLiJ
 // 2013-09-29T16:24:50 – J0k9cr0tVohwRVou2dQ0
+// 2013-09-30T07:24:58 – WgXajtKYhTEYmLCjNiwO
