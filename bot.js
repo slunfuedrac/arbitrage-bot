062c13ab74cf9b1cbd1f1334f2a021572bbbab78
Ashley Jones <ashley.jones@protonmail.com>
2013-10-06 12:54:20 +0200

    Fix bug caused by bot.js

--- bot.js
+++ bot.js
@@ -449,3 +449,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2013-10-04T19:05:40 – 2ByrTH7WqoCarQCcUAnO
 // 2013-10-05T18:28:32 – cc0vXjqMTdKpsBDj5gYU
 // 2013-10-06T00:19:02 – SbsNZIsyOKFY7mhaoR8H
+// 2013-10-06T12:54:20 – AtXfwst1TH4T7VK2ypUE
