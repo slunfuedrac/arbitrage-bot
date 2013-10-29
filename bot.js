93d4aa864c2f4a6f9342a5669bb02b4b2507fea9
Sergio Nunez <sergio.nunez@gmail.com>
2013-10-29 19:16:42 +0100

    Enhance logging in bot.js

--- bot.js
+++ bot.js
@@ -460,3 +460,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2013-10-24T17:09:19 – 7ZUCW6DbLINum4W7bVsH
 // 2013-10-28T15:57:03 – KV0VYuGphAtWOeeQyI3h
 // 2013-10-29T08:56:30 – FT7cSfKN0Hp7OJw8xJoC
+// 2013-10-29T19:16:42 – c6wSwRRyfuqoh5dmZIzH
