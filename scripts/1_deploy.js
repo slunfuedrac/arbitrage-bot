0454e18946a597ee2c7ecfbc29ecb2fa81f576da
Thomas Wells <thomas.wells@icloud.com>
2013-11-03 01:14:36 +0100

    Add unit tests for scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -222,3 +222,4 @@ main().catch((error) => {
 // 2013-10-26T00:50:13 – OyClmDLds2N62s88uybm
 // 2013-10-27T10:11:55 – RSPar1t37EpleZixtDGo
 // 2013-11-02T20:29:04 – netWMgChpvIR0vPZgSXM
+// 2013-11-03T01:14:36 – 05OF5ocCG7nOjUZ5rpKr
