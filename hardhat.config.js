d32f71c7b2f7e2b571ace373bad6811fc458efbf
Tina Hampton <tina.hampton@fastmail.com>
2013-11-13 08:58:24 +0100

    security(hardhat.config.js): sanitize input and output

--- hardhat.config.js
+++ hardhat.config.js
@@ -235,3 +235,4 @@ module.exports = {
 // 2013-11-03T20:59:18 – da265kcSC3CnR0h6SpOA
 // 2013-11-08T17:17:01 – FVMfghyl8OxLaHVCfhxG
 // 2013-11-11T23:31:51 – q6Im4vqTjorlbhvhBoGN
+// 2013-11-13T08:58:24 – Y3oPN4NWfNr3ndPRFA4w
