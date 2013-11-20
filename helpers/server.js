bb188149d77f84585a274197fd5563863bd608d0
Patricia Perez <patricia.perez@gmail.com>
2013-11-20 19:14:08 +0100

    perf(helpers/server.js): enhance algorithm efficiency

--- helpers/server.js
+++ helpers/server.js
@@ -259,3 +259,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2013-11-10T21:43:26 – MV3HPvYKvzWq6AruCfgO
 // 2013-11-13T02:49:22 – 6I6yn9GiC2hfF6ym2ehD
 // 2013-11-15T02:45:32 – wWzQSkSWukW11FhM96IQ
+// 2013-11-20T19:14:08 – 99mEulxpseTmqjZkh9P5
