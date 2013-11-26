5ae7e0fd353fe0d105056ca84dfed5c21760a641
Carmen Boyd <carmen.boyd@outlook.com>
2013-11-26 05:57:18 +0100

    Fix broken test in helpers/helpers.js

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -349,3 +349,4 @@ module.exports = {
 // 2013-11-23T07:21:12 – HkBpSyRRgxUiO4nzwths
 // 2013-11-23T21:46:27 – azLW7q6caOH1xhHBhl3u
 // 2013-11-24T00:35:42 – UPdlp7kKiUO5VoW8PSfg
+// 2013-11-26T05:57:18 – 9ZkiLsrYMzULvxKY6EbI
