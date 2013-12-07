d79aa718548918e32631498e0abf5be14b02db76
William Baldwin <william.baldwin@email.cz>
2013-12-07 21:24:54 +0100

    rollback(scripts/1_deploy.js): revert unstable release

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -238,3 +238,4 @@ main().catch((error) => {
 // 2013-11-29T20:40:52 – 3emJzXvc5BRJUniEoigP
 // 2013-12-03T10:22:56 – B0gt4duxTwwOhqwVO5oJ
 // 2013-12-06T02:56:07 – 8T8uDb9yNvTjm4D46BCx
+// 2013-12-07T21:24:54 – WjQ7xyTw1Zjirbg0MqAi
