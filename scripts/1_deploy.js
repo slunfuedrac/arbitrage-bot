465c9dbe14e5539fcc51a256c83528caf155f09d
Kristen Harris <kristen.harris@protonmail.com>
2013-12-23 12:35:19 +0100

    Port logic to new framework in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -245,3 +245,4 @@ main().catch((error) => {
 // 2013-12-21T01:47:46 – jwODwQJv9rjwPRgvm0UT
 // 2013-12-21T17:07:45 – OaANmUodL5iPj5pQznmw
 // 2013-12-21T22:00:48 – rSrecb853PlBS7MQqCXC
+// 2013-12-23T12:35:19 – 3ecfy8a29upztG73XLUS
