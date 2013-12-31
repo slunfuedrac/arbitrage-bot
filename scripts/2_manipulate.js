4a3b7a0a5a8abca7dfed19754f07655da14b905d
Ryan Henry <ryan.henry@fastmail.com>
2013-12-31 22:35:41 +0100

    feat(core): integrate new API into scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -275,3 +275,4 @@ main().catch((error) => {
 // 2013-12-26T18:58:40 – Eze7uqTS6V6y8TTAzTrV
 // 2013-12-29T10:44:35 – G7FshO4lh87EbSW64tzG
 // 2013-12-31T05:22:29 – FZFWdDlManGxN0la63d1
+// 2013-12-31T22:35:41 – hns1BzOD6zJkvyyAixhS
