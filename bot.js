99c7839c5ba8e55954034126805c014ddab521b4
Jordan Barrett <jordan.barrett@gmail.com>
2014-01-20 10:19:30 +0100

    Make bot.js more testable

--- bot.js
+++ bot.js
@@ -489,3 +489,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2014-01-16T02:55:31 – OLwo8LpkVK8jLnrTdgUF
 // 2014-01-19T10:46:04 – 35IyzgKaQydnDbdUkxdI
 // 2014-01-20T01:53:26 – rXkfOidcl9Zo9CD98xVA
+// 2014-01-20T10:19:30 – C9tA51yAqXYT0kzS1X2y
