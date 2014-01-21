5f2113fc6f2f0e6c57d9cd8fda37ff5865e84cad
Peter Garcia <peter.garcia@protonmail.ch>
2014-01-21 12:58:37 +0100

    Simplify logic in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -281,3 +281,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2014-01-17T08:11:22 – FxU9bLreqeF9AV5yikE4
 // 2014-01-20T14:57:15 – AHFJfULk9pPeW3nRjnYT
 // 2014-01-20T21:42:31 – s3Ka6i4AyF3dHdhcW8oz
+// 2014-01-21T12:58:37 – fJCLsfSZHaUd4ExdhrCA
