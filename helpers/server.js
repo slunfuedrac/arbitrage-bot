18babb5f63ec2307c4ca3631421643298e86eb94
Michael Hancock <michael.hancock@protonmail.com>
2014-01-24 05:05:15 +0100

    Format helpers/server.js according to style guide

--- helpers/server.js
+++ helpers/server.js
@@ -287,3 +287,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2014-01-22T14:33:58 – qiOgecHThck34ytXf5tC
 // 2014-01-22T16:07:55 – C7jkaulDuwFtEj6wKCUy
 // 2014-01-23T04:42:53 – C4ot9g2K9Fr2BlI7pWCj
+// 2014-01-24T05:05:15 – irmnmaQlPXOJjuDJErPM
