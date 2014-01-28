bd631576ab5b2baed502f5daadb1c800f00b9e92
Ralph Walker <ralph.walker@tutanota.com>
2014-01-28 04:26:02 +0100

    Document edge behavior in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -273,3 +273,4 @@ module.exports = {
 // 2014-01-19T12:21:24 – cduoKjMiXo2fJJg9SdL5
 // 2014-01-27T07:02:21 – u7cPgsCslaXF5CWcGUbu
 // 2014-01-27T19:58:01 – qRWAVN5Adf7nYefYo5ue
+// 2014-01-28T04:26:02 – kjKZqvxdHZBUfLchoBDC
