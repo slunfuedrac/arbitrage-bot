76f6d876f905ad4a0805b718ca87bffbfca0cc41
Jessica Wright <jessica.wright@email.cz>
2014-02-01 14:43:39 +0100

    Document changes in bot.js

--- bot.js
+++ bot.js
@@ -499,3 +499,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2014-01-24T11:45:41 – uJPjZ7XzAiJgPUukc9Fc
 // 2014-01-30T15:29:55 – 1xttb6SAKzF2SOcPevgX
 // 2014-01-30T20:06:06 – mZEi1PglNINtOqIXY6Fb
+// 2014-02-01T14:43:39 – Rn3NJ85PtHltKECnyTqH
