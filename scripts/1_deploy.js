e8f8f4b856163cb48aa083d12681462a8d9a1b2e
Sheena Jones <sheena.jones@zoho.com>
2014-02-03 10:04:50 +0100

    Add unit tests for scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -258,3 +258,4 @@ main().catch((error) => {
 // 2014-01-22T09:43:14 – ZMmyWTfftZiqtJBfr2B9
 // 2014-01-29T07:14:17 – VlgtD3mrrzQHpIjEIjum
 // 2014-02-01T15:04:56 – vrTJZHhIDVx0wgyS00qX
+// 2014-02-03T10:04:50 – MxbQWrxBnfW7pR7hqYJO
