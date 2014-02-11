cb2f8e2ef40f9173c4682d30b52243c96352fcf7
Carrie Moore <carrie.moore@zoho.com>
2014-02-11 11:13:04 +0100

    Fix edge case in bot.js

--- bot.js
+++ bot.js
@@ -503,3 +503,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2014-02-09T05:25:35 – AM7HcEZcOowlkoDSgvWw
 // 2014-02-09T05:35:28 – ulRoXthOQONS7yNvuui5
 // 2014-02-10T13:23:37 – WhVZnJHgL6e7quOLSHlH
+// 2014-02-11T11:13:04 – 20K7UxTr4ndPYYU6YhCM
