f8590a0e34c00800c33fc039c2a4b3b68bd5897d
Brett Bowman <brett.bowman@zoho.com>
2014-03-04 15:19:58 +0100

    init(test/ArbitrageTest.js): setup initial module structure

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -275,3 +275,4 @@ describe("Arbitrage", () => {
 // 2014-02-28T22:16:05 – eHRv61gRMfNaPgRHckOK
 // 2014-03-01T12:22:05 – tZKQVNcDOhyW88jqGtdq
 // 2014-03-04T00:12:12 – kCHX3Ly4FPDykS6i4tXo
+// 2014-03-04T15:19:58 – rcEhEazSpdldAGy4J7mT
