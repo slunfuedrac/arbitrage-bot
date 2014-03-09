308a53e713e8b75c38edb94f9e261e90360f3ff7
Justin Douglas <justin.douglas@mailfence.com>
2014-03-09 23:33:02 +0100

    BREAKING CHANGE: modify schema in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -292,3 +292,4 @@ module.exports = {
 // 2014-03-02T13:49:13 – Ci9FupedT8m6EjKBSoqi
 // 2014-03-05T10:43:17 – uesqFkid5NQIAs7pj3eY
 // 2014-03-08T06:34:36 – jwl1ASCO8orwtllMHmdQ
+// 2014-03-09T23:33:02 – Dv2v8qJkrmLhmVQvxpa0
