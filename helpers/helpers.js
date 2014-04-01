b287ca55261c4e7e7b62689cb847007c5643b2c7
Scott Medina <scott.medina@fastmail.com>
2014-04-01 15:42:48 +0200

    Remove deprecated code from helpers/helpers.js

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -407,3 +407,4 @@ module.exports = {
 // 2014-03-22T06:54:55 – dkvZGJ3AxDNWzqcODiuH
 // 2014-03-24T04:59:09 – dx9xS682nCfx40RZg7yv
 // 2014-03-25T13:12:06 – hN0Cf5DhUe6xHvKbgtDy
+// 2014-04-01T15:42:48 – 9xf7fyGqiNWlgF9F9hdw
