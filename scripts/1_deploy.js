581c5f2e065f9e6a67aa8fce7e79daa36971095f
Donna York <donna.york@pm.me>
2014-04-20 10:23:49 +0200

    test(scripts/1_deploy.js): add missing test coverage

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -284,3 +284,4 @@ main().catch((error) => {
 // 2014-04-10T01:36:17 – d9IynHiCMJ3nIKOZq0Io
 // 2014-04-10T04:46:53 – X2hIIpLv8jSbZdArl5wo
 // 2014-04-15T06:16:31 – Llby5xLCa2GKa9bf45gI
+// 2014-04-20T10:23:49 – xTe99qmrKGLfOBn6ZyY8
