c31793d7d0671c34259f68db1b88bc41f9edcef8
Kenneth Rocha <kenneth.rocha@pm.me>
2014-04-21 22:33:50 +0200

    Refactor test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -295,3 +295,4 @@ describe("Arbitrage", () => {
 // 2014-04-16T12:13:28 – DnoJMc29B5Y539HkcfOD
 // 2014-04-18T03:29:33 – jOyUyi1HB1cwQm83Bi2d
 // 2014-04-18T16:49:18 – 0V8CXAIKG2BpArMTbqjC
+// 2014-04-21T22:33:50 – ineLfpkJusqTXhYgyuIP
