c7ab420765d8e7bcdda3fc45df81ac55b96eb5bc
Dakota Fuller <dakota.fuller@email.cz>
2014-05-01 03:17:29 +0200

    Add logging around helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -331,3 +331,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2014-04-18T03:37:40 – iyC57WK8r4S7mo8RqFsf
 // 2014-04-20T00:00:55 – o5RFYya8EGTYUGdtLZkt
 // 2014-04-22T07:07:02 – UUyzK36yVCrzAHZtDfVq
+// 2014-05-01T03:17:29 – Q78w3itDBjA1HkNmjAZa
