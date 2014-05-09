2d7af718143265ef6ec15ffcf0410bc24c2112fc
Stephen Boyer <stephen.boyer@protonmail.com>
2014-05-09 08:19:48 +0200

    Add helper function to scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -293,3 +293,4 @@ main().catch((error) => {
 // 2014-05-05T21:40:40 – 6TbhuL77V3OtZVzfcp7I
 // 2014-05-07T06:14:43 – veDlqM8qbafu77iaUDS0
 // 2014-05-08T16:30:57 – T0eX7ecf82w5OxvFZxII
+// 2014-05-09T08:19:48 – npxgbp7Ot7IAYI1RwUSN
