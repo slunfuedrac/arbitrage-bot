7b92629a134319fbf1a5410c0531d219d214bdf6
Scott Weaver <scott.weaver@pm.me>
2014-05-11 09:01:56 +0200

    Format helpers/server.js according to style guide

--- helpers/server.js
+++ helpers/server.js
@@ -336,3 +336,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2014-05-06T02:32:08 – d4Ghl3ziC8MfNAes4LFB
 // 2014-05-07T20:15:18 – tkFlCtoqdp1fEuyOQbH4
 // 2014-05-08T16:32:40 – BES8WZ7S1ao7TimOXapE
+// 2014-05-11T09:01:56 – glcqJCuQUZsLMrTUPiBS
