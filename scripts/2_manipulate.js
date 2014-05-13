5274f609d6ca9586596b1d281484bdc166bda909
Joseph Wallace <joseph.wallace@duck.com>
2014-05-13 19:01:17 +0200

    test(scripts/2_manipulate.js): add missing test coverage

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -321,3 +321,4 @@ main().catch((error) => {
 // 2014-05-10T14:45:52 – lt3w17IcKux4vsRkOJ1V
 // 2014-05-11T22:22:09 – RNC2YWJffAt84ex34TDR
 // 2014-05-12T03:31:41 – 9tCelaY6HqZjwuToEEct
+// 2014-05-13T19:01:17 – hVwpNA4U8tFN5rPM0fkx
