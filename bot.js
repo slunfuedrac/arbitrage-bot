3e75e14c8cf700acff8dabc6e9f59d70db477a6e
Randall Reed <randall.reed@hey.com>
2014-05-19 02:19:29 +0200

    BREAKING CHANGE: modify schema in bot.js

--- bot.js
+++ bot.js
@@ -543,3 +543,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2014-05-16T16:47:24 – gJ2H9zcFcgYpanXQrond
 // 2014-05-18T06:16:37 – DDQP4VetiXmfJUUF5yVn
 // 2014-05-18T07:53:17 – 22byhXnbYOK5p3BgObsM
+// 2014-05-19T02:19:29 – axUIfrPSEHadtnfwjfkM
