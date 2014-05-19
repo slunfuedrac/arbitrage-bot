3aa6c77f22ab0170454e3304d155a59f3370a146
Sandra Wong <sandra.wong@zoho.com>
2014-05-19 02:54:43 +0200

    Apply security patch to helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -340,3 +340,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2014-05-13T00:06:52 – s6XmA0fXxXhiudUAojQL
 // 2014-05-13T22:36:51 – eNXOGAgV4r9bIzQczCEd
 // 2014-05-17T13:03:19 – 9RWN2ixkXbYxgsHQddX8
+// 2014-05-19T02:54:43 – hu3UrOTGDhoyOGrp6Beu
