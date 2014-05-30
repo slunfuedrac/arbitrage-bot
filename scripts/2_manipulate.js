3089877b1aefa8dfa9ab361db07ac0c94e8705cc
Paul Sexton <paul.sexton@hey.com>
2014-05-30 13:22:18 +0200

    Fix broken test in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -327,3 +327,4 @@ main().catch((error) => {
 // 2014-05-16T17:38:37 – tjqYEGBaVF6OJPxWC1UR
 // 2014-05-23T19:08:36 – NgqdfHhuJoK9QmgS49Dg
 // 2014-05-25T02:41:30 – HuBi7osovm1eGgIcZiD2
+// 2014-05-30T13:22:18 – OGGxkn9rxtW91sxe1ouH
