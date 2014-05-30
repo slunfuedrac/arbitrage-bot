67844d6dcbb7f223245332a85b6f9ffe47e4773b
Christina Allen <christina.allen@fastmail.com>
2014-05-30 22:07:25 +0200

    Improve exception handling in bot.js

--- bot.js
+++ bot.js
@@ -550,3 +550,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2014-05-26T16:56:59 – LYOkUGW4xg7ZC5bqMhAF
 // 2014-05-27T16:42:59 – DeSLlEjwbEv6eyI0nkKa
 // 2014-05-29T22:57:56 – cdvc9hJg3WcAuNTVMaM7
+// 2014-05-30T22:07:25 – OqSWGqJivcGRml5lS8CW
