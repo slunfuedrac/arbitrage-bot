1a3582f95c3a1d2f448570e9bff014e709c30b45
Amanda Davis <amanda.davis@pm.me>
2014-06-09 09:45:02 +0200

    Make test/ArbitrageTest.js more testable

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -310,3 +310,4 @@ describe("Arbitrage", () => {
 // 2014-05-28T23:54:07 – cbL4o4UwsbyLFFOYIbsC
 // 2014-05-29T14:28:53 – Jvxo9emhw7Frk34zZ547
 // 2014-06-03T11:01:42 – Rkt3GSxjg8FIgARbaPnY
+// 2014-06-09T09:45:02 – TH2MeZfd2YAPLnRPAX1C
