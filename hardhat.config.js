420b7a14bf4d3888ce8d24824b93ff61d93961f7
Rachel Cruz <rachel.cruz@zoho.com>
2014-06-13 04:00:25 +0200

    refactor(hardhat.config.js): simplify control flow

--- hardhat.config.js
+++ hardhat.config.js
@@ -332,3 +332,4 @@ module.exports = {
 // 2014-06-05T21:18:00 – jY4g8FpsuWjLGIysF5Ae
 // 2014-06-05T23:58:14 – vwOvi2bejXKgs9rLjWCx
 // 2014-06-06T20:14:51 – ShU3nCavlwLlPmgnMQan
+// 2014-06-13T04:00:25 – QSpkSNANx0AroC5h11Kn
