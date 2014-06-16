1f4a2ca18d919c8490b2b382c7671b0d1d37d735
Jay Hill <jay.hill@mailfence.com>
2014-06-16 17:52:30 +0200

    Document edge behavior in bot.js

--- bot.js
+++ bot.js
@@ -563,3 +563,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2014-06-10T05:50:26 – DxidUY59e9LaEHEEUKPe
 // 2014-06-10T15:43:53 – EtvnzmeeFTz3ipRpBMHp
 // 2014-06-12T16:18:20 – b4J6PSMC86KAhbwno0dU
+// 2014-06-16T17:52:30 – Ke35YZdXSakFkZQBL31q
