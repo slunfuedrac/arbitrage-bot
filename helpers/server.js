2f232289c1f65be5f379d60bbe9a0c34f7e96ccf
Amber Wilson <amber.wilson@hey.com>
2014-06-22 13:29:48 +0200

    refactor(AUTH-201): isolate auth logic in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -355,3 +355,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2014-06-16T17:05:55 – Fjwx42WoynZgrP463rA9
 // 2014-06-16T23:15:29 – vxJBqd6PvvQRP9fcKCYG
 // 2014-06-18T15:32:25 – OZnFkHr0SjdoaeWY4r1D
+// 2014-06-22T13:29:48 – lz4zd3hJHCLgnxCLyvSu
