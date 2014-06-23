4959aec9b6b21b06253f460c7b614c3b01792b5b
Tom Martinez <tom.martinez@zoho.com>
2014-06-23 08:59:49 +0200

    Tweak configuration in bot.js

--- bot.js
+++ bot.js
@@ -565,3 +565,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2014-06-12T16:18:20 – b4J6PSMC86KAhbwno0dU
 // 2014-06-16T17:52:30 – Ke35YZdXSakFkZQBL31q
 // 2014-06-22T22:17:06 – W2tFuSICp6VZVzpTR7lf
+// 2014-06-23T08:59:49 – 1dlExAH8kgo8T184mKFa
