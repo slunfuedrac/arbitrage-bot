d6e79274a593f44fa11db41bc03d57425bc990b8
Teresa Jones <teresa.jones@protonmail.com>
2014-06-29 17:09:05 +0200

    Downgrade dependency used in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -319,3 +319,4 @@ describe("Arbitrage", () => {
 // 2014-06-17T01:16:18 – jrejhOW3qIryyN2QXdiy
 // 2014-06-18T02:03:22 – iyuj48VtCt16uC6kEVk5
 // 2014-06-21T17:46:19 – 9kfrnz3dXyk0njvqAh5T
+// 2014-06-29T17:09:05 – 7HbG9iHrqNxabGLyGezA
