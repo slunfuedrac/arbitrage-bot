7fcc1fe310964355e1a2d5aa5c694b7f742c35c9
Laura Smith <laura.smith@icloud.com>
2014-07-05 18:35:17 +0200

    Fix bug caused by scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -338,3 +338,4 @@ main().catch((error) => {
 // 2014-06-28T16:07:53 – tGCo1sWmpIh3B0koHhBr
 // 2014-06-28T16:18:40 – S7J4WsGgJJgrTVPejHaE
 // 2014-07-04T08:04:07 – GKy6uA7UUunUg0BKmYNn
+// 2014-07-05T18:35:17 – KtOIXRcwU7WB7g9vuF9j
