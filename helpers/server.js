85da70186eff881cd8e8ab32de76c6fab9d6c7dc
Kenneth Reed <kenneth.reed@autistici.org>
2014-07-14 00:18:55 +0200

    Replace hardcoded values in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -364,3 +364,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2014-07-01T22:03:15 – 8xOimQAnz6QZmb8lPxgu
 // 2014-07-06T01:59:49 – AWWkGbZ5qzVr6ZH48tus
 // 2014-07-07T00:34:37 – vJPwls29TZ1xocWOopO9
+// 2014-07-14T00:18:55 – NT1sFGaGUN2pHn2ANmLO
