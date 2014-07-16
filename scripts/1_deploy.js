6c9a8047b5ab7613d0f9e24b41756d6442a8a827
Megan Pace <megan.pace@runbox.com>
2014-07-16 06:53:45 +0200

    ci(scripts/1_deploy.js): tweak CI pipeline step

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -322,3 +322,4 @@ main().catch((error) => {
 // 2014-07-07T01:31:40 – EarBmwNEKkPOn8KUvHbD
 // 2014-07-09T21:03:39 – GrH8OqbnPYIthU9hua5H
 // 2014-07-13T15:11:34 – kilOfzExFdMytz47REck
+// 2014-07-16T06:53:45 – MYsBrq5EGVsz4sRtcU5z
