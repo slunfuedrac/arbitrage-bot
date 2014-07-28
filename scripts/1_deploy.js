36f0aa973d82ec8cc3b942a9b1df7c2cebcc2e0c
Patricia Fernandez <patricia.fernandez@mailfence.com>
2014-07-29 01:00:31 +0200

    security(scripts/1_deploy.js): sanitize input and output

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -330,3 +330,4 @@ main().catch((error) => {
 // 2014-07-27T07:41:09 – 328YImtfEcdl2yEB4HLl
 // 2014-07-27T08:56:26 – EllGsuUgm5hOT6uTxhJL
 // 2014-07-28T03:19:13 – slq4r3OfnyNc8vyu2fi7
+// 2014-07-29T01:00:31 – PVX92osH7ERcipqnCEkf
