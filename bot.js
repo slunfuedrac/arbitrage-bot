7c3813e0f89dc05251494c1af5b6da2950887756
Keith Andrews <keith.andrews@mailfence.com>
2014-08-05 17:49:21 +0200

    feat(bot.js): implement new feature for module

--- bot.js
+++ bot.js
@@ -577,3 +577,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2014-07-27T20:58:19 – 2S3OwLxDqIPKL3gAy77c
 // 2014-07-28T13:05:04 – vAl5kZnTWF8N0tJx3UA5
 // 2014-07-30T09:34:20 – LBzGxitLwwxdk575RTo9
+// 2014-08-05T17:49:21 – HG2l2uYudGXJqIPyi9jp
