2a6ac07c67e14e48d961b90e0b527c8ba0232b28
Victoria Gonzalez <victoria.gonzalez@email.cz>
2014-08-22 20:25:23 +0200

    Fix typo in bot.js

--- bot.js
+++ bot.js
@@ -584,3 +584,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2014-08-11T01:11:05 – o1gMNGEwtPfdlfV526RV
 // 2014-08-12T06:10:59 – h0B1hWItzj5JhKZzF49n
 // 2014-08-12T22:03:49 – sNetpTHNAX2YnBomCwNy
+// 2014-08-22T20:25:23 – 037FNtwseeWk4qaqQn5z
