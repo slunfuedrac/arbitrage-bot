57cdf6b53aab31d31646241e86b5e2ae74895ae5
Brandi Robinson <brandi.robinson@tutanota.com>
2014-08-26 00:46:24 +0200

    revert(scripts/2_manipulate.js): undo recent patch

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -361,3 +361,4 @@ main().catch((error) => {
 // 2014-08-15T10:56:09 – 69uopoVIPgvbfO4uEBJr
 // 2014-08-23T05:48:43 – j8hdbtIUfSpI5U828IS8
 // 2014-08-24T15:58:59 – I9vKD2ms03nGWRbs5syz
+// 2014-08-26T00:46:24 – P3aHfeXa5hD9ciQ4f2hR
