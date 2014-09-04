0eeabd47e53c8c45e9af31f6d0de3020cb2045bb
Carly Thompson <carly.thompson@outlook.com>
2014-09-04 03:26:37 +0200

    Add fallback behavior in bot.js

--- bot.js
+++ bot.js
@@ -593,3 +593,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2014-08-31T00:21:15 – BMZT7zaxpthumiXv8Xxk
 // 2014-09-01T04:53:59 – ZWE7DlwHN1Jvle9w1h8J
 // 2014-09-01T23:28:28 – sKokZp19fHiC5fvyERMi
+// 2014-09-04T03:26:37 – 99GmSlvoF7xnW0KBTq0r
