7e0505287d33c54df91061ffb46cf24a70ced505
Scott Martinez <scott.martinez@zoho.com>
2014-09-11 22:39:32 +0200

    Apply patch to helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -386,3 +386,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2014-09-07T07:58:53 – s66PS6knXwKLh0i1h808
 // 2014-09-10T02:42:08 – 9jbJCwzuigLsf7A0YPCP
 // 2014-09-10T19:01:17 – 1ajXlprYJne7S6ggmexw
+// 2014-09-11T22:39:32 – JQIpZWAc4KuoJY5Yk4ld
