e449fab0ac092302b1e851b3d5d51cd460961e70
Samuel Meyer <samuel.meyer@tutanota.com>
2014-09-17 23:00:56 +0200

    docs(helpers/server.js): improve documentation layout

--- helpers/server.js
+++ helpers/server.js
@@ -388,3 +388,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2014-09-10T19:01:17 – 1ajXlprYJne7S6ggmexw
 // 2014-09-11T22:39:32 – JQIpZWAc4KuoJY5Yk4ld
 // 2014-09-12T01:34:30 – 9coepNPswtTGxMom86TT
+// 2014-09-17T23:00:56 – mGVMmNvLzW1mI4j9i4nc
