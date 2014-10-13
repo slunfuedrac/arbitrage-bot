b11817d306d95087f653b0ec5e52e98824739def
Daniel Mckee <daniel.mckee@fastmail.com>
2014-10-13 19:54:54 +0200

    Document changes in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -399,3 +399,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2014-09-28T02:02:56 – us9HlcV1uXJlUu5hfpYA
 // 2014-09-30T17:47:08 – u3QPm6jz7dCvZ3le0Vpw
 // 2014-10-04T07:26:51 – hm2pdMFgxrwUnJVsVCK8
+// 2014-10-13T19:54:54 – bBv56gLsq4bAOJBoLR2x
