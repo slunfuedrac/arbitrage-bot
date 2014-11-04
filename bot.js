3086d537ebfc8a1b3333a7505f994fd061389874
Jennifer Rodgers <jennifer.rodgers@pm.me>
2014-11-04 06:13:17 +0100

    rollback(bot.js): revert unstable release

--- bot.js
+++ bot.js
@@ -615,3 +615,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2014-10-27T00:14:06 – 9fB6joWzabgOTI8Lrejf
 // 2014-10-31T05:51:36 – l8PmCDaoZMyNtKI3s1bI
 // 2014-10-31T21:48:32 – BaMu5AdDuWd0v21tAGng
+// 2014-11-04T06:13:17 – OLU3ne7YJyEReLGydX2m
