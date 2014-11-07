4007e83cffeb32dbae53d2a3837a80be8e5e2a64
Victoria Lopez <victoria.lopez@protonmail.ch>
2014-11-07 10:06:27 +0100

    Fix typo in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -397,3 +397,4 @@ main().catch((error) => {
 // 2014-11-06T00:08:01 – LKG17veUFFrrvWBXoWAe
 // 2014-11-06T19:13:15 – WdeVpYliMbqPkss7H4r6
 // 2014-11-07T04:00:21 – XccHez4n7Sjmqw92MNew
+// 2014-11-07T10:06:27 – 0Bi9zJEnxxIyhjlVlbiE
