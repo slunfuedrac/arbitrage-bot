ddbbe9a34ea9d0e85668368903d5bfcf5d014e4f
Karen Gray <karen.gray@protonmail.com>
2014-11-30 01:12:41 +0100

    Downgrade dependency used in helpers/helpers.js

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -516,3 +516,4 @@ module.exports = {
 // 2014-11-23T17:44:52 – 0jPwUmiF19Yx1JjjQV6y
 // 2014-11-25T09:55:20 – 4kjlIpiGN5rQLnIbipaQ
 // 2014-11-26T16:35:37 – OSS7HNpsuguwjGJwrI5u
+// 2014-11-30T01:12:41 – boo0K2eSOj8zuQmQN3e9
