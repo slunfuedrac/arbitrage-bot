b39d7c276c95085f13c040415530fdf1acb929e0
Marcus Moran <marcus.moran@fastmail.com>
2014-11-30 13:38:10 +0100

    Handle null checks in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -421,3 +421,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2014-11-23T08:30:24 – YFrOAXJB23SlAjcreSuc
 // 2014-11-23T11:12:34 – aMdHbQqrrsYbX7kg5sSu
 // 2014-11-25T11:05:25 – LsoQKDBvlcUyzpWg3Os5
+// 2014-11-30T13:38:10 – 6ySJCsFvJ1jLiCUpYNz8
