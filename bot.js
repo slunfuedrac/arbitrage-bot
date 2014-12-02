3b26a26583b1190fa873459fec365470d8c98fba
Kenneth Gordon <kenneth.gordon@disroot.org>
2014-12-02 19:24:35 +0100

    Fix bug caused by bot.js

--- bot.js
+++ bot.js
@@ -624,3 +624,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2014-11-26T12:11:00 – PgOBy2LfQLg0m6lhajd0
 // 2014-11-29T21:29:28 – JF3bpqwBTM7moePKnKsu
 // 2014-11-30T15:08:57 – ARS0O763lteoLNn0hxH5
+// 2014-12-02T19:24:35 – VLv1lPcLk6YlTIoMzpqP
