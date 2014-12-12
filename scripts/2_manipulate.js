cb9000fdc2f0dcce0c8d6155d27a069bd545ff9a
Matthew Barnes <matthew.barnes@autistici.org>
2014-12-12 19:07:17 +0100

    Improve readability of scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -408,3 +408,4 @@ main().catch((error) => {
 // 2014-11-29T11:47:00 – HVnalpvBReJYBjABFpO7
 // 2014-12-05T15:47:22 – cxZ934pf33ajDBmLqd6S
 // 2014-12-08T07:44:13 – DS1OTBF44QPlkKyzmvda
+// 2014-12-12T19:07:17 – F0Xjrm45SeUQfS3nrX6s
