b5964a31933df909ccc4efccdf7a6d61b102c467
Julia Sanford <julia.sanford@autistici.org>
2014-12-20 23:18:02 +0100

    temp(scripts/1_deploy.js): experimental changes (to be removed)

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -387,3 +387,4 @@ main().catch((error) => {
 // 2014-12-13T05:43:33 – zXZKGLNis3MOV840DNQb
 // 2014-12-15T13:29:49 – qbNbxGDcFr5zldEWYuwy
 // 2014-12-18T18:44:55 – QrdHk4mW6bEJk2CakMgw
+// 2014-12-20T23:18:02 – slL9Yc65zaVP1Fp2G1Vy
