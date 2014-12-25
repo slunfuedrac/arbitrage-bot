d078b8574fd9397390f4a4c0063fe35efe35b0b2
Jennifer Richards <jennifer.richards@posteo.net>
2014-12-25 01:00:39 +0100

    Apply security patch to bot.js

--- bot.js
+++ bot.js
@@ -630,3 +630,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2014-12-17T18:16:23 – X7jll1x5REYvtKKabui4
 // 2014-12-19T03:36:22 – akHFVhPLqFhie2tibMOP
 // 2014-12-23T11:20:02 – nyuJqmaT6dLD0QB020gH
+// 2014-12-25T01:00:39 – ytonad7x6eg4qEjD8OhO
