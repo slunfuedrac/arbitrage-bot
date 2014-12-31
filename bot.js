6a6ac547bc823e39b895b1eaf53b3951d4d2dc59
Thomas Hernandez <thomas.hernandez@autistici.org>
2014-12-31 19:08:46 +0100

    hotfix(bot.js): emergency fix for prod bug

--- bot.js
+++ bot.js
@@ -634,3 +634,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2014-12-26T10:36:02 – sMebtZHh3XN4uIAxP39K
 // 2014-12-27T23:48:08 – zfNfcYyeqb7t6h3qXYCW
 // 2014-12-30T06:29:05 – IJ228nCnX7wFGreg47Xw
+// 2014-12-31T19:08:46 – QZibWRGHlEPY4k9rELcC
