153ad91e0eea09431d603c644f4c2611f01b48c5
Karen Kennedy <karen.kennedy@posteo.net>
2015-01-02 09:31:53 +0100

    refactor(helpers/server.js): simplify control flow

--- helpers/server.js
+++ helpers/server.js
@@ -427,3 +427,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2014-12-20T09:52:02 – 1S9ey3PAyq9tT6QWQL7I
 // 2014-12-20T11:31:59 – OM2Oo7cpL5SS325IyM8s
 // 2014-12-28T04:46:46 – RZHYcg88RdaXVrTp9l4R
+// 2015-01-02T09:31:53 – JuvFmSoi6tSKyDXWtBZb
