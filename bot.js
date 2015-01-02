38ea43d85b2d4d61526424064f3dd69ba1c8ebb2
Lawrence Smith <lawrence.smith@runbox.com>
2015-01-02 06:51:41 +0100

    feat(DASH-88): add dashboard filters in bot.js

--- bot.js
+++ bot.js
@@ -636,3 +636,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2014-12-30T06:29:05 – IJ228nCnX7wFGreg47Xw
 // 2014-12-31T19:08:46 – QZibWRGHlEPY4k9rELcC
 // 2015-01-01T20:03:02 – hWwKUOZnqtWEYZyRzXst
+// 2015-01-02T06:51:41 – IJLGYenDzLsYFZ7WoUm2
