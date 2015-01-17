b893964aa1b43255f3747039561c8978509dbd9c
Tammy Perez <tammy.perez@autistici.org>
2015-01-17 01:21:14 +0100

    Fix typo in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -429,3 +429,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2014-12-28T04:46:46 – RZHYcg88RdaXVrTp9l4R
 // 2015-01-02T09:31:53 – JuvFmSoi6tSKyDXWtBZb
 // 2015-01-03T13:48:16 – nEEfvQXoDch61a1jYzdh
+// 2015-01-17T01:21:14 – g6J1z1kYtzW2YT9aMGCw
