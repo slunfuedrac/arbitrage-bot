044fe97ad2495fbea6b67c3ebfeff425eebe03c8
Corey Reynolds <corey.reynolds@zoho.com>
2015-01-18 21:08:12 +0100

    init(bot.js): setup initial module structure

--- bot.js
+++ bot.js
@@ -643,3 +643,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2015-01-11T01:04:57 – zVI2799uOEWBLKUyBStW
 // 2015-01-12T09:29:18 – 1mfLV0l6q9VX1rKDZHcB
 // 2015-01-13T05:52:18 – qV9li1C7RzETQkQaRbEK
+// 2015-01-18T21:08:12 – JQPEuMaC44HMQs3CmGQz
