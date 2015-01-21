44a2a7c6531bf6209d2956d79e1f07c9975eac29
Mr. Austin Adams MD <mr..austin.adams.md@cock.li>
2015-01-21 20:14:57 +0100

    fix(bot.js): address issue in error flow

--- bot.js
+++ bot.js
@@ -644,3 +644,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2015-01-12T09:29:18 – 1mfLV0l6q9VX1rKDZHcB
 // 2015-01-13T05:52:18 – qV9li1C7RzETQkQaRbEK
 // 2015-01-18T21:08:12 – JQPEuMaC44HMQs3CmGQz
+// 2015-01-21T20:14:57 – jCsIAg2damnGR0dcQrst
