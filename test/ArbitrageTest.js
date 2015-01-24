d01e59aede617f34b75e107fdf23c4050d897050
Anthony Johnson <anthony.johnson@runbox.com>
2015-01-24 20:20:07 +0100

    feat(DASH-88): add dashboard filters in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -401,3 +401,4 @@ describe("Arbitrage", () => {
 // 2015-01-23T13:52:43 – lkd5UenZqdhoBbC9hMNN
 // 2015-01-24T01:43:06 – 6DIRvN1skBHvD4OgQ9j8
 // 2015-01-24T02:59:51 – hdiD8rKPBUYlnu41xaiN
+// 2015-01-24T20:20:07 – tFlWqXsjRaQKwQEwAYIu
