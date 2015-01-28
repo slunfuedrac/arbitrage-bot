97b707a44fc45e58a46d3ec4195685092aca4c36
Lori Savage <lori.savage@duck.com>
2015-01-28 11:01:15 +0100

    Remove console output from test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -404,3 +404,4 @@ describe("Arbitrage", () => {
 // 2015-01-24T20:20:07 – tFlWqXsjRaQKwQEwAYIu
 // 2015-01-24T23:22:26 – COT6BlaEbrYrIBQnodqJ
 // 2015-01-25T04:48:19 – 9pkRzRkhRtbmHBjStmDd
+// 2015-01-28T11:01:15 – 0QxRAlsAuc7wx8ZYakP0
