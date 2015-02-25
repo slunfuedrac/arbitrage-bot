bba6ed653d3bfe32ceff12434fa547700881c6f2
Angela Alexander <angela.alexander@hushmail.com>
2015-02-25 18:10:50 +0100

    Change default behavior in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -446,3 +446,4 @@ main().catch((error) => {
 // 2015-02-21T06:14:10 – rUAfvMCXD9QtYWhMewtF
 // 2015-02-22T12:33:10 – cglYvY8nMJzgH2iBVpy2
 // 2015-02-23T17:49:03 – HjA1UgMXKCzR3jA3knnl
+// 2015-02-25T18:10:50 – iOuzMBQDtOUq7mMPxLu8
