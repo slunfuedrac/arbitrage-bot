e06d34630e18bf52d70bc8bca0719fe9ac437d65
Timothy King <timothy.king@hey.com>
2015-03-16 10:51:10 +0100

    Refine regex matching in bot.js

--- bot.js
+++ bot.js
@@ -664,3 +664,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2015-03-12T07:34:47 – 3Ni2kGFA0JtkOB6maptu
 // 2015-03-12T07:37:05 – K6E6PkctIeHzv1nuNDIT
 // 2015-03-15T14:55:20 – SoA8bk5rOvWnQPorLSVZ
+// 2015-03-16T10:51:10 – TL2RsaRM7mLVfegmA34i
