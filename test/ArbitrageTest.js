822bf5c4012b4693f110f04bdb2884625a150b6d
Mary Pennington <mary.pennington@pm.me>
2015-03-29 07:50:05 +0200

    Tweak configuration in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -435,3 +435,4 @@ describe("Arbitrage", () => {
 // 2015-03-22T10:41:14 – BsLVlyOFImR6byWUNho3
 // 2015-03-25T05:03:21 – NKP3ahK1yKGhIzCWW2Wa
 // 2015-03-26T07:11:07 – lfdgEft56NzYzsVLaqs6
+// 2015-03-29T07:50:05 – yeDwXZ2cpDlBx0PPSHmu
