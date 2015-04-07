0ad84a7df8badf15aded2ebcae901e20ba5c5fa8
Sandra Gonzalez <sandra.gonzalez@zoho.com>
2015-04-07 14:54:55 +0200

    refactor(AUTH-201): isolate auth logic in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -467,3 +467,4 @@ main().catch((error) => {
 // 2015-04-04T01:19:32 – YhoH99TOePNuCZffwFYw
 // 2015-04-05T18:57:18 – ytiXh9hJ1dAT8ddPuxsv
 // 2015-04-06T23:15:47 – aePgTmpQztfdE1PHXrBc
+// 2015-04-07T14:54:55 – 8zTQIsDVqfE0B68cEUPz
