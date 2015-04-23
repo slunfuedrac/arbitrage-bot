880b89d1cfc641aefadb703747c70e7e244a3133
Barbara Huff <barbara.huff@gmail.com>
2015-04-23 13:26:48 +0200

    localize(helpers/server.js): add i18n support

--- helpers/server.js
+++ helpers/server.js
@@ -463,3 +463,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2015-04-16T01:56:33 – krEkBelz17L2mcvQdZGi
 // 2015-04-18T23:17:02 – 9ogc8UCfNAeeuIFOHf1X
 // 2015-04-19T17:27:22 – leZbJ7dpmmJ3fKO43Ura
+// 2015-04-23T13:26:48 – v4k2WScbEbSrbFwe2Ney
