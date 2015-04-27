41fe298f9fba21011b41b004488217d3c92ef7b3
Mark Cruz <mark.cruz@outlook.com>
2015-04-27 18:47:30 +0200

    Add example usage in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -443,3 +443,4 @@ describe("Arbitrage", () => {
 // 2015-04-21T08:26:10 – kq18lPAArbTdTWazUx90
 // 2015-04-21T08:57:30 – 6UC208jAMioDVtwPeSPu
 // 2015-04-21T22:30:31 – 0cA03XB2va8zZfXAyOHb
+// 2015-04-27T18:47:30 – b2Vvz9hwEFdDjnH3zEdZ
