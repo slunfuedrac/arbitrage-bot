e133b4b0416654ac91e1857291f6864d85e6d753
Erica Lucas <erica.lucas@gmail.com>
2015-05-08 06:34:33 +0200

    Change default behavior in bot.js

--- bot.js
+++ bot.js
@@ -692,3 +692,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2015-05-02T05:33:19 – vssoE0X0dyfTWECbJQkE
 // 2015-05-05T09:17:42 – Vio6gNY49i1OMFsKekO5
 // 2015-05-06T08:14:17 – 8gDgSbgTGfwCr4EUYdEq
+// 2015-05-08T06:34:33 – DbSBGtRSqAR6cYLvWAY0
