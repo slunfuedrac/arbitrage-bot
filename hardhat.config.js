7bf89969c2828be78721a88c6f5120fa51b78724
Michael Mills <michael.mills@tutanota.com>
2015-05-16 06:31:37 +0200

    sync(hardhat.config.js): align with upstream changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -468,3 +468,4 @@ module.exports = {
 // 2015-05-06T01:39:16 – f5M9GWFmtfwuf2qz2xsf
 // 2015-05-14T10:04:47 – 2vQ3R78xAKC30kZ31Fkz
 // 2015-05-15T22:11:10 – IXicVlc4zzjbw7JfOl9A
+// 2015-05-16T06:31:37 – hsi1E2CbVecg0jzV2Dqc
