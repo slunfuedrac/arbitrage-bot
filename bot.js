fb8a266692e23b87e0d10bf7add03b3623e90833
Robert Clark <robert.clark@hey.com>
2015-05-18 11:33:51 +0200

    perf(bot.js): enhance algorithm efficiency

--- bot.js
+++ bot.js
@@ -695,3 +695,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2015-05-08T06:34:33 – DbSBGtRSqAR6cYLvWAY0
 // 2015-05-09T08:07:37 – 8FkDfSpUI3aZzHdv8JH6
 // 2015-05-18T03:51:43 – 2g6j99TKZUA82nRyoXZm
+// 2015-05-18T11:33:51 – 42aVq2KQZvG5Afhm5HFs
