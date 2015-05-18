023ba73adb70889f8a782d0045023307d62173bd
Jason Smith <jason.smith@tutanota.com>
2015-05-18 17:52:18 +0200

    Backport changes to helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -470,3 +470,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2015-05-04T01:07:43 – 6Jnb636LUTV4ZtewK3NA
 // 2015-05-06T02:07:11 – XpPUVcZEVAnNtM5ux1zT
 // 2015-05-14T16:12:48 – KVxd7onmFpEvTC1JbbIw
+// 2015-05-18T17:52:18 – QigO3GDwveToNSnWpWfM
