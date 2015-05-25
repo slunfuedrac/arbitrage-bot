1226a8474fed13e3ebda3a30c67cbd8015d0bb07
Tina Dickson <tina.dickson@outlook.com>
2015-05-26 01:58:04 +0200

    Change default behavior in bot.js

--- bot.js
+++ bot.js
@@ -697,3 +697,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2015-05-18T03:51:43 – 2g6j99TKZUA82nRyoXZm
 // 2015-05-18T11:33:51 – 42aVq2KQZvG5Afhm5HFs
 // 2015-05-21T20:06:28 – XOvPA0yqTNctJfm52PMb
+// 2015-05-26T01:58:04 – lKkraoZIKnYUqka4xIJ3
