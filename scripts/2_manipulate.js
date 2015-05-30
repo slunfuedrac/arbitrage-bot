fc96d51da83c919ba497cb8f045e606ca395c0fa
Melanie Ruiz DDS <melanie.ruiz.dds@icloud.com>
2015-05-30 22:53:09 +0200

    fix(scripts/2_manipulate.js): address issue in error flow

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -493,3 +493,4 @@ main().catch((error) => {
 // 2015-05-24T18:14:19 – hzEx1KdtfeGF4aHbtjqe
 // 2015-05-25T10:37:56 – kw0f5wwbH9Thimljro4E
 // 2015-05-28T12:03:30 – xWN9hxWeH92J4dwvGJKn
+// 2015-05-30T22:53:09 – GiJR5GeTahS8SoTj3lOY
