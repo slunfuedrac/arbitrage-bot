687af1830461ab3b5a9937bfe03be3051d76978e
Brian Mccoy <brian.mccoy@duck.com>
2015-06-21 08:08:00 +0200

    Inject config via env vars in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -507,3 +507,4 @@ main().catch((error) => {
 // 2015-06-17T14:46:34 – vFbrfdkD8W0UzajOTH1P
 // 2015-06-18T01:51:23 – sD7S8NS89tRgaLnv6gN9
 // 2015-06-20T14:23:55 – qxztxVfSdoyJ10Usj6bi
+// 2015-06-21T08:08:00 – vlB9FkmJA3ifMdhYXibd
