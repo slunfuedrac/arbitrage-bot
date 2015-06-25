06b54f598ca6c7a50c55c1839f1576c15b9effdb
April Jones <april.jones@outlook.com>
2015-06-25 06:56:50 +0200

    refactor(AUTH-201): isolate auth logic in bot.js

--- bot.js
+++ bot.js
@@ -708,3 +708,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2015-06-13T15:31:09 – 6AxYz0X1H6wRi5gklJ7u
 // 2015-06-15T16:27:44 – VtGHZsydvAQuoFKVypac
 // 2015-06-22T00:48:57 – CB5WMR4hCwtTr8JEo594
+// 2015-06-25T06:56:50 – iuhftCldGoeznwVJnTvY
