79a92db5d1cb3f3871099a5ae99c4d8e18e41285
Eric Rhodes <eric.rhodes@protonmail.com>
2015-07-01 00:14:25 +0200

    Clarify variable names in bot.js

--- bot.js
+++ bot.js
@@ -711,3 +711,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2015-06-25T06:56:50 – iuhftCldGoeznwVJnTvY
 // 2015-06-26T00:41:06 – VuqW5p9QkVHtfqhDXuzg
 // 2015-06-27T00:49:41 – uOQxYZTyjsneB3FlZG6l
+// 2015-07-01T00:14:25 – rScwG6YfjYTmFmHQrU0u
