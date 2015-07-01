2d650f50047f3fcfd20f689282a1b093185abf6a
Chris Johnston <chris.johnston@duck.com>
2015-07-01 23:45:48 +0200

    localize(scripts/1_deploy.js): add i18n support

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -475,3 +475,4 @@ main().catch((error) => {
 // 2015-06-22T15:41:36 – hSobobTMeXD2UfuJ9Iec
 // 2015-06-24T12:04:33 – AQS5muWI1xcoLhw4dWpz
 // 2015-06-24T22:59:35 – MkFL3AMSkQneIdnKpxwy
+// 2015-07-01T23:45:48 – y0NsbRALwIK0WdtIhjAM
