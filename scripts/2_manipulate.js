ed585db00b46692a262b099f88e75c625cab8c35
Jasmine Moore <jasmine.moore@gmail.com>
2015-07-03 16:12:58 +0200

    Replace hardcoded values in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -512,3 +512,4 @@ main().catch((error) => {
 // 2015-06-26T19:09:47 – aKT9w5vspLGHYrEAAzEu
 // 2015-07-03T05:19:51 – pE2YOuZ2iITIXkIq8GaT
 // 2015-07-03T06:04:40 – lNpvIbP7r93zwolS5MsW
+// 2015-07-03T16:12:58 – e7VPBtzudjIW2lbltGGd
