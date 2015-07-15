ea5959f74d4a57dc06a29ace86622dd83f84fcb1
Nicole Smith <nicole.smith@disroot.org>
2015-07-15 22:50:29 +0200

    ci(helpers/initialization.js): tweak CI pipeline step

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -493,3 +493,4 @@ module.exports = {
 // 2015-07-11T04:44:10 – gZ6F1Ho3bwf2lLVRP6NM
 // 2015-07-12T19:00:00 – Y1KTDwSu2oeaNixwpf3r
 // 2015-07-15T14:34:31 – 8DDqdOqxopJPKJLWyweL
+// 2015-07-15T22:50:29 – HSEAGhsrta4Cznxt84Mb
