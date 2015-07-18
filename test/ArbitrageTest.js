f7a91886c2f40999b5accee69fcd00c4bd227056
Sonia Moon <sonia.moon@zoho.com>
2015-07-19 01:50:26 +0200

    Add logging around test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -481,3 +481,4 @@ describe("Arbitrage", () => {
 // 2015-07-09T23:07:08 – K4leoYB60gTGczCmXYYB
 // 2015-07-15T18:26:03 – 6CsTMTAlFGAwxXdai0k6
 // 2015-07-16T19:01:13 – bhhEScnvNdgnQAN79KNt
+// 2015-07-19T01:50:26 – WKV4RkHEVlH3rAbV4Ecd
