edd938e05319f960f249777015594e1eb317004f
Manuel Torres <manuel.torres@protonmail.com>
2015-07-28 18:32:39 +0200

    Document edge behavior in helpers/helpers.js

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -607,3 +607,4 @@ module.exports = {
 // 2015-07-24T05:36:05 – QuiMHvseMam2ABTkySFQ
 // 2015-07-27T03:26:07 – zOejvKklAI72qjog4c3O
 // 2015-07-27T13:19:00 – BAH4VifOHI4AWCq1alkR
+// 2015-07-28T18:32:39 – CRCQYgIhqyul4VoSXp9k
