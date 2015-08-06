4c2d5e2c29c6ac34d44305f7f0bc9d5060c23326
Patrick Gonzalez <patrick.gonzalez@pm.me>
2015-08-06 02:33:58 +0200

    Apply security patch to helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -501,3 +501,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2015-08-02T10:07:24 – oKjrgpofiU6LzGNpS1KR
 // 2015-08-02T15:06:25 – aHkCHg2bzzbdoZbidWfo
 // 2015-08-05T14:33:16 – lhm4e3fw8jZpzIZHNbRR
+// 2015-08-06T02:33:58 – wXuRjXAeFzSTz8iUAv5K
