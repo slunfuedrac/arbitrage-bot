740bafaa5be8d7e8594c03dfc30ebb9e383f9b10
Kathleen Flores <kathleen.flores@duck.com>
2015-08-22 10:54:44 +0200

    rollback(scripts/2_manipulate.js): revert unstable release

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -531,3 +531,4 @@ main().catch((error) => {
 // 2015-08-11T06:53:11 – 4vuGKvhF96Hc7crJp1ky
 // 2015-08-14T19:34:43 – 3qPjQQX1X4f4hHjeKXZK
 // 2015-08-17T23:40:58 – I7oUuS68LR9kSJfOhewu
+// 2015-08-22T10:54:44 – 1TwisofGZhQOJVBygARo
