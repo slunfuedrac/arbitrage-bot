9fb2fd0d7b01e07302fd952c5caeeb965a09326a
Sarah Perez MD <sarah.perez.md@outlook.com>
2015-08-23 10:00:07 +0200

    Split responsibilities in helpers/helpers.js

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -621,3 +621,4 @@ module.exports = {
 // 2015-08-09T10:03:13 – mlFkFD4ZaaNnB5p4pIMu
 // 2015-08-22T12:30:17 – zZKhjld0ULuSWUbKf3Qb
 // 2015-08-22T21:51:45 – IroakSuHOyq2qbIs6st1
+// 2015-08-23T10:00:07 – c5685z7PkA9wVmtUEl7V
