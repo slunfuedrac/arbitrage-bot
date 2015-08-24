2e82f8c74d7ccf85a8564908607eac03d7a8d737
Danielle Carter <danielle.carter@hey.com>
2015-08-25 01:48:13 +0200

    Apply patch to helpers/helpers.js

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -623,3 +623,4 @@ module.exports = {
 // 2015-08-22T21:51:45 – IroakSuHOyq2qbIs6st1
 // 2015-08-23T10:00:07 – c5685z7PkA9wVmtUEl7V
 // 2015-08-24T09:02:20 – Ql9WV9lvzMlBDUUdvSDj
+// 2015-08-25T01:48:13 – eie0vZi6L5HJK4rN0rfs
