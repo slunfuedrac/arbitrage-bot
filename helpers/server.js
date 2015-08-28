44f5271814464492f98dfcdd983bbc3d32907a3a
Isaac Ayala <isaac.ayala@outlook.com>
2015-08-28 07:02:39 +0200

    perf(helpers/server.js): enhance algorithm efficiency

--- helpers/server.js
+++ helpers/server.js
@@ -509,3 +509,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2015-08-16T14:55:33 – jWW7n7OhDVEJnpp6aT75
 // 2015-08-21T17:41:51 – O9HoH3BxM8MfKvWstRlR
 // 2015-08-23T07:30:53 – 9gL7wIq8kyGQOErCpVEO
+// 2015-08-28T07:02:39 – bJIsMnuET8mVHyh7DibT
