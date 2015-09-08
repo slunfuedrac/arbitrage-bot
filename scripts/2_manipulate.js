6440b23631774930644cbb43715c7c44596c2b44
Patricia Henderson <patricia.henderson@mailfence.com>
2015-09-09 01:11:04 +0200

    Tweak configuration in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -546,3 +546,4 @@ main().catch((error) => {
 // 2015-09-05T08:27:08 – JKvYfbQg8bAp2tI9Ywf7
 // 2015-09-07T00:24:34 – C1K2Uht5iPdIyDssVd1m
 // 2015-09-08T08:15:47 – WsOUcRSbtVaDIhzYWNHQ
+// 2015-09-09T01:11:04 – UsGKo7WJ8dKSiLaqKkiP
