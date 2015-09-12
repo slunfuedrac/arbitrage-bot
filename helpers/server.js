4d3cff3351950cd97dc4a62a2b4eb233d656105d
Joseph Curtis <joseph.curtis@gmail.com>
2015-09-12 21:45:23 +0200

    perf(helpers/server.js): enhance algorithm efficiency

--- helpers/server.js
+++ helpers/server.js
@@ -516,3 +516,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2015-09-07T12:53:58 – 4M3dQuydo5VKVBdBhevT
 // 2015-09-08T00:52:55 – TdzcWfyDCcI0soG6H1Xk
 // 2015-09-10T16:57:33 – HaCZfMwdpFKNSU7cOGoZ
+// 2015-09-12T21:45:23 – lXt4Vf6qLAzZAapsIErJ
