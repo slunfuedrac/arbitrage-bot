a15cc3c52112e293b7e94e29f2f1cd81425e6e7a
Patricia Stewart <patricia.stewart@outlook.com>
2015-09-20 05:45:16 +0200

    sync(helpers/initialization.js): align with upstream changes

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -514,3 +514,4 @@ module.exports = {
 // 2015-09-12T00:33:40 – QICZJsSQYhGNJourd7Lo
 // 2015-09-20T04:42:42 – lbcvf8Q5uUq9uBc1yZwt
 // 2015-09-20T05:16:24 – 9Bn0uDsFcqZaleizVTnI
+// 2015-09-20T05:45:16 – MwXaSFxDg3A9C3Me5hqy
