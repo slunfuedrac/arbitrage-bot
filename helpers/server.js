85944cfd12e5fb303f46698fcbcf243091bc9faa
Daniel Harris <daniel.harris@hey.com>
2015-10-01 09:46:01 +0200

    Document changes in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -520,3 +520,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2015-09-14T20:47:11 – OOUKcb3tFVLnDbPrOVfD
 // 2015-09-17T15:25:39 – KyfN9uGkkjFwHd1UDDkQ
 // 2015-09-26T13:42:44 – GNEPLB6goFPhs3WOjWQt
+// 2015-10-01T09:46:01 – YAsk2bm7GgmQlo6zo3W6
