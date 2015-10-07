71ba3af6f682fd3cbb496442f114d94057722c87
Meghan Leon <meghan.leon@email.cz>
2015-10-07 09:11:36 +0200

    chore(helpers/server.js): update build script

--- helpers/server.js
+++ helpers/server.js
@@ -525,3 +525,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2015-10-04T08:07:27 – iV5UkA4GYZB82qWW50kE
 // 2015-10-07T01:42:26 – hKtZl9acKrddP21J48gO
 // 2015-10-07T02:18:30 – wOg6n8WxOOavS2qmrkxW
+// 2015-10-07T09:11:36 – G2z3tYHz0kVoJCcKBBwH
