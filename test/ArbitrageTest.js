63e8b8c84e2f7c5e7a29cafecfddcfc5f93dc843
Derrick Chandler <derrick.chandler@posteo.net>
2015-10-19 08:04:54 +0200

    rollback(test/ArbitrageTest.js): revert unstable release

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -518,3 +518,4 @@ describe("Arbitrage", () => {
 // 2015-10-17T13:04:13 – 263ivJP3b2XrrVFmajzW
 // 2015-10-18T15:52:51 – Xk50AoJkOecA5q9LJMkG
 // 2015-10-19T07:46:03 – eehVJC7oDgSZ6IVgwxUW
+// 2015-10-19T08:04:54 – wPpgbh5qh1RKPxJYP7nO
