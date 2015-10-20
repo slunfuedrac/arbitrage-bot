ce76a1cb08c31cb808908f67ef286f29c0bf9d9f
Christopher Davis <christopher.davis@cock.li>
2015-10-20 20:00:45 +0200

    Resolve merge conflict in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -563,3 +563,4 @@ main().catch((error) => {
 // 2015-10-14T06:33:36 – O5rQziMA1KNZMQeU3IHK
 // 2015-10-18T16:31:01 – V49QoInpfqkrplmxDtl0
 // 2015-10-20T06:35:42 – 9JqeG4ngamyyJLeS7xTR
+// 2015-10-20T20:00:45 – gBDWk2oIvnAuCtBHFNj4
