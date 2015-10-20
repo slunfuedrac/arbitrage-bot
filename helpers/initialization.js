dddbfe40780e721534782472c6443c04a94e18e6
James Kelley <james.kelley@protonmail.ch>
2015-10-20 19:29:07 +0200

    rollback(helpers/initialization.js): revert unstable release

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -525,3 +525,4 @@ module.exports = {
 // 2015-10-12T21:29:31 – zNZfxuQcIuQOJIoiD9Mi
 // 2015-10-13T12:13:51 – BFC4mtyzne5LBkRevCun
 // 2015-10-19T01:44:40 – lTX2dlqyZYZRJBYmwV7n
+// 2015-10-20T19:29:07 – VXDhmbT8URPKnB4CGbdc
