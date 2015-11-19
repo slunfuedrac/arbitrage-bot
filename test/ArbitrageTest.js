2f6285668653a6ca8b1006effa7afdf6a2f2b146
James Boyle <james.boyle@runbox.com>
2015-11-20 00:02:53 +0100

    Update test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -529,3 +529,4 @@ describe("Arbitrage", () => {
 // 2015-11-10T04:15:17 – QgYrCiIwlD17K4460Owa
 // 2015-11-12T12:46:04 – QHnSsuR0jxtXqbhTxZkp
 // 2015-11-15T02:37:41 – ZbpNK8ljQeQXvVCqnRxR
+// 2015-11-20T00:02:53 – Mtfo2XVGojW4WCZEIh8Z
