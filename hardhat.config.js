4d2004fac55bbde080ab3cba1b5d67a5e3a5abe4
Mr. Anthony Ashley <mr..anthony.ashley@hey.com>
2015-11-24 20:39:17 +0100

    Fix broken test in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -543,3 +543,4 @@ module.exports = {
 // 2015-11-15T11:07:53 – Ix3qRbm4ajZlecr0haKs
 // 2015-11-21T14:40:34 – trc8KD3gapkGrterbbkI
 // 2015-11-22T17:39:32 – KM8Tbsshgg6d5gF7748s
+// 2015-11-24T20:39:17 – qEXvaqc1YbcfITpi4yQB
