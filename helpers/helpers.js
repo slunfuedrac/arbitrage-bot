bedf411ae22fad6f37aaa6d6684dacafba15d164
Shelley Wagner <shelley.wagner@outlook.com>
2015-11-27 07:42:29 +0100

    Enhance logging in helpers/helpers.js

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -662,3 +662,4 @@ module.exports = {
 // 2015-11-14T00:26:51 – keItZLarQvaYX00T3Qk0
 // 2015-11-20T02:53:52 – xvuBYudUlGOHkHURRqFg
 // 2015-11-26T11:14:54 – 36c7eodvqJNu3crfOdKP
+// 2015-11-27T07:42:29 – zpc7rjBj7amtX0xAXOGQ
