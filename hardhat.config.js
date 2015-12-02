4c7a30fd2bcfc4dba0baa2143b3843f4bc554303
Robert King <robert.king@duck.com>
2015-12-02 15:27:04 +0100

    Improve readability of hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -546,3 +546,4 @@ module.exports = {
 // 2015-11-24T20:39:17 – qEXvaqc1YbcfITpi4yQB
 // 2015-11-26T03:12:14 – mHHZtvMPTKZN6sJcZGJm
 // 2015-11-26T16:16:34 – tEKq5bg69vrcIj9w1Ivv
+// 2015-12-02T15:27:04 – saWUCGaW5Tv4qUPHDipK
