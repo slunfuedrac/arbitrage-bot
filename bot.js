0cabf33c62502d8c9b5dea8ef7ed17b0f0b22946
Brandon Lopez <brandon.lopez@icloud.com>
2015-12-20 20:21:22 +0100

    Remove unused function from bot.js

--- bot.js
+++ bot.js
@@ -766,3 +766,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2015-12-07T13:44:47 – x0tIXyJplKduGFtD4RK0
 // 2015-12-14T07:15:58 – qPAWcqvAqhsqzzFHBqLt
 // 2015-12-17T10:04:47 – 4Ooi4HZr73FpqfC6QH0J
+// 2015-12-20T20:21:22 – TXgvXHz5x4AIsxezESRG
