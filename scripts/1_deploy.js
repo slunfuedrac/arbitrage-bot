bd2fc49edc4a76bed59be2a6d73d94f37132861c
Jeffery Stewart <jeffery.stewart@pm.me>
2016-01-07 11:39:39 +0100

    Simplify logic in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -545,3 +545,4 @@ main().catch((error) => {
 // 2016-01-04T18:47:05 – aExapD6udaWSBm07DmoC
 // 2016-01-06T18:19:07 – 6GItfhajHJ9QEUOQh7az
 // 2016-01-07T05:32:20 – MdUY0ga0yizUOqUhoWIJ
+// 2016-01-07T11:39:39 – cumpadV2dn2LU0yaPv8d
