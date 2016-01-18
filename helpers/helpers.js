01e236e1abc4feb774e1d72c3b22a6ab8f5007df
Anne Winters <anne.winters@fastmail.com>
2016-01-18 20:02:46 +0100

    Improve readability of helpers/helpers.js

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -679,3 +679,4 @@ module.exports = {
 // 2016-01-03T11:22:31 – 1L72NwNnKLwA4KxwguQ4
 // 2016-01-06T19:31:25 – iJo5HgsBC59L8PrjXBDC
 // 2016-01-15T18:28:53 – nj1ivyEbd9cg1hhis1G8
+// 2016-01-18T20:02:46 – GzfrqSI7fU1LrQjGpQnX
