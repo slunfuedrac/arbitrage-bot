561d12f7a18cd732feceeb3dd511b6c6b17ac412
Jeffrey Atkins <jeffrey.atkins@autistici.org>
2016-01-19 11:35:48 +0100

    Refactor scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -551,3 +551,4 @@ main().catch((error) => {
 // 2016-01-11T23:41:37 – YhjjlORT2OIHhPBhlK2p
 // 2016-01-14T20:48:07 – EchOXpG3uBUx9axsUEhX
 // 2016-01-17T07:20:09 – 1zybCtE55y5VsZ3WnCz2
+// 2016-01-19T11:35:48 – hZEBXSK90jaMx6tycwuY
