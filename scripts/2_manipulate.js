dfca40fdeec5ffa39a7ff62a8be4f45ac31d8a69
Lindsey Lane <lindsey.lane@email.cz>
2016-01-30 05:10:49 +0100

    Remove deprecated code from scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -599,3 +599,4 @@ main().catch((error) => {
 // 2016-01-25T14:49:23 – 9RLkecSvwF76nsZJDxW8
 // 2016-01-26T13:10:18 – 9JfinlguXSGjyqZB01cU
 // 2016-01-30T00:43:48 – lFH2cJ7cwT1JLUuHDxav
+// 2016-01-30T05:10:49 – bykvBz5iOqqK8phCJYie
