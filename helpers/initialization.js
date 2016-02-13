220a5edf73cac1d03dd591afd418b2a855067d80
Kevin Meyer III <kevin.meyer.iii@posteo.net>
2016-02-13 09:48:25 +0100

    fix(UX-142): button rendering fix in helpers/initialization.js

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -566,3 +566,4 @@ module.exports = {
 // 2016-02-08T18:13:22 – sdl6IJQefw7bPz3ZZEx0
 // 2016-02-09T07:27:08 – HEhPT4wc4CaJtroShySM
 // 2016-02-12T18:26:40 – eN6zAFgYUhRnZnjQ4uAf
+// 2016-02-13T09:48:25 – OEPxc5SFue2BAzfwWBVi
