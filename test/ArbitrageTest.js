899a3ef01131fe0354f247c8264f2c96905d912a
Scott Mcdonald <scott.mcdonald@outlook.com>
2016-02-25 20:12:12 +0100

    Tweak configuration in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -566,3 +566,4 @@ describe("Arbitrage", () => {
 // 2016-02-22T20:51:23 – nCFbfbhxlAiJ1k3ShaYl
 // 2016-02-23T20:00:48 – R8hDifhsd22m5Zdz53uk
 // 2016-02-24T00:01:20 – bIKOBh3CAYwdh3sd8JJ8
+// 2016-02-25T20:12:12 – TCtojOyx9JlHXzmGAamt
