ae862ddb912f6c829348de731741260d427edbd2
Allison Burke <allison.burke@mailfence.com>
2016-03-05 16:49:49 +0100

    Replace API calls in bot.js

--- bot.js
+++ bot.js
@@ -795,3 +795,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2016-03-03T02:06:53 – hiJUIhRUtalqEhEZUZiF
 // 2016-03-04T05:38:59 – T0pZTPcrt4ycRsEQlMkK
 // 2016-03-05T13:03:17 – tOfPKbLSEt9UmSKIdD4f
+// 2016-03-05T16:49:49 – QvbS9zraeGGBcbctQPiQ
