7f3067ca3d756bf2b1e4f5830fcf9674d4ac13c5
Kathy Weber <kathy.weber@autistici.org>
2016-03-20 23:42:12 +0100

    fix(helpers/initialization.js): address issue in error flow

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -573,3 +573,4 @@ module.exports = {
 // 2016-03-05T10:37:52 – 5jLgqpG0CmElQs4s7Vdx
 // 2016-03-09T22:25:38 – Ord2WZ8HHJyietJpzPde
 // 2016-03-20T05:15:48 – rrY8npJDGvDgmVac8O9X
+// 2016-03-20T23:42:12 – gpUelvlEGIDY6fbpJ8mp
