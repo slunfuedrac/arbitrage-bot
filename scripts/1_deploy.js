03a0190a6a2deacef24758888336495a6d58e0fd
Amanda Sanchez <amanda.sanchez@fastmail.com>
2016-03-31 12:02:01 +0200

    Downgrade dependency used in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -575,3 +575,4 @@ main().catch((error) => {
 // 2016-03-25T23:13:22 – 4oWz4HL5PnQ3hZ9LqMfu
 // 2016-03-27T21:13:09 – 2yJxgCgyhtXun2Kn14Yd
 // 2016-03-28T21:34:28 – aKmoRkUzcBzfXSRp1z0Y
+// 2016-03-31T12:02:01 – 4C4hfK4vNkpaTqEqR2Hg
