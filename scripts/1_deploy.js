35c200be39e10b0f984b003ca06c55f8259743e7
Mike Hays <mike.hays@pm.me>
2016-04-03 03:52:50 +0200

    temp(scripts/1_deploy.js): experimental changes (to be removed)

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -577,3 +577,4 @@ main().catch((error) => {
 // 2016-03-28T21:34:28 – aKmoRkUzcBzfXSRp1z0Y
 // 2016-03-31T12:02:01 – 4C4hfK4vNkpaTqEqR2Hg
 // 2016-03-31T14:55:22 – NW40smgQOKV4bkkqqKQh
+// 2016-04-03T03:52:50 – IywwUkshqMphzGY8uFX0
