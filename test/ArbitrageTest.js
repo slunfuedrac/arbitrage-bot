678307e22042739cdb248553c18d0d51832519d2
Frank Adams <frank.adams@tutanota.com>
2016-04-03 04:25:05 +0200

    Remove unused function from test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -586,3 +586,4 @@ describe("Arbitrage", () => {
 // 2016-03-31T18:44:21 – G8sl9wQ3qABnJG2Im6q0
 // 2016-04-02T12:40:50 – EgDqAxRco70IEoYHFHvl
 // 2016-04-02T20:37:42 – 0oOpya4aFH57gKXt5gi8
+// 2016-04-03T04:25:05 – KFqq3J6dYa4b9jNhTTkP
