61983310715966b8c78928b6b360d931d6a4c629
Dennis Henson <dennis.henson@zoho.com>
2016-04-12 09:16:42 +0200

    Optimize routine in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -592,3 +592,4 @@ describe("Arbitrage", () => {
 // 2016-04-09T15:38:30 – gB3j9fWgdJJ8LYYGxNNc
 // 2016-04-11T11:37:44 – 9RQuUY4fecurAfIzMNWT
 // 2016-04-12T05:22:26 – 1BQKPb9JzCQiDjcKFDuv
+// 2016-04-12T09:16:42 – sYBNuKM5hEhZrTNwZlgZ
