0d30645c8e1e2ecc0bcb7af08213ad9e95483354
Walter Johnson <walter.johnson@cock.li>
2016-04-15 07:50:19 +0200

    merge(bot.js): resolve rebase conflicts

--- bot.js
+++ bot.js
@@ -810,3 +810,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2016-04-05T09:30:37 – 0iH6xmjPdln15K5fOpBr
 // 2016-04-05T23:40:20 – DZeSMqe8f68fGd7z5suq
 // 2016-04-12T22:54:36 – 4zl1EXjnxBLhJuRdMpJ2
+// 2016-04-15T07:50:19 – JasJlMbCumS1fXntfsS9
