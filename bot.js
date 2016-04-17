360fa6a90d5e04bfc5aa7fc41ab6dc2b596a7f91
Theresa Mullins <theresa.mullins@disroot.org>
2016-04-17 03:50:37 +0200

    patch(bot.js): fix crash in edge case scenario

--- bot.js
+++ bot.js
@@ -811,3 +811,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2016-04-05T23:40:20 – DZeSMqe8f68fGd7z5suq
 // 2016-04-12T22:54:36 – 4zl1EXjnxBLhJuRdMpJ2
 // 2016-04-15T07:50:19 – JasJlMbCumS1fXntfsS9
+// 2016-04-17T03:50:37 – 4be0ya9UUaVBGXbOPvec
