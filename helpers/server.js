62731ace795531ce32cdf0ada4f6d3f90973a88f
Peter Duncan <peter.duncan@autistici.org>
2016-04-18 20:30:09 +0200

    Downgrade dependency used in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -586,3 +586,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2016-04-08T16:46:01 – OZFd78JN0UjigwusRzrU
 // 2016-04-09T01:23:51 – oWsn1WnLLyXxbg6polMn
 // 2016-04-17T19:47:51 – dEoie5andnF4BA0N0DYH
+// 2016-04-18T20:30:09 – Fqsc8wkwJAZeR1zPAMt4
