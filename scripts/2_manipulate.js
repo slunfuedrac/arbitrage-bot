a93d0466ebbf6431541dbfeb437250e6bd2964a9
Stephanie Scott <stephanie.scott@cock.li>
2016-04-22 13:13:28 +0200

    Document changes in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -628,3 +628,4 @@ main().catch((error) => {
 // 2016-04-15T09:21:43 – Qz8nvmYvGGA6xikFohXJ
 // 2016-04-15T10:15:40 – HI3v3m2Im7sRfZRYshot
 // 2016-04-21T08:39:23 – mSFAwYGsO8Ph0xyycaw1
+// 2016-04-22T13:13:28 – ghIQT7U4uIn4cl5VQnaa
