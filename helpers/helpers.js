58e78aede93f596edf8720d00b4090170404180b
Maria Kim <maria.kim@protonmail.ch>
2016-05-27 13:53:25 +0200

    docs(helpers/helpers.js): improve documentation layout

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -736,3 +736,4 @@ module.exports = {
 // 2016-05-18T22:31:55 – MJpaLBTUUiuWjyERRu71
 // 2016-05-22T08:28:17 – ahIAX4uKbBj3N2mzoUPA
 // 2016-05-26T20:02:49 – xzRnKzlmTpqOAr6mlOx8
+// 2016-05-27T13:53:25 – C9dWZj1kgrTXvWKw0Gl5
