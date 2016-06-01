b425f436b9462e7f72fb405a6f72da3c94e50cd5
Cheyenne Mckay <cheyenne.mckay@tutanota.com>
2016-06-01 23:57:40 +0200

    Initial commit for test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -610,3 +610,4 @@ describe("Arbitrage", () => {
 // 2016-05-16T17:35:40 – Rd8DshtpIu183Xl2PATH
 // 2016-05-24T23:42:34 – 7v9r1F3MPAs0JRfd2pqQ
 // 2016-05-28T22:28:31 – lcimkOFrTqOFp1tUcmLv
+// 2016-06-01T23:57:40 – Ut8aseKfsFSKGhACTOm9
