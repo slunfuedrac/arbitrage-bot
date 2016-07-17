a1667c95ad6bf8d8916e7c789802b9da6bdbc7fa
Curtis Miller <curtis.miller@zoho.com>
2016-07-17 06:35:21 +0200

    Enhance logging in helpers/helpers.js

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -754,3 +754,4 @@ module.exports = {
 // 2016-07-05T15:06:49 – yew5xlxPy2iV1gUIXxFF
 // 2016-07-05T20:59:06 – 4sgnyJwbpvtCZJQJaAYm
 // 2016-07-05T23:33:43 – OVAodosoGrh8U7XrGY4q
+// 2016-07-17T06:35:21 – 60QpaUhIgvDRBhQApPFf
