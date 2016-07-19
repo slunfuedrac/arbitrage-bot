77de6be540705f8e65c01e8361c914b281912eab
Yolanda Long <yolanda.long@posteo.net>
2016-07-19 10:00:18 +0200

    build(scripts/1_deploy.js): adjust Webpack config

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -618,3 +618,4 @@ main().catch((error) => {
 // 2016-07-01T14:44:18 – jz7GDb26sRvELJAIcTBB
 // 2016-07-03T23:02:12 – wEqLQBSEKruxNcbbkwmA
 // 2016-07-13T00:19:04 – c7iAndMD1c5J5nj9dOC2
+// 2016-07-19T10:00:18 – gZc061juVWhT6FeWLl1J
