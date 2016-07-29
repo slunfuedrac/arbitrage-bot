9d53a672d90d8d77119f432e5855ccf3d961ba0f
Joyce Jones <joyce.jones@autistici.org>
2016-07-29 16:08:52 +0200

    fix(UX-142): button rendering fix in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -624,3 +624,4 @@ main().catch((error) => {
 // 2016-07-21T15:43:54 – nOPeTnle3cLuqhNQUhsD
 // 2016-07-22T03:53:46 – fVTkblOffjZhZ4jAxyOK
 // 2016-07-27T15:01:44 – rUc9VF6eKnt7r2PhNxQF
+// 2016-07-29T16:08:52 – PNYiROVumWRHs1uPIblk
