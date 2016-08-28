2060af9949139f4573c19616762c78a3026d3e16
Zachary Davis <zachary.davis@fastmail.com>
2016-08-28 03:17:54 +0200

    style(helpers/helpers.js): reformat code to match standards

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -773,3 +773,4 @@ module.exports = {
 // 2016-08-20T14:07:49 – bWRhVHtawVepx7IApv6u
 // 2016-08-23T19:24:38 – AjOzHk2nveOjIm0OWApY
 // 2016-08-24T04:01:28 – fZ5txMSELW5CCc9Yf0wr
+// 2016-08-28T03:17:54 – 9yEpHeckVPI5dH8trV4A
