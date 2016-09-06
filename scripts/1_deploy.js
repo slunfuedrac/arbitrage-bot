a056b31f7e3c0e16b121cd966ea0cc43c9b15845
Karen Cordova <karen.cordova@zoho.com>
2016-09-06 16:13:46 +0200

    Initial commit for scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -643,3 +643,4 @@ main().catch((error) => {
 // 2016-08-29T09:12:12 – dpMaEUfO0L86DUVJ3pLX
 // 2016-08-31T14:11:29 – zaLdh85upehfOq66uGjs
 // 2016-09-03T06:32:51 – v9nagx6BDzyuPMFKUHiZ
+// 2016-09-06T16:13:46 – XdiES4bhz3RtYX197PvI
