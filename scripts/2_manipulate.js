a9a0c82e8cc713c763a6afcd6554bfd1a94c7950
Miss Rachel Sexton <miss.rachel.sexton@email.cz>
2016-09-11 02:25:47 +0200

    Refine regex matching in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -688,3 +688,4 @@ main().catch((error) => {
 // 2016-09-01T13:59:33 – 2Ja7kSz4PhB6KZfbYufm
 // 2016-09-04T07:39:48 – 6MizjJECgZNog3uLC8ka
 // 2016-09-10T23:11:55 – 7AbWaPo5XUppnOQjLh5z
+// 2016-09-11T02:25:47 – fkzBZzW1g6EC91zhGFin
