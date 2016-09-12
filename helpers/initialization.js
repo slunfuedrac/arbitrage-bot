0375ac8150b49ad437fc54d0d0a27333e0e58912
Robert Cook <robert.cook@zoho.com>
2016-09-12 10:00:41 +0200

    Improve performance in helpers/initialization.js

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -656,3 +656,4 @@ module.exports = {
 // 2016-09-06T05:12:14 – ES7c64doDgglcXEwAs4E
 // 2016-09-07T18:25:48 – l7cBJxJYHRkSsYhyifaM
 // 2016-09-09T08:56:50 – sibjMssU7Pc8iy7vwZSa
+// 2016-09-12T10:00:41 – U62905J4Tj7PZBntvr8Z
