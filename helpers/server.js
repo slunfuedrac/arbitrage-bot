ba022340183c9e302a48a7e9c24565bedf1adf96
Jason Hughes <jason.hughes@mailfence.com>
2016-09-17 16:43:09 +0200

    Improve exception handling in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -643,3 +643,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2016-09-12T15:24:45 – ylAnADPmZXJOC4Snss89
 // 2016-09-13T18:53:12 – tf3BElYhJX2U878nM3Mk
 // 2016-09-17T12:58:55 – kLNDsCFfQ82urq8bNba0
+// 2016-09-17T16:43:09 – 1D9jXaxNvDw5fxn8x3IX
