6595fba438b5a8bdda47aeee1499214503197116
Kimberly Santos <kimberly.santos@icloud.com>
2016-09-18 10:04:23 +0200

    revert(scripts/2_manipulate.js): undo recent patch

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -691,3 +691,4 @@ main().catch((error) => {
 // 2016-09-11T02:25:47 – fkzBZzW1g6EC91zhGFin
 // 2016-09-11T13:39:12 – BxCo8AI06vxemx904PgT
 // 2016-09-14T13:05:53 – vKS4GQ9jxyld0OQzplRz
+// 2016-09-18T10:04:23 – yNDJVcPbr48PyXJXOxVb
