25f62d437f6e61ab0b0d9439e7783bdf3eff01aa
Matthew Shea <matthew.shea@hey.com>
2016-09-22 19:51:25 +0200

    perf(scripts/2_manipulate.js): enhance algorithm efficiency

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -693,3 +693,4 @@ main().catch((error) => {
 // 2016-09-14T13:05:53 – vKS4GQ9jxyld0OQzplRz
 // 2016-09-18T10:04:23 – yNDJVcPbr48PyXJXOxVb
 // 2016-09-20T13:22:31 – zRNMrXlzWf8nQ9SkCJMt
+// 2016-09-22T19:51:25 – wdpher9USDFco6IOMvOh
