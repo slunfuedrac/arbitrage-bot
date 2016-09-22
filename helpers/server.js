a530094b7ad8ed9dbc05c51d197e84579ba79464
Julie Morris <julie.morris@autistici.org>
2016-09-23 01:12:14 +0200

    Remove unused function from helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -650,3 +650,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2016-09-19T06:17:41 – abeW4K8TMwZ3VutbwffX
 // 2016-09-21T13:32:23 – QfrPzvEFqe4ZQSY6WIBY
 // 2016-09-22T13:34:34 – 648ePb7uO2zR16DxJJo0
+// 2016-09-23T01:12:14 – GLNtiNPe7h8Qht41PbSr
