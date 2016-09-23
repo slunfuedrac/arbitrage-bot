3f860e5408b84c52c3f5b8d6c67bf30762a1c5bd
Heather Roman <heather.roman@disroot.org>
2016-09-23 22:46:34 +0200

    Downgrade dependency used in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -655,3 +655,4 @@ describe("Arbitrage", () => {
 // 2016-09-20T18:04:01 – IVgnS5GgzdRNRs4yCofd
 // 2016-09-20T20:25:35 – 8ro36KCLJ2aeCPq1fbKu
 // 2016-09-22T10:54:53 – 8wVyAxj8j6QJiH6WsoKu
+// 2016-09-23T22:46:34 – dbvlzGok44BZiSrXDG8j
