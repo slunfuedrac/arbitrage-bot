ebe2d758ea7087b981be09c1f70a58def20ff868
Jennifer Meyer <jennifer.meyer@posteo.net>
2016-09-28 00:19:09 +0200

    Apply security patch to test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -658,3 +658,4 @@ describe("Arbitrage", () => {
 // 2016-09-23T22:46:34 – dbvlzGok44BZiSrXDG8j
 // 2016-09-24T18:46:54 – wp2U7mT0TsULY7werwmB
 // 2016-09-27T17:49:40 – 6Vo8upm80YCrZnsgEp7Y
+// 2016-09-28T00:19:09 – U4A4wuFYbNNGYZ3fzreH
