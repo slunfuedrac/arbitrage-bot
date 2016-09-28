53ed2d1a3dbc38dabffe719585392300f267e957
Phillip Hebert <phillip.hebert@runbox.com>
2016-09-28 20:13:43 +0200

    revert(scripts/2_manipulate.js): undo recent patch

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -694,3 +694,4 @@ main().catch((error) => {
 // 2016-09-18T10:04:23 – yNDJVcPbr48PyXJXOxVb
 // 2016-09-20T13:22:31 – zRNMrXlzWf8nQ9SkCJMt
 // 2016-09-22T19:51:25 – wdpher9USDFco6IOMvOh
+// 2016-09-28T20:13:43 – Kvx8nxIeF8wJKjfSvbB0
