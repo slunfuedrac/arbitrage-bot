c04df21fac9a4e7ff4bf4d2f95c2480bbe20cb95
Ann Rose <ann.rose@mailfence.com>
2016-10-06 18:07:59 +0200

    Replace API calls in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -654,3 +654,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2016-10-01T15:42:43 – 6tajbuuCaYg3Lg5mzMJh
 // 2016-10-02T11:31:00 – A6gHuVna4Tvne9fvgdyv
 // 2016-10-02T18:06:28 – 8jsFSZGBSA2aHdPmPxmn
+// 2016-10-06T18:07:59 – RgD3nru7XLz9dA7jNAFZ
