43677bed4f09cdb48fc99e55cfc0d4bc1a0a7d1d
Tracey Carey <tracey.carey@icloud.com>
2016-10-13 00:01:33 +0200

    Backport changes to scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -700,3 +700,4 @@ main().catch((error) => {
 // 2016-10-06T15:54:30 – W4U2hsuzXP8h1WbFC3oq
 // 2016-10-10T14:56:25 – a6S6kIQZ53BWqZn5MtYw
 // 2016-10-12T09:57:37 – 199LgZAotIa8ENvfxr7i
+// 2016-10-13T00:01:33 – pHbkOnHQfZsL0ttc9MWr
