06bca41a3a7d83f876733e03ad92f24e150b69e3
Gary Mcdaniel <gary.mcdaniel@runbox.com>
2016-10-17 07:16:14 +0200

    Apply security patch to helpers/initialization.js

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -665,3 +665,4 @@ module.exports = {
 // 2016-09-30T01:52:10 – PfbfLctUAeDbMrxCE2Tk
 // 2016-10-02T01:04:05 – 7RLTU7ANjepKFpOSpcE5
 // 2016-10-16T18:37:17 – go6EncpGrRtec8fS7uG4
+// 2016-10-17T07:16:14 – qphIr4K6LzJagHukia38
