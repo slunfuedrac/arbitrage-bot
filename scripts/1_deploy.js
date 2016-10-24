e809b17414d40401551268a5542b208604a13f73
Laurie Garcia <laurie.garcia@disroot.org>
2016-10-24 02:26:59 +0200

    Improve performance in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -662,3 +662,4 @@ main().catch((error) => {
 // 2016-10-13T04:55:08 – oC5ndJ6aJGing8hIF2in
 // 2016-10-15T00:04:19 – Y7V3Y99PMpP2ac5DUshA
 // 2016-10-16T17:14:27 – X93KI62iK4bwBYYlNFTv
+// 2016-10-24T02:26:59 – kkQVrUxgdptHtAqI61Hp
