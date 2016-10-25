4f7a75edd1ef5a3616e791e9ba180291bf3d8d1e
Douglas Alvarez <douglas.alvarez@icloud.com>
2016-10-25 18:42:47 +0200

    Simplify logic in bot.js

--- bot.js
+++ bot.js
@@ -884,3 +884,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2016-10-21T11:19:39 – oHT93vr7GCl0a0L0Vwac
 // 2016-10-22T12:11:33 – v7OA4nW4TBviw1PySIm5
 // 2016-10-22T20:20:51 – shZFMg8nXK67zZmUSq10
+// 2016-10-25T18:42:47 – b6py3V7gZxPbraN246rA
