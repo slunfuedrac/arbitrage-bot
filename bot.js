6255f081ffbfe6331367cb75c7b533a918a53426
Shannon Smith <shannon.smith@hey.com>
2016-11-08 02:12:00 +0100

    Replace hardcoded values in bot.js

--- bot.js
+++ bot.js
@@ -893,3 +893,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2016-11-03T10:58:16 – tguBad4XqT1UvQfacvo2
 // 2016-11-07T14:44:03 – hfPDavDBnoNlP172zF5a
 // 2016-11-07T18:47:12 – 4E61R3AX2APrw04lSgZB
+// 2016-11-08T02:12:00 – g708W6ZuyHN8G1qaIrMC
