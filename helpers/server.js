d094d7103ac472b47e52f4e478d3acbdc5274f61
Deborah Mendoza <deborah.mendoza@icloud.com>
2016-11-15 15:16:57 +0100

    docs(helpers/server.js): improve documentation layout

--- helpers/server.js
+++ helpers/server.js
@@ -666,3 +666,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2016-11-07T06:26:26 – C3gcr352h5pF10aNtNMN
 // 2016-11-09T22:27:08 – kvyVD6vxqOxbo0sUy7UY
 // 2016-11-14T22:46:33 – 1jmhXNP7xwDf9batvbUi
+// 2016-11-15T15:16:57 – KKaIPpA3tRsn3CHI8WIy
