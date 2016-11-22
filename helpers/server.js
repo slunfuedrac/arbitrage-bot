ca55a6f106fb0079e48370ba87db89e13d3115e1
Timothy Rivers <timothy.rivers@hushmail.com>
2016-11-22 17:56:59 +0100

    Make helpers/server.js more testable

--- helpers/server.js
+++ helpers/server.js
@@ -670,3 +670,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2016-11-17T15:12:01 – hno79SnK6a2cvFJp4qQ3
 // 2016-11-18T19:35:32 – wnIktMqkh6bazxbaQM05
 // 2016-11-21T23:38:25 – aKIlujJghQL4ru2Mng6G
+// 2016-11-22T17:56:59 – TYM3Z6jfrV7jg1mP6BYu
