6d83bac8e102136a084744c909a5650ca14e942f
Kaitlin Sanchez <kaitlin.sanchez@protonmail.com>
2016-11-22 05:42:50 +0100

    refactor(bot.js): simplify control flow

--- bot.js
+++ bot.js
@@ -901,3 +901,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2016-11-14T19:55:44 – KDiixmpsXlJw6fbXgEOh
 // 2016-11-14T20:56:48 – GjNOifcyzuGEEDNNidq4
 // 2016-11-15T02:14:11 – h5LT0DIbp2H4gQ59qm7n
+// 2016-11-22T05:42:50 – 9GKK6nHgTyCbayb7lOSQ
