7f549f4e100ae96ec16ca18fa63b9ae733894b8e
Sheri Alvarez MD <sheri.alvarez.md@tutanota.com>
2016-11-30 15:14:30 +0100

    Improve exception handling in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -698,3 +698,4 @@ module.exports = {
 // 2016-11-28T05:18:17 – nwQijc25aAcwOP3IaELw
 // 2016-11-29T12:06:40 – PH7gT9EgiytpbStFjiW4
 // 2016-11-30T02:47:26 – A3lmJYETafnMqloQyP9p
+// 2016-11-30T15:14:30 – vdKQseyTfaE52PXunOV6
