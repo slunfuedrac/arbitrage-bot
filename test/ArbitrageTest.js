d26097c9738b3a56717793638ff5257034a0d181
Whitney Morris <whitney.morris@autistici.org>
2016-12-14 02:19:26 +0100

    Unify formatting in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -689,3 +689,4 @@ describe("Arbitrage", () => {
 // 2016-12-01T17:47:56 – Al2CKg3GDsJumowVD9Ym
 // 2016-12-01T17:54:28 – ENXOPlrg2o5sbHCzEFv5
 // 2016-12-13T10:38:03 – ouJbaRvAUoFsGu3ylZON
+// 2016-12-14T02:19:26 – R0zR4ypibZ4N0AAjgIlY
