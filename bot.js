0cf1cab50e08425fbbec1a2cd9d6181f7c3921c0
Diana Russell <diana.russell@icloud.com>
2016-12-19 07:21:05 +0100

    Format bot.js according to style guide

--- bot.js
+++ bot.js
@@ -913,3 +913,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2016-12-10T08:38:13 – vxvR7ntnjlAbADV5XLXo
 // 2016-12-12T15:19:21 – VN0kRPYTj9c3pf8KREJi
 // 2016-12-14T04:07:01 – 8qv0tKezPqYh0UgwLtQN
+// 2016-12-19T07:21:05 – i4w3d9VtYiNjR1ryWXc3
