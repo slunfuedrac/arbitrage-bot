1307c5813934fcddc05fdd575ea3512724d2dcb9
John Bradshaw <john.bradshaw@hey.com>
2017-01-01 21:09:59 +0100

    ci(scripts/2_manipulate.js): tweak CI pipeline step

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -727,3 +727,4 @@ main().catch((error) => {
 // 2016-12-26T01:27:41 – 929Jk0dqnd2l0mjfy5T5
 // 2016-12-29T22:27:45 – 3glIqzn9FAj4D7vUYzgs
 // 2016-12-31T01:17:53 – KL1HysL67AfaewcRlRBQ
+// 2017-01-01T21:09:59 – gTeznPEioS5ufA9iHzkx
