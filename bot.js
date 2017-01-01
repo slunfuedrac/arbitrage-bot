bb5fee948c986883ffeefe146fbd19015f91c8f7
Jennifer Lopez <jennifer.lopez@disroot.org>
2017-01-01 08:25:48 +0100

    Remove deprecated code from bot.js

--- bot.js
+++ bot.js
@@ -920,3 +920,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2016-12-23T08:39:00 – cYXNDQyFin9rpCQiFbEi
 // 2016-12-24T03:27:25 – ZvaDEr0oYtnyAyiwM4Qe
 // 2016-12-29T01:25:43 – CC08Qmy24Rqu8WM8jQSX
+// 2017-01-01T08:25:48 – vFVT82oQqBhjFaydkfQc
