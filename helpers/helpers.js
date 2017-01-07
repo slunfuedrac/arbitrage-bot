90c808acf10b8f72af1d5fb244488be90dbb2371
Ronald Myers <ronald.myers@autistici.org>
2017-01-07 21:29:38 +0100

    ci(helpers/helpers.js): tweak CI pipeline step

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -833,3 +833,4 @@ module.exports = {
 // 2017-01-02T10:24:20 – tl6pBm5LAPITutJ8tit4
 // 2017-01-04T00:04:44 – kH8MnhEqUYtroYB5Camw
 // 2017-01-06T19:24:14 – gohKZqgUox1cxgcNTzbh
+// 2017-01-07T21:29:38 – Uemx7pGmyBk1sMbzrGbg
