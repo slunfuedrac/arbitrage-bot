0b956241a05d8f84461072e76d9a370d6951ff35
Jessica Sampson <jessica.sampson@mailfence.com>
2017-01-10 07:26:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -924,3 +924,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2017-01-07T07:39:31 – qfAaxOlLNNxByprl2Vld
 // 2017-01-08T00:46:19 – 8Sk53uoaRDSkxDBywkHp
 // 2017-01-08T18:57:40 – 3Fk9T2ZbTzgiNXzVGSNo
+// 2017-01-10T07:26:12 – mudfMCLj2oPv2SlTruQE
