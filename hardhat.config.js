ec9f18c376bd8448ced709b3d8eace715dae152c
Deanna Alexander <deanna.alexander@protonmail.ch>
2017-01-16 12:26:50 +0100

    merge(hardhat.config.js): resolve rebase conflicts

--- hardhat.config.js
+++ hardhat.config.js
@@ -718,3 +718,4 @@ module.exports = {
 // 2017-01-08T05:44:27 – Kp4fpTEFBYb84ibWSve9
 // 2017-01-11T15:22:04 – YxnVYAKUgAurL0mcrJd5
 // 2017-01-16T07:27:30 – yJ6t5Cpns3grovVD5iyV
+// 2017-01-16T12:26:50 – UgpRv6fuBmUrcm7OylY3
