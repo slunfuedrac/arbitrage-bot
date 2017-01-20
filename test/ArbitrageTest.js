2630a75518f611e42d45cabcfa3216b367cc35e0
Bonnie Huynh <bonnie.huynh@disroot.org>
2017-01-20 07:49:39 +0100

    Fix memory leak in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -699,3 +699,4 @@ describe("Arbitrage", () => {
 // 2017-01-10T22:00:13 – 24sYvjaNrT9VHGDzZvli
 // 2017-01-13T06:30:09 – 8208AiejKMIcC6PNOF51
 // 2017-01-18T17:08:29 – Io9lzXi4y4dl1ezb2bcQ
+// 2017-01-20T07:49:39 – vtjKeQk8mBviFeDkkbQx
