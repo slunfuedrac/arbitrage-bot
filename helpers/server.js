14c8c42c4067b772cf8959698a9d7b99500c6b49
Christopher Shepherd <christopher.shepherd@tutanota.com>
2017-01-23 10:14:37 +0100

    Format helpers/server.js according to style guide

--- helpers/server.js
+++ helpers/server.js
@@ -698,3 +698,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2017-01-18T20:40:41 – frpgrGI8hjnx09rT0Yv6
 // 2017-01-21T06:33:01 – xfLzyOKQTrAjUN5KQPmX
 // 2017-01-23T05:32:13 – BwYTOoUFydAjHQAF0GPo
+// 2017-01-23T10:14:37 – QUxTzE6dUpTJfzJp6YgV
