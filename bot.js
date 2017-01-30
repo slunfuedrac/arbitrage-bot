243d15914a742aedfea207c5a74ef7474e46be07
Michelle Smith <michelle.smith@autistici.org>
2017-01-31 00:12:30 +0100

    Improve performance in bot.js

--- bot.js
+++ bot.js
@@ -933,3 +933,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2017-01-29T21:02:43 – Ua0PpNv6YLsZ2vzxzYwC
 // 2017-01-30T08:42:28 – r5iqbsQX3E6zzjWcw0s9
 // 2017-01-30T12:58:19 – 0Md3uYHB7hEfidYvx3Ns
+// 2017-01-31T00:12:30 – NPrBvQt4a3T4vhFlyqdT
