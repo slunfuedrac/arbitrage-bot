31d8f048c3c42d0a261ea9650b69fe798f0cabea
Judy Jones <judy.jones@pm.me>
2017-02-02 07:06:23 +0100

    ci(scripts/2_manipulate.js): tweak CI pipeline step

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -745,3 +745,4 @@ main().catch((error) => {
 // 2017-01-26T10:29:04 – fJ8bMcloUV7Pe6enwbk2
 // 2017-01-26T21:50:54 – CWAncevZ5xhYSfW9UhsL
 // 2017-01-29T10:02:38 – O3eNh3BOyg72MFowvBvi
+// 2017-02-02T07:06:23 – 5Zo7aS59a7nuEmjvK4Bp
