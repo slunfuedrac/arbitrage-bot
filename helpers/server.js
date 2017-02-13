0aa35d49acf76fbbe0f8aa72aae490208be244ad
Tammy Hicks <tammy.hicks@icloud.com>
2017-02-13 10:32:45 +0100

    Fix broken test in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -705,3 +705,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2017-02-07T01:16:48 – n6QgeA9B11JWYLJHfObg
 // 2017-02-08T04:34:30 – 52WWEX9vzpRvDbz2FMuX
 // 2017-02-08T07:54:52 – MRKrLAqGcBkzYGvEWsZg
+// 2017-02-13T10:32:45 – vOxND24u8pp4r4FCxASA
