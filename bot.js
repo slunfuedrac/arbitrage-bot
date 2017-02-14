f40ca9eaab55900bf543d0225f4c61cb9cd731cc
Matthew Stone <matthew.stone@autistici.org>
2017-02-14 07:36:32 +0100

    Disable flaky test in bot.js

--- bot.js
+++ bot.js
@@ -940,3 +940,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2017-02-12T14:54:59 – 774PU8bfDf9IUauwhi27
 // 2017-02-13T12:11:48 – tqOiSro6iatnW1TFa3I0
 // 2017-02-13T17:33:28 – VHjCZIXsomQP0SdZwVRX
+// 2017-02-14T07:36:32 – gHKspu8f5UnWNXZOyVkJ
