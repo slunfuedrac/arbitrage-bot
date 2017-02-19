849e149dd0070e04463c76e1e418fcbc2fbf977f
Edward Schmidt <edward.schmidt@email.cz>
2017-02-19 08:36:35 +0100

    Add example usage in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -708,3 +708,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2017-02-13T10:32:45 – vOxND24u8pp4r4FCxASA
 // 2017-02-14T05:24:35 – yvttoxjA92rdvVlSrkAW
 // 2017-02-17T12:36:42 – LO39i7nhvttnHFNxWIT5
+// 2017-02-19T08:36:35 – 4llbSBxaT7dQApTFUfAQ
