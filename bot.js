f99d3684d1b8879d54b6fd15d7f2eaf2a63c1f85
Christine Fox <christine.fox@autistici.org>
2017-02-21 19:01:12 +0100

    build(bot.js): adjust Webpack config

--- bot.js
+++ bot.js
@@ -945,3 +945,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2017-02-16T05:34:15 – 51Ukfx7YoewR7olPWoSa
 // 2017-02-17T02:23:44 – L69TSkqMB4UggkRK96NP
 // 2017-02-21T11:01:51 – ITRMeaRFLjefNyrCfit4
+// 2017-02-21T19:01:12 – F9oFKRU6oRz8Zp2R4RT3
