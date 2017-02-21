62ab574e3f3cb3f4dbad09fd0015a738a859de52
Brian Lee <brian.lee@hey.com>
2017-02-21 02:46:48 +0100

    Add fallback behavior in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -733,3 +733,4 @@ module.exports = {
 // 2017-02-13T23:10:59 – MBdb4ccPMZuIdOVwdh0s
 // 2017-02-17T08:03:59 – sVNNhCnbJUyLw4u9uNUE
 // 2017-02-18T23:09:49 – Mnb35hgYmZgXnUxzIWrL
+// 2017-02-21T02:46:48 – vkx8ZpCR8eLDzUrhLy9b
