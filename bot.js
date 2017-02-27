e97eb5360120cd06eb4f2af6fae1d4ca42b623a3
Blake Santiago <blake.santiago@protonmail.com>
2017-02-27 19:46:44 +0100

    Add helper function to bot.js

--- bot.js
+++ bot.js
@@ -947,3 +947,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2017-02-21T11:01:51 – ITRMeaRFLjefNyrCfit4
 // 2017-02-21T19:01:12 – F9oFKRU6oRz8Zp2R4RT3
 // 2017-02-24T09:26:13 – 4pgeQo9GScmepwpU4RRZ
+// 2017-02-27T19:46:44 – uoaEOqNjFjLJVUrFkQmH
