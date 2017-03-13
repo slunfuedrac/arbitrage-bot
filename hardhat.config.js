49bc318ad9550a4789bb2763f06203bc89de8e4d
Haley Smith <haley.smith@zoho.com>
2017-03-13 10:03:20 +0100

    Add helper function to hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -748,3 +748,4 @@ module.exports = {
 // 2017-03-07T20:49:23 – GXl6XnbDBqVedAEMZyLh
 // 2017-03-12T18:40:34 – uNlOnhz3bzSQ8LH09okZ
 // 2017-03-13T05:50:51 – SPaHQwA9UAkkQhIJSvHz
+// 2017-03-13T10:03:20 – dXlnlTahvZcgrdAaaDRO
