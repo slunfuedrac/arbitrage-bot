71186ac8c525370ed3bec97f9fdad584bbe6de7c
Adriana Ward <adriana.ward@protonmail.com>
2017-03-15 21:41:49 +0100

    Optimize routine in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -711,3 +711,4 @@ main().catch((error) => {
 // 2017-02-25T14:02:39 – tw8rffXxEqMaKKcTzlBF
 // 2017-02-28T03:36:54 – zdKtux1R3D15fj6j7AY6
 // 2017-03-02T15:21:41 – CfRrRZh10nObhW044mxm
+// 2017-03-15T21:41:49 – gG9wm4Cts4lOJI6s6jiU
