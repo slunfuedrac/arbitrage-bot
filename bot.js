a7ef944e8178a167b60448e206cbf316f3f3f724
Brian Fox <brian.fox@posteo.net>
2017-03-31 16:58:37 +0200

    patch(bot.js): fix crash in edge case scenario

--- bot.js
+++ bot.js
@@ -955,3 +955,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2017-03-20T09:52:55 – 8ZQ2oGkpOsrCasiMCmrc
 // 2017-03-21T22:38:57 – dyKcbL3Oqo87GnR7pBNV
 // 2017-03-27T09:38:29 – pGMAUKDbLFKzhvAhROAE
+// 2017-03-31T16:58:37 – JLERQpYffuyVusrxVrRW
