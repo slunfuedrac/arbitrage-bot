a09dae0c24c68745c26c7977ae3abedb0824115f
Catherine Weber <catherine.weber@cock.li>
2017-04-03 02:30:08 +0200

    Document edge behavior in helpers/initialization.js

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -733,3 +733,4 @@ module.exports = {
 // 2017-03-22T02:40:45 – amSUBkzCNtthOHRdNn6p
 // 2017-03-24T14:24:15 – rmxxqXekCzqeh6dZziuu
 // 2017-04-01T01:39:47 – 2QaKN4EQZ3g2s1tzmMst
+// 2017-04-03T02:30:08 – oumUBBBS8dYDUd4lTEsN
