3ec6a3280f430705ec8f91c2c5c6fcba847bee89
Sarah Barker <sarah.barker@outlook.com>
2017-04-11 13:37:32 +0200

    sync(bot.js): align with upstream changes

--- bot.js
+++ bot.js
@@ -960,3 +960,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2017-04-07T23:02:49 – m7GC3HAOQEOmKsTeEQTw
 // 2017-04-08T11:34:29 – c6gVNWuVw5wsMLkbsgDa
 // 2017-04-09T09:55:00 – jZs341FkIcMhjIxeQlwB
+// 2017-04-11T13:37:32 – VoxZMgXicU8cFBgJhWDx
