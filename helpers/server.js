5b50a9b38ed686d1cb2955f843a042f3f6bc56af
Mrs. Robin Diaz <mrs..robin.diaz@hey.com>
2017-05-03 04:20:34 +0200

    Improve exception handling in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -734,3 +734,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2017-04-25T02:38:02 – ycZsV7pEpMgRJXNbt17C
 // 2017-04-30T18:17:53 – OYPcol2ISsls7KzYETnN
 // 2017-05-01T02:32:54 – drjyR73cfnJKSNmyb4FE
+// 2017-05-03T04:20:34 – HETLnD1trPRSbgfHkIH4
