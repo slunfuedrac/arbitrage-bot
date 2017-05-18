1c2eff4aa2fdc9657404080b5332ffc6ebc8b5d9
Jennifer Clarke <jennifer.clarke@tutanota.com>
2017-05-18 20:35:27 +0200

    rollback(test/ArbitrageTest.js): revert unstable release

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -747,3 +747,4 @@ describe("Arbitrage", () => {
 // 2017-05-11T20:50:06 – 3wY8kj5AlrzsJoG4NOwx
 // 2017-05-16T16:44:39 – KbS6NiBnXKJIvgyJlcqt
 // 2017-05-18T20:04:01 – KfpIBQx4zEQYIVSoV4LA
+// 2017-05-18T20:35:27 – zC9LP7V4CT9dWxeDVh8O
