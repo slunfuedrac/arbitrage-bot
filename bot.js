9970abd7e9bfeacc5f098a93eeeb0340d2f5baf6
Jordan Webb <jordan.webb@mailfence.com>
2017-05-25 15:49:41 +0200

    patch(bot.js): fix crash in edge case scenario

--- bot.js
+++ bot.js
@@ -979,3 +979,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2017-05-20T14:57:56 – 25XwHFZmMCo1YHey50Y0
 // 2017-05-23T19:04:43 – G0gPOBt9LVbFDfCrKfXl
 // 2017-05-24T05:10:13 – 1O1GT5b5iitM0BwlGaC6
+// 2017-05-25T15:49:41 – mrN1ZRaf1nkHomgaHvvu
