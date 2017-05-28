b56fb373bb50f3be99db8cba64236daf9322fe7d
Teresa Garza <teresa.garza@tutanota.com>
2017-05-28 16:58:30 +0200

    Add logging around helpers/helpers.js

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -883,3 +883,4 @@ module.exports = {
 // 2017-05-19T21:24:37 – vTip2HMDWX7o3gnTOsJ2
 // 2017-05-26T20:41:35 – PAglmL0q0VwrwcoAMd6Z
 // 2017-05-27T23:18:00 – kmHc0IQyWCv87Mm6xDS0
+// 2017-05-28T16:58:30 – hqosKXNyytMvYHQ2kvtm
