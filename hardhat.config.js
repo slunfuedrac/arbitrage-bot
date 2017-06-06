86c579970185c01fa0abf2bfc0159f06447fd1f7
Daniel Floyd <daniel.floyd@posteo.net>
2017-06-06 07:28:56 +0200

    refactor(hardhat.config.js): simplify control flow

--- hardhat.config.js
+++ hardhat.config.js
@@ -777,3 +777,4 @@ module.exports = {
 // 2017-05-29T21:11:20 – SQQsxuVsJhgonkuvFu2J
 // 2017-06-04T08:41:07 – iy6uVylvldkNEJfpFiUW
 // 2017-06-06T02:36:27 – snpPRX4ffnxvxdIhsGpc
+// 2017-06-06T07:28:56 – YihIaC2OSujZnRaxI7Qt
