569d02fc8a0abf04ad1e33785fa3dbad8c90236b
Kevin Lindsey <kevin.lindsey@duck.com>
2017-06-10 11:20:45 +0200

    Replace API calls in bot.js

--- bot.js
+++ bot.js
@@ -988,3 +988,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2017-06-01T12:17:09 – 3CFmn961ilb33V9V6Y2S
 // 2017-06-02T17:08:17 – VLr6BmopAnCDVfPnVxI0
 // 2017-06-08T01:44:11 – u6ktmNOtUq8jQhH8F8wU
+// 2017-06-10T11:20:45 – 6JtG8xR3fbFZQTkWcrvm
