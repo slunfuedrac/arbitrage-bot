144fd667fc8f6e577f32e207d3f9181042046402
Mark Cain <mark.cain@email.cz>
2017-06-14 03:01:37 +0200

    Apply patch to hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -780,3 +780,4 @@ module.exports = {
 // 2017-06-06T07:28:56 – YihIaC2OSujZnRaxI7Qt
 // 2017-06-06T10:51:52 – rn53wsmIw6UM7ihlXtWq
 // 2017-06-12T09:01:08 – UtjAH8ZCnwb7xGwTab7V
+// 2017-06-14T03:01:37 – 6KQjbzmOozHo1a4sdIS8
