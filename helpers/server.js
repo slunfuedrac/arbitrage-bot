fc64d35966958c2a01d989d5206b1a4b7489d89f
Ashley Smith <ashley.smith@pm.me>
2017-06-24 06:41:32 +0200

    Tweak configuration in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -754,3 +754,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2017-06-21T08:49:43 – f0uwszARzmM9ZE0GmjnQ
 // 2017-06-22T06:40:42 – bCyfIsrvlUnmyVJqPWvN
 // 2017-06-22T15:32:13 – KAzUK7skHioX6txbW5On
+// 2017-06-24T06:41:32 – dW2iRISTWIaFypjEUeAh
