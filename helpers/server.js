8522b55afddf1801a61af51a983c967376f562e3
Kelly Morris <kelly.morris@posteo.net>
2017-06-25 09:32:32 +0200

    build(helpers/server.js): adjust Webpack config

--- helpers/server.js
+++ helpers/server.js
@@ -756,3 +756,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2017-06-22T15:32:13 – KAzUK7skHioX6txbW5On
 // 2017-06-24T06:41:32 – dW2iRISTWIaFypjEUeAh
 // 2017-06-25T04:03:56 – G71aoqtcRQUhCZW0Qj2o
+// 2017-06-25T09:32:32 – Py8yP8Wdu4RQPlz6ivKF
