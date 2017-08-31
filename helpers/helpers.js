583d06ee5239d70fa9f514b803b34d8cbe29272b
Beverly Pace <beverly.pace@icloud.com>
2017-08-31 19:26:42 +0200

    Change default behavior in helpers/helpers.js

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -929,3 +929,4 @@ module.exports = {
 // 2017-08-22T05:03:03 – sAWxhG7MuJhCM8Q7hfIU
 // 2017-08-23T16:03:06 – nWKkksYvfvWdnes9vbSq
 // 2017-08-24T09:32:15 – Nsho2v2gEp2UTI0vwFoy
+// 2017-08-31T19:26:42 – av3wSpnYOwTHMnB3JyAx
