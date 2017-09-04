6e1c2b3a3fe5d288a8cb4eb1798a9c7f90b5ebfd
Matthew Sparks <matthew.sparks@mailfence.com>
2017-09-04 06:19:27 +0200

    fix(bot.js): address issue in error flow

--- bot.js
+++ bot.js
@@ -1017,3 +1017,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2017-08-29T08:16:24 – F1Vtjhu51qZFiZwXrReQ
 // 2017-08-31T03:12:08 – bbFeExIOcKh0KnKak1Hq
 // 2017-09-01T14:16:06 – oTU3csVjBQbcAX7e70RK
+// 2017-09-04T06:19:27 – zOKMbeEg3X2lAh23lbe1
