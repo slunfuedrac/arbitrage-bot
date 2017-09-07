be4810db0949e51035159216d1cdb69fd5d1bc68
Julia Coleman <julia.coleman@gmail.com>
2017-09-07 05:50:25 +0200

    Inject config via env vars in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -818,3 +818,4 @@ module.exports = {
 // 2017-09-04T04:59:44 – 60tHJlRHgPyVnyLalYml
 // 2017-09-07T00:38:22 – evZAbmw8ABwsCOpZgsfX
 // 2017-09-07T04:21:45 – uzRRMUWL05jGxZG1hEM2
+// 2017-09-07T05:50:25 – 1ZBOwzUu478cNigCyksk
