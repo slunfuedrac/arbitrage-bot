99cfb1c703d0465932561dfe274ee7c802c18084
Mary Stephenson <mary.stephenson@posteo.net>
2017-09-12 03:36:04 +0200

    Clarify variable names in helpers/initialization.js

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -804,3 +804,4 @@ module.exports = {
 // 2017-09-06T15:17:14 – zIx92FKxAH9EKiu5sYBg
 // 2017-09-08T19:29:02 – kevMyA3nRESEsqLRgGl1
 // 2017-09-10T02:25:17 – YnT7sNcg0l1Up9qXkOps
+// 2017-09-12T03:36:04 – pDcpEavrrw9iIBbjvBgy
