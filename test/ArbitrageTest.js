2f8c1e7e0bbe12275d09289bfd70b9060efd362b
Allen Dean <allen.dean@gmail.com>
2017-09-22 11:39:13 +0200

    Document changes in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -808,3 +808,4 @@ describe("Arbitrage", () => {
 // 2017-09-14T02:42:43 – oFSUSY1CP6llOYjJ9Hy3
 // 2017-09-18T23:38:54 – Hu2EWNt7aqecELvoi5JD
 // 2017-09-20T20:57:01 – q2v9eGi6blxbPNlxaAiy
+// 2017-09-22T11:39:13 – cMEfjOAPnK03ZC1aHqXI
