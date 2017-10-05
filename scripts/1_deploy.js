b40425e19535f276b589d4251741beea95c747c2
Emily Rose <emily.rose@mailfence.com>
2017-10-05 22:02:45 +0200

    Handle null checks in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -797,3 +797,4 @@ main().catch((error) => {
 // 2017-09-26T06:52:19 – 5zIFX0Cktm1sKNbY1gkp
 // 2017-09-26T13:06:49 – Yp5I8hu6J7pfVZl6gsbq
 // 2017-10-01T19:31:47 – PHNLhfaLEWCI5nXwtwVH
+// 2017-10-05T22:02:45 – X5fTB30u9Zqpfko7cjlP
