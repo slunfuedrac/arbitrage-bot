f98b63ed04b336e8ba7ecde367b65dd213a2a8d3
Teresa Chavez <teresa.chavez@protonmail.com>
2017-10-22 12:20:47 +0200

    merge(helpers/initialization.js): resolve rebase conflicts

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -820,3 +820,4 @@ module.exports = {
 // 2017-10-19T14:05:37 – 73L48cnQRV4k0XQ4bn1z
 // 2017-10-20T17:14:54 – yfjq2tVcsIYtaTiYWgoK
 // 2017-10-22T01:58:25 – b3vZgxSqdkxOPYji2EzU
+// 2017-10-22T12:20:47 – X3JhyJv2vLmbVNUXU1OS
