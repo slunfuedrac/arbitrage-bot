65444bd0eec5e9f82a3782c9e027a25b4a4e2034
Henry Sanchez <henry.sanchez@cock.li>
2017-11-14 19:44:32 +0100

    Port logic to new framework in bot.js

--- bot.js
+++ bot.js
@@ -1044,3 +1044,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2017-11-11T03:06:08 – 8nC3cJFJfZj7HDOK0rQL
 // 2017-11-11T12:13:56 – iOsc8qwFveH90n9sLIIv
 // 2017-11-13T01:26:23 – oI2pQVluzNFwRPa5lP2Y
+// 2017-11-14T19:44:32 – FectIkUuf5A2dioObMj1
