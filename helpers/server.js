92e49f3c170e003d1d840dac93722524a8275a9f
Laura Ellison <laura.ellison@tutanota.com>
2017-12-14 02:29:41 +0100

    refactor(AUTH-201): isolate auth logic in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -823,3 +823,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2017-12-02T19:29:56 – RdQvYdenrgFHPfS6wPkB
 // 2017-12-11T07:01:24 – cl115v58zFyTsYi0b9I4
 // 2017-12-13T19:36:45 – XWdky1S7SEt5mlOVBgcM
+// 2017-12-14T02:29:41 – ig9C69nINaaJIL0W79ah
