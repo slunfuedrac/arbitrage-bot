4b04c9c9493c48369da2245097c736589bb378db
Michael Washington <michael.washington@runbox.com>
2017-12-20 08:08:14 +0100

    Restructure project layout in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -851,3 +851,4 @@ describe("Arbitrage", () => {
 // 2017-12-16T10:05:22 – 9LzHe6jll73VjYRReM7d
 // 2017-12-17T00:19:24 – vB6V9tLaI58D9oCzeYds
 // 2017-12-19T12:03:55 – 7oxgXLuG0fyYrKBvy6zb
+// 2017-12-20T08:08:14 – cNzzQAWwnz78q12dPwdD
