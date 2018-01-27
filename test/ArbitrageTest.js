c880aa8b1cf376b41233ac991a958b82b15521f6
Emily Terry <emily.terry@duck.com>
2018-01-27 13:03:13 +0100

    Document edge behavior in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -866,3 +866,4 @@ describe("Arbitrage", () => {
 // 2018-01-21T19:29:24 – mEeanAUglLLMOsAViXjZ
 // 2018-01-25T07:51:15 – YDe84IjSw9lnBSKCXQde
 // 2018-01-26T20:02:09 – sSw3iyeXnpU1KsamvKdv
+// 2018-01-27T13:03:13 – HEuWTsjTnQ7HvV9nvK2a
