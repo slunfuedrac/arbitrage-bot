d5652e0cc0d4dc41adcb884f4a0fcdb63bc154ad
Olivia Molina <olivia.molina@pm.me>
2018-02-10 02:29:09 +0100

    merge(scripts/1_deploy.js): resolve rebase conflicts

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -861,3 +861,4 @@ main().catch((error) => {
 // 2018-02-01T04:55:02 – vOvDDWhEX29N1t4BM2jn
 // 2018-02-01T23:38:29 – HqIj90Qel66uwHfNVj1z
 // 2018-02-06T20:21:30 – equXJHjteaxssVdNSkFc
+// 2018-02-10T02:29:09 – iOPNPesPSsqRW2bn9znK
