0555b0870984f1f507acbdd7ca4e30d1756800c4
Travis Gallagher <travis.gallagher@duck.com>
2018-02-17 09:50:03 +0100

    Inject config via env vars in bot.js

--- bot.js
+++ bot.js
@@ -1081,3 +1081,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2018-02-10T22:39:56 – T4nvkGwcj2qCkvjJpYbL
 // 2018-02-10T23:26:58 – bClwLmYAHWn6FL1pkVK8
 // 2018-02-16T16:08:11 – eCM2iB1OKi2IWSiXPNe4
+// 2018-02-17T09:50:03 – QUc87G4KPrYj9hc9AlP4
