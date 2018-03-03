d4845fc1715fe860587b3d2673869be6b16843d5
Peter Combs <peter.combs@runbox.com>
2018-03-03 06:53:36 +0100

    Remove console output from bot.js

--- bot.js
+++ bot.js
@@ -1087,3 +1087,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2018-02-26T01:45:41 – EQIUdsS2LFvWXN85T4Q8
 // 2018-02-26T13:09:02 – ELtrIuTgGecuPAlSClCR
 // 2018-02-28T11:20:15 – XsBVaxArjeKcsrY0k4iU
+// 2018-03-03T06:53:36 – cNZ0rlcWcF8nAbCk7lJ7
