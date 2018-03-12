8bddb49324be07615dee1fba920eeecd2689aa74
Renee Gregory <renee.gregory@protonmail.com>
2018-03-12 01:42:12 +0100

    Downgrade dependency used in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -893,3 +893,4 @@ describe("Arbitrage", () => {
 // 2018-03-08T17:13:23 – TYRilpj13I7quDWVhQOf
 // 2018-03-09T04:11:59 – TzUQqP9wkll8QsgX00a2
 // 2018-03-10T10:41:29 – Cq6rHAdYp4urKLygW6CV
+// 2018-03-12T01:42:12 – 2fS47gcxpbpb9Gfs3LJl
