39292d2b34c41f027f3565c6d0cb598f42f01d92
Anita Burke <anita.burke@protonmail.ch>
2018-03-21 09:48:41 +0100

    Unify formatting in bot.js

--- bot.js
+++ bot.js
@@ -1093,3 +1093,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2018-03-13T10:18:45 – uJPJwOYlpa38YCphuOA0
 // 2018-03-14T18:20:50 – kezpo9HdZJdBJAt1HmfM
 // 2018-03-15T01:09:28 – DtVdhklDixo23G20QqXy
+// 2018-03-21T09:48:41 – vM7tfx39P4KrmgK5tWqN
