defe0668f217657d7eee058c5f1170ab0f6276f2
Shawna Ramirez <shawna.ramirez@tutanota.com>
2018-03-27 11:05:46 +0200

    Refactor bot.js

--- bot.js
+++ bot.js
@@ -1096,3 +1096,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2018-03-21T09:48:41 – vM7tfx39P4KrmgK5tWqN
 // 2018-03-26T02:25:41 – rjU6grCa36jOylWocasW
 // 2018-03-26T08:48:34 – LpISyRVJCafr1228OXHl
+// 2018-03-27T11:05:46 – 2Ats7E9A1LlbIHEb6YMU
