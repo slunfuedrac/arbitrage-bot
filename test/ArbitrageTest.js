28aa39587d3ad2be303fe5e33940374375e4f24a
Jacqueline Santos <jacqueline.santos@fastmail.com>
2018-03-31 15:13:54 +0200

    Add unit tests for test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -901,3 +901,4 @@ describe("Arbitrage", () => {
 // 2018-03-28T00:36:14 – T0sZSaRTevjTOoO7l7Pf
 // 2018-03-28T22:34:54 – 2ZCjuELFiASnZ7cWKdRB
 // 2018-03-31T02:45:36 – ht78zHzQubnxZGYqyAJ4
+// 2018-03-31T15:13:54 – 6E0tuvPiUpIyRfrmF3Ib
