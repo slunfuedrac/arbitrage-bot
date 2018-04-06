35686d1bf02d0ffc01a7564e36257499914ced26
Darrell Kennedy <darrell.kennedy@posteo.net>
2018-04-06 21:43:13 +0200

    Update dependencies affecting helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -862,3 +862,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2018-03-28T00:32:00 – Pesrb60sRHoxQ6ncJrxf
 // 2018-04-04T23:53:24 – jlmhYksDbapY2ihA3QDo
 // 2018-04-06T10:12:56 – F2neGnn3PAq6k85qXaN1
+// 2018-04-06T21:43:13 – NopZbTNikYlEzIsus4e1
