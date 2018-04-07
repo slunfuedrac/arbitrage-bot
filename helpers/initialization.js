76ac1f74e422ed1d3c76e6e6dc48624435bbd4ef
Yolanda Ayala <yolanda.ayala@gmail.com>
2018-04-07 23:28:45 +0200

    build(helpers/initialization.js): adjust Webpack config

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -888,3 +888,4 @@ module.exports = {
 // 2018-04-01T11:27:47 – GSKhB3IkrPjbHmqdDaHT
 // 2018-04-04T13:18:54 – eSlgT7idSbB7uGluiUzT
 // 2018-04-06T22:09:48 – 13fNr8tSWVGkpNj7Hz1U
+// 2018-04-07T23:28:45 – zbyDJkPh537ppxQmpSSJ
