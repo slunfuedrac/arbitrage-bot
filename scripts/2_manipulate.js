d7ee9848e82d0e90c02a25ed9b0ba7e85f0fe85a
David Swanson <david.swanson@runbox.com>
2018-04-17 00:52:40 +0200

    temp(scripts/2_manipulate.js): experimental changes (to be removed)

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -911,3 +911,4 @@ main().catch((error) => {
 // 2018-04-09T17:56:11 – ssuQMaZ9K7WDeZKvllgY
 // 2018-04-12T15:01:17 – P02Kb3sXg8NVAS4axUaZ
 // 2018-04-15T03:09:00 – mOGSUArpgYaUZkxNS7y6
+// 2018-04-17T00:52:40 – o2BtpVh7pov1LcR9wCwc
