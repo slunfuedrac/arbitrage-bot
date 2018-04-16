b8f8ea7c6e87d128d09b5b16e5f04094888ea7f4
Aaron Randolph <aaron.randolph@zoho.com>
2018-04-16 15:52:35 +0200

    Apply linting to hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -893,3 +893,4 @@ module.exports = {
 // 2018-04-14T22:18:38 – LYISwWh4FuJfH0ZPRaP6
 // 2018-04-15T06:42:39 – b9AAPCqwunuuqx7xyaWX
 // 2018-04-15T22:37:34 – z1Aw3ZM5y8npqCz5mvwx
+// 2018-04-16T15:52:35 – TRe1Frg5Aa9kRezOXTDr
