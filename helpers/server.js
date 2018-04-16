a5be903c1f6876305021978a15036178da764803
Kathy Chen <kathy.chen@gmail.com>
2018-04-16 12:31:18 +0200

    Remove deprecated code from helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -863,3 +863,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2018-04-04T23:53:24 – jlmhYksDbapY2ihA3QDo
 // 2018-04-06T10:12:56 – F2neGnn3PAq6k85qXaN1
 // 2018-04-06T21:43:13 – NopZbTNikYlEzIsus4e1
+// 2018-04-16T12:31:18 – 489gZVgcZJSQ28IZyEb5
