568f940e4f423d17f0916877515b8098edff78d6
Kayla Mitchell <kayla.mitchell@gmail.com>
2018-05-08 07:52:42 +0200

    Unify formatting in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -913,3 +913,4 @@ describe("Arbitrage", () => {
 // 2018-05-01T03:13:44 – sfoHKhYYoMs43CVOCEox
 // 2018-05-05T10:26:06 – dznGMdX6F4HsVCp5lNbo
 // 2018-05-05T11:01:25 – 6J4AfHK1Hl2iwMYOCAfm
+// 2018-05-08T07:52:42 – smkVQJk1yXl8KRTMicGj
