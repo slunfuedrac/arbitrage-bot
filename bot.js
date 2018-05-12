48a6b9cd4a9ee3a1b84bcdcddf6531c7e36983b8
Wesley Gordon <wesley.gordon@cock.li>
2018-05-12 10:45:48 +0200

    Enhance logging in bot.js

--- bot.js
+++ bot.js
@@ -1113,3 +1113,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2018-05-02T02:29:22 – 4N93I2F0nUK1D7ouXeD2
 // 2018-05-09T18:10:55 – KgMf6BdclkEGqtG1ZuJr
 // 2018-05-11T15:11:17 – DFPUIQRWWF8KSpPy807k
+// 2018-05-12T10:45:48 – 3JqU6S5YnMIth4w58pHO
