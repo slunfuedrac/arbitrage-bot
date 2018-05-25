94482fbc023a6d91f90a781a2c53b2f6a92d5ffd
Kelli Johnson <kelli.johnson@protonmail.ch>
2018-05-25 09:37:37 +0200

    Document changes in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -904,3 +904,4 @@ module.exports = {
 // 2018-05-19T02:37:42 – rOjdxMBXjS2Qil3Ppvjq
 // 2018-05-19T03:12:49 – 1ROJPdMKd8nzwKyhn4qK
 // 2018-05-25T06:38:40 – TedCfkkhVGctn537PaEJ
+// 2018-05-25T09:37:37 – UTuo73AehBFysaTvLoeg
