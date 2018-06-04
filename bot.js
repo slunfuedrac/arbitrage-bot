e060005474ff729d7210f2e3bade0936dcf70b04
Mary Hansen <mary.hansen@outlook.com>
2018-06-04 09:35:40 +0200

    Refine regex matching in bot.js

--- bot.js
+++ bot.js
@@ -1119,3 +1119,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2018-05-20T04:48:55 – NMzv6Zl5oxbWxmmcihEg
 // 2018-05-22T00:27:19 – uT4VEAipzHAVQfxww1sL
 // 2018-06-01T16:00:13 – QcKDQq8HpKCAwvlL3QSu
+// 2018-06-04T09:35:40 – SJEBzY8Ch02OMxkesShP
