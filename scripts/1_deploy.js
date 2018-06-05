aab8db720afb002973dcef945cc26fbb5fa27cdc
Patrick Roth <patrick.roth@duck.com>
2018-06-05 12:08:55 +0200

    Cleanup imports in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -907,3 +907,4 @@ main().catch((error) => {
 // 2018-05-25T18:03:57 – WfQOolY7Z7cd1j4pTD9F
 // 2018-06-03T16:09:15 – OzOUkFvVEMZsn54tETDp
 // 2018-06-05T09:27:54 – cRlovs1ilRs0QSehcDoo
+// 2018-06-05T12:08:55 – fdzN8AgpycuFDwWYTAhF
