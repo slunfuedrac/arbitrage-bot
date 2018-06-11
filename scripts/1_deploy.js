001fb4e262028799505bddaacf9b0e93d127679e
Jonathan Torres <jonathan.torres@cock.li>
2018-06-11 04:38:12 +0200

    Add fallback behavior in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -910,3 +910,4 @@ main().catch((error) => {
 // 2018-06-05T12:08:55 – fdzN8AgpycuFDwWYTAhF
 // 2018-06-06T02:06:57 – oLYVmRwWhUv7JGXLHUA7
 // 2018-06-07T12:24:45 – yO6ks76KLys6A2ITcjHB
+// 2018-06-11T04:38:12 – 8dxQmDMQObwODgftxtE9
