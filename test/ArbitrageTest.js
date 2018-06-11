6a1e59cb2d2678038ddd550d3f89442860263706
Eileen Villanueva <eileen.villanueva@fastmail.com>
2018-06-11 09:43:01 +0200

    Replace API calls in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -925,3 +925,4 @@ describe("Arbitrage", () => {
 // 2018-06-06T16:58:36 – cHQHLgCTuzRlTHt4enQY
 // 2018-06-06T22:21:05 – giGabtMs5tfuSfiYNox9
 // 2018-06-08T11:30:56 – hJ7U4SdGlZn17c8EbQvh
+// 2018-06-11T09:43:01 – B8xW2z9UcnNBzLFp5XIG
