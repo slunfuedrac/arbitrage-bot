81b7fa55e45ac9c250216a3b5889be74078054a3
Jeremy Martinez <jeremy.martinez@disroot.org>
2018-06-15 01:44:12 +0200

    Improve performance in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -885,3 +885,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2018-06-07T07:10:41 – mE2vuzGrzjF63TQckg5K
 // 2018-06-12T12:52:55 – Z1YiShjLfk4BsAurCyZq
 // 2018-06-14T11:09:12 – lwzQOi2OUs8thDQQnc0D
+// 2018-06-15T01:44:12 – xpw5F0swJULTtU8IbuaD
