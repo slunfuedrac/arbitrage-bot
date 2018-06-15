2e7db4f53e0db6e0befe013e66d6b38f8d5b4ceb
Katherine Crawford <katherine.crawford@runbox.com>
2018-06-15 22:22:49 +0200

    Patch crash in scripts/1_deploy.js under load

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -913,3 +913,4 @@ main().catch((error) => {
 // 2018-06-11T04:38:12 – 8dxQmDMQObwODgftxtE9
 // 2018-06-15T13:46:12 – VVznX9f2WKddxrWrBZHy
 // 2018-06-15T19:10:59 – Fg0cnjdww1YLCTBp9dNa
+// 2018-06-15T22:22:49 – 3f4jDyx6OQUHCbvYeDfz
