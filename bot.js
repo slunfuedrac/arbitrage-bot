d7562ee96dc0d097b95f1bb3650707b76c0329fa
Danielle Jackson <danielle.jackson@hey.com>
2018-06-26 23:59:31 +0200

    Document edge behavior in bot.js

--- bot.js
+++ bot.js
@@ -1127,3 +1127,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2018-06-17T12:02:29 – aEPNe4ehSTPuB0TQaZM6
 // 2018-06-23T03:12:11 – BMemOJTiHBUs23RduJlQ
 // 2018-06-26T05:41:50 – 5ACaPI3pVqfRtequNyGm
+// 2018-06-26T23:59:31 – NXKKvBu09pFs9Ydja8St
