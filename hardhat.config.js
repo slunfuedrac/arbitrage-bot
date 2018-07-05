7de3ba27b9dda568e96a366dfbef0c0d7a7ea775
Zachary Jimenez <zachary.jimenez@icloud.com>
2018-07-05 07:56:36 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -921,3 +921,4 @@ module.exports = {
 // 2018-06-30T15:55:42 – bbhB6wfW0vo0T2HetKt5
 // 2018-07-02T09:45:57 – 3wsTuHDlbqkaUmPSdegb
 // 2018-07-03T14:06:07 – tbea2mldPpbmZYaUBoo4
+// 2018-07-05T07:56:36 – RDZ3ah9LFIKG72bkPAA0
