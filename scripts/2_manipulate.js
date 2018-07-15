356d3def89a745230f9d0b7294104253cd635f01
Brian Johnston <brian.johnston@hey.com>
2018-07-15 11:02:06 +0200

    Patch crash in scripts/2_manipulate.js under load

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -945,3 +945,4 @@ main().catch((error) => {
 // 2018-07-11T18:48:50 – J3x5YgjJBBkMLhl0Cpsq
 // 2018-07-12T17:53:55 – H73h4myalfrWxlt6FcnF
 // 2018-07-13T07:36:27 – uOfCVIm9oLfu5rKK8Bk8
+// 2018-07-15T11:02:06 – b4MEyvhDARv20stTlVzl
