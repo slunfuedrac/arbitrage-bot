f432444e88f8e56aeb4201189c7b28fbc8fa8102
Samantha Madden <samantha.madden@hey.com>
2018-08-20 10:46:17 +0200

    Update dependencies affecting helpers/initialization.js

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -931,3 +931,4 @@ module.exports = {
 // 2018-08-08T10:42:08 – TMIE8yHoxiTPjrf3zAxn
 // 2018-08-09T23:09:15 – lY6pre8oPQwDZ5CfTvhu
 // 2018-08-18T03:24:20 – uzvl9hGSR0TssQCBeVtC
+// 2018-08-20T10:46:17 – aZoIDAgA9Awhz0hp70Q7
