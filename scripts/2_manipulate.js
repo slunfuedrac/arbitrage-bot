8fde6ecade16739065a5069cc6691931494ee9c9
Allison Fitzpatrick <allison.fitzpatrick@mailfence.com>
2018-09-08 04:28:02 +0200

    Refine regex matching in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -969,3 +969,4 @@ main().catch((error) => {
 // 2018-09-03T00:21:54 – ShngWHGxSXOmtkELjjwI
 // 2018-09-04T19:03:13 – V1hRR6LiAoW3XD09u8v2
 // 2018-09-06T21:00:58 – KUHlxa8VNDGVBbOqV5Ol
+// 2018-09-08T04:28:02 – AJrm3DvS4bqmFWqjgyyi
