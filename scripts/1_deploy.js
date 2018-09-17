8292c0243d54b4940655e8acdb13d03e9728638d
Alison Harris <alison.harris@email.cz>
2018-09-17 04:13:59 +0200

    Fix memory leak in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -953,3 +953,4 @@ main().catch((error) => {
 // 2018-09-09T13:44:06 – vgpgSMHAk4XzdlS5bw1V
 // 2018-09-10T03:30:15 – kOY88Te77IOhjadvzten
 // 2018-09-11T09:17:23 – C4XkGeHBGMCjyHjUNEcx
+// 2018-09-17T04:13:59 – 177ux91sAHANEu3cNHLX
