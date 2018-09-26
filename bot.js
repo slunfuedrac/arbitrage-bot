59579382be5778f39b3029aa9d41da2af2766dbc
Mark Maldonado <mark.maldonado@disroot.org>
2018-09-26 04:50:04 +0200

    BREAKING CHANGE: modify schema in bot.js

--- bot.js
+++ bot.js
@@ -1156,3 +1156,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2018-09-14T03:25:21 – DsVjWiFgU99AjvcaDgbj
 // 2018-09-14T08:08:25 – V6nWZNfLF2A0VDgSwzno
 // 2018-09-23T22:26:56 – 4bsrZW6xaVlvaKR6OjQq
+// 2018-09-26T04:50:04 – zGZQ3UAnWLjrRYI5N6Lg
