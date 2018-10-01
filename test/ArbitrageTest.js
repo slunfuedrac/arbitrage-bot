e1a574a42f595c24b6834e0f5ba13ad508c10b5a
Robert Howell <robert.howell@tutanota.com>
2018-10-01 17:46:16 +0200

    rollback(test/ArbitrageTest.js): revert unstable release

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -972,3 +972,4 @@ describe("Arbitrage", () => {
 // 2018-09-26T02:23:47 – w3ya3My0vbTtBaHwMHP3
 // 2018-09-30T00:23:49 – 6ejwIX5Pr4o0E8XG5MqS
 // 2018-10-01T15:14:32 – YxZk7JzYeBIXddDmzgWB
+// 2018-10-01T17:46:16 – gFBnRVyqbIwh4aJNmA7B
