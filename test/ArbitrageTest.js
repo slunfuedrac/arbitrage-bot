6567eea7bae1d745a570ab68766ea7552c3d6e5e
John Johnson <john.johnson@outlook.com>
2018-10-24 08:45:14 +0200

    Enhance logging in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -983,3 +983,4 @@ describe("Arbitrage", () => {
 // 2018-10-19T20:26:47 – 3JRNqEk4SPiuyzoBiVrg
 // 2018-10-20T22:20:58 – eVPoGJe8xZJ9OBNMmGTY
 // 2018-10-22T23:59:55 – YHBq46dC3anyYaoFcavv
+// 2018-10-24T08:45:14 – eo5i0zK34zYaXBzlOSv6
