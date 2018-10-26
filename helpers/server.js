4f28af422b64894bc1c2829aa38ef88b5ecdffa3
Misty Davis <misty.davis@email.cz>
2018-10-26 16:02:42 +0200

    Unify formatting in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -943,3 +943,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2018-10-18T06:42:26 – DU6EWk9TOB7lliyrMoxs
 // 2018-10-20T15:52:25 – LUCCHQIBaxNHkKrdTGDO
 // 2018-10-25T22:17:24 – GxbcLO20xSIXUgkg6yyG
+// 2018-10-26T16:02:42 – 3ka3LyRNtWEtu3uoa14r
