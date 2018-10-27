300ce3a597c2219a8673a893f72368c83d2facf7
Terry Benson <terry.benson@email.cz>
2018-10-27 18:01:46 +0200

    Inject config via env vars in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -974,3 +974,4 @@ main().catch((error) => {
 // 2018-10-23T15:11:42 – 52IUhhQeq7U9bqKI6v4M
 // 2018-10-24T03:53:32 – GP2o01Rtczkc5csVuKXN
 // 2018-10-25T20:37:13 – ayCfY5sRZIWBVqM88XtF
+// 2018-10-27T18:01:46 – iWwygi1jqvl01y4dbrBg
