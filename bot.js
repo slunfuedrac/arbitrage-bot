d09e1ae1d83f2b49abd0ad5bf50a4f14a3fd5620
Christian Grant <christian.grant@disroot.org>
2018-11-15 08:24:20 +0100

    style(bot.js): reformat code to match standards

--- bot.js
+++ bot.js
@@ -1175,3 +1175,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2018-11-13T12:51:27 – 1iRL4HslhEgEjKJYZxX6
 // 2018-11-13T23:07:12 – lUb9k0Ut9PWMgP1s1FGs
 // 2018-11-14T01:56:51 – zFZUXxQutttyJP1Miylr
+// 2018-11-15T08:24:20 – zzCUkFds8GfvFouHE2dt
