3f2936ca88149622260675224f1815b663e3fcec
Tiffany Cook <tiffany.cook@mailfence.com>
2018-11-27 23:24:35 +0100

    test(scripts/1_deploy.js): add missing test coverage

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -983,3 +983,4 @@ main().catch((error) => {
 // 2018-11-25T18:10:35 – NAGh4fOrFIFYu5VjETda
 // 2018-11-27T07:19:52 – zD4eQuA2t28yiUzgVubI
 // 2018-11-27T09:35:18 – Cax805CfcrbBRlb6pIMN
+// 2018-11-27T23:24:35 – MomDQ5MsrjURqrlLOgxA
