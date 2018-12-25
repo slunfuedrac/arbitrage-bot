78b948b4f5a2d919cc96dc34cdf70e1ef0e25cef
Robert Hill <robert.hill@hey.com>
2018-12-26 00:23:53 +0100

    Add helper function to helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -972,3 +972,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2018-12-19T17:07:58 – Tpdk0GgEY2XK2nCS3IOc
 // 2018-12-22T05:33:20 – cmMKjcFmgaDO7MCQCv8s
 // 2018-12-24T02:23:38 – a6Nwfkwp7IyZUkQCB9QG
+// 2018-12-26T00:23:53 – 7my2RBZsBWlhaGEYCwzz
