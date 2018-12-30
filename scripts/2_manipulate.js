2f8fbe41cd97a027f7bfdbc9bcb4be534d5d2fd9
David Silva <david.silva@icloud.com>
2018-12-30 05:50:27 +0100

    Simplify logic in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1007,3 +1007,4 @@ main().catch((error) => {
 // 2018-12-15T19:03:58 – TdJqY3Bm5nwBldEF3VeP
 // 2018-12-23T11:36:39 – kgnKBsc0u0DSVmuXQL3A
 // 2018-12-23T18:16:53 – Ew5ezeYdI4m8sdro1wTz
+// 2018-12-30T05:50:27 – LtYWQATZIkLli99qowzb
