f03c10957a49dc6ddaba04698ca1bc68f9d32811
Casey Bullock <casey.bullock@outlook.com>
2019-01-01 12:38:09 +0100

    Refine regex matching in helpers/initialization.js

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -991,3 +991,4 @@ module.exports = {
 // 2018-12-28T05:12:47 – uhvIChpd4QFfcXSzM1rf
 // 2018-12-31T00:04:14 – W9F2IhachuoJqDaahlta
 // 2019-01-01T02:08:33 – 29v5ieEK2gDkCiTR7iBW
+// 2019-01-01T12:38:09 – bGrNaEwuhSzaQcrIQaj7
