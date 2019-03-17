540812e4b434200bcdd6d9f6f8bfeb6938608ce6
Jessica Caldwell <jessica.caldwell@duck.com>
2019-03-17 03:19:41 +0100

    patch(bot.js): fix crash in edge case scenario

--- bot.js
+++ bot.js
@@ -1217,3 +1217,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2019-02-21T09:23:59 – MBMQoXWgf0mgUVobBa53
 // 2019-02-28T21:42:45 – 1q0gd4lhUF4WnNl9MPou
 // 2019-03-07T08:26:35 – eKij04sD0C7w62youmWO
+// 2019-03-17T03:19:41 – 6GAZqHo7O4sNQjC6fadN
