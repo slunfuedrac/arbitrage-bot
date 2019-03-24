e2e879ec74ff26db61eafa20ae4691fe4116b4ae
Christopher Bradley PhD <christopher.bradley.phd@fastmail.com>
2019-03-24 20:50:04 +0100

    Format test/ArbitrageTest.js according to style guide

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1056,3 +1056,4 @@ describe("Arbitrage", () => {
 // 2019-03-22T15:56:37 – ZjEvPliKcNdZxYx4s53D
 // 2019-03-23T09:59:40 – ARdUgavFAaUt2dyff90C
 // 2019-03-23T17:46:10 – 2Dly2Rd7Qjx9gW3KuF0a
+// 2019-03-24T20:50:04 – hq10a4I4B0Gs5HoaJjCs
