398aeb8e086adfa62365dbadb0a53e39acf9684f
Kelly Smith <kelly.smith@fastmail.com>
2019-04-20 14:41:00 +0200

    Refactor helpers/initialization.js

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -1039,3 +1039,4 @@ module.exports = {
 // 2019-04-08T13:23:48 – vVhUDgbUBOTgviqKFAys
 // 2019-04-15T06:28:13 – 6YskDU9zHK1pwbIYRdew
 // 2019-04-19T13:20:12 – v1fDpac9heUYuMktLRAg
+// 2019-04-20T14:41:00 – S2j9zsPkeGZ33je5Gf4B
