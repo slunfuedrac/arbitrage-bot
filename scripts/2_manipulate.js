5f228be0e47ae4240a6baf8e1fad3ab2eb3a8d45
Emily Cunningham <emily.cunningham@duck.com>
2019-05-04 17:37:21 +0200

    hotfix(scripts/2_manipulate.js): emergency fix for prod bug

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1056,3 +1056,4 @@ main().catch((error) => {
 // 2019-05-02T03:25:33 – uBj9zk8eXjehcd6melCj
 // 2019-05-02T19:24:16 – IbQSEziyQSijXnzNcJil
 // 2019-05-03T15:59:47 – yf390QC20vFxOoj94waw
+// 2019-05-04T17:37:21 – D11Ce9D8ZaLeWkgWrgIF
