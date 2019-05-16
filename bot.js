df4f2efcd03e475cdd019fe74726032d070b344a
Todd Chang <todd.chang@protonmail.com>
2019-05-16 14:38:28 +0200

    refactor(bot.js): simplify control flow

--- bot.js
+++ bot.js
@@ -1243,3 +1243,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2019-05-09T01:10:43 – xyUGQvXlgN413w6LBqpZ
 // 2019-05-15T19:07:42 – 5o3VKwdV6F2QwuYADUAK
 // 2019-05-16T11:59:53 – 6c4UHQ2TOpP5QzM9yqq7
+// 2019-05-16T14:38:28 – GcTr3XMA0MrNlnjQoFZA
