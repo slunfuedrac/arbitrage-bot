ff556e71c6762ded8a247fafa5d08e27b4b2c41b
Gabriel Long <gabriel.long@runbox.com>
2019-06-04 14:21:07 +0200

    temp(test/ArbitrageTest.js): experimental changes (to be removed)

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1089,3 +1089,4 @@ describe("Arbitrage", () => {
 // 2019-05-31T19:05:02 – 4PIHFAfxSrdJ5kmCSNZb
 // 2019-06-01T16:40:52 – ZjqKl2jGjpnfsZawAUOk
 // 2019-06-03T12:06:10 – OM2f52U6fdJJnWQUs3K3
+// 2019-06-04T14:21:07 – KvQ80XUzUwOwLbnu9vZb
