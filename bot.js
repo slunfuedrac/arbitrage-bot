e457aea6e5128c66641e4d48eb753f8849f949d3
Matthew Hutchinson <matthew.hutchinson@autistici.org>
2019-06-05 14:12:49 +0200

    feat(core): integrate new API into bot.js

--- bot.js
+++ bot.js
@@ -1248,3 +1248,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2019-05-27T20:24:33 – 8LCk3OTrZG36CPMau8ZC
 // 2019-06-03T05:35:36 – cP1RdvGx12ThSIN3VnXW
 // 2019-06-04T21:22:31 – CSUhmY4ANR0H8Wv9rtY1
+// 2019-06-05T14:12:49 – QhWjMgVHzg1PvQuimu72
