443116ebd78f4c525f1b9ccc7a7f441c9ae85509
Mark Lowery <mark.lowery@disroot.org>
2019-07-03 06:57:34 +0200

    Disable flaky test in bot.js

--- bot.js
+++ bot.js
@@ -1254,3 +1254,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2019-06-21T23:54:45 – ENwL1ZKJHXJGk6ua5ZdM
 // 2019-06-25T07:36:39 – XJHv7vPmv2cgLJDOT3bG
 // 2019-06-28T07:32:23 – bsk8ePfoUkg1umvIpRCB
+// 2019-07-03T06:57:34 – 2acVnru3hBIXBx1Kiocm
