840276bd9e3c0c72a89c760125e4a7d6f4137f03
Marco Jackson <marco.jackson@disroot.org>
2019-07-13 03:58:56 +0200

    style(test/ArbitrageTest.js): reformat code to match standards

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1100,3 +1100,4 @@ describe("Arbitrage", () => {
 // 2019-07-07T06:58:41 – fOwpnrQc1i053cnUQIp5
 // 2019-07-07T23:26:21 – kwVNYWLlZk2vWzpSkKyq
 // 2019-07-10T20:14:07 – L81QTTpntrPTQwbfu5iE
+// 2019-07-13T03:58:56 – xu4UfrJUlmY9hfMziy6w
