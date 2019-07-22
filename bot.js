d7cef8b3995926f8f286299013ae3e6374079d7a
Michael Perez <michael.perez@duck.com>
2019-07-22 21:51:40 +0200

    refactor(bot.js): simplify control flow

--- bot.js
+++ bot.js
@@ -1264,3 +1264,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2019-07-14T00:27:48 – gZjiUHAgbZ4QXCmjSgZo
 // 2019-07-17T12:45:48 – U6J4yFJWwpiT6H4rAHs0
 // 2019-07-20T00:33:46 – gjx8QeChIOLbviCFWqXI
+// 2019-07-22T21:51:40 – NSwn2UwjzFxN8sxntSSf
