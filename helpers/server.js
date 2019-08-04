e546dfa9208b3306652b8584894152a363896c80
Justin Herrera <justin.herrera@protonmail.ch>
2019-08-05 00:45:51 +0200

    Port logic to new framework in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1060,3 +1060,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2019-07-29T11:06:49 – ChGOgKz1YROCWgBNRAvg
 // 2019-07-30T12:04:18 – hubvZGpksa1pgtF3qEiZ
 // 2019-08-01T17:30:50 – KXCle3laOARvXQHtrNh7
+// 2019-08-05T00:45:51 – LWwW49pVzxBn1Mi1EtFC
