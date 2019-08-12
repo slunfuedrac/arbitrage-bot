24f24e96141702b3375d3263ec33cd37829cd813
Stacie Cobb <stacie.cobb@zoho.com>
2019-08-12 02:51:59 +0200

    Inject config via env vars in bot.js

--- bot.js
+++ bot.js
@@ -1269,3 +1269,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2019-07-27T18:25:32 – lwYnEAG6qRCeeaq6eOrK
 // 2019-07-29T18:32:02 – D8q0jWWmLc6U3y11koFK
 // 2019-08-06T22:52:07 – yATrdh9C9HoGYdhNIVaV
+// 2019-08-12T02:51:59 – 04itJOIBvno7C4QL8YkL
