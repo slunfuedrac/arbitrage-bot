160b148032619d0df2c3d50badcc2fec5a3fba8d
Nathaniel Charles <nathaniel.charles@icloud.com>
2019-08-12 05:25:58 +0200

    Refactor helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1065,3 +1065,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2019-08-06T20:16:18 – mWT4eAjzbjWyLHzFKNYM
 // 2019-08-09T12:12:59 – OSd8epYrk0GAxGRdW3w7
 // 2019-08-10T19:48:58 – YTns2bR4lOc9Kz9v9IyG
+// 2019-08-12T05:25:58 – Lu2vWYUpXGYj6wFEXSMi
