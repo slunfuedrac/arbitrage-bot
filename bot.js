26fb6c5dd8f8d4212c99d16d49965787d1f728d0
Nicole Owens <nicole.owens@protonmail.ch>
2019-08-27 16:25:58 +0200

    Add example usage in bot.js

--- bot.js
+++ bot.js
@@ -1278,3 +1278,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2019-08-18T15:08:14 – FyZ1h9829sosbavfZkEt
 // 2019-08-19T15:37:33 – RZpmZG8H3HYeVIPliUl5
 // 2019-08-22T08:24:37 – PjqA9N4U76zjC0WPeOPS
+// 2019-08-27T16:25:58 – Bw1X38aoBiLIx35rYSOv
