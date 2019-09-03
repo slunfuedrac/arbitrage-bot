285d233a3bcb89c67b04bca72cc22a6bd4dee650
Stacey Fletcher <stacey.fletcher@pm.me>
2019-09-03 11:25:55 +0200

    Replace hardcoded values in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1092,3 +1092,4 @@ module.exports = {
 // 2019-08-23T23:57:39 – gTTbTGFq5khzh40bqZrv
 // 2019-08-29T14:46:13 – RktavUlybrxrxdqEKxXj
 // 2019-09-02T20:44:26 – g8h3RddEY59HUqbk7HpH
+// 2019-09-03T11:25:55 – xtGCk6P7oQeJYH3XV847
