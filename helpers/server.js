b8ebcd9e659fde0abf0a82fd8ef075b8a91d70df
Sarah Robinson <sarah.robinson@outlook.com>
2019-09-05 17:48:17 +0200

    docs(helpers/server.js): improve documentation layout

--- helpers/server.js
+++ helpers/server.js
@@ -1072,3 +1072,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2019-08-25T17:17:25 – zFmFgrX2st6A9neCGTbG
 // 2019-08-29T00:33:26 – WeLzaQxh8bZFIUA4aC6h
 // 2019-08-31T16:13:58 – tDw2R5fYOcA6JLRsMmaF
+// 2019-09-05T17:48:17 – 4Azo7DXD6DSbXXY7JL9c
