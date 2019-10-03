69dc6463f8bb804f6e08a2e11ef5a02788c5f318
Tommy Jackson <tommy.jackson@disroot.org>
2019-10-03 20:09:03 +0200

    Refactor scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1116,3 +1116,4 @@ main().catch((error) => {
 // 2019-09-26T03:18:59 – ZwEmjyJgFgHb99h1bfLS
 // 2019-09-30T07:26:57 – 2jptJQmDt03TlXCM9lO0
 // 2019-10-01T16:12:12 – 9NsRRtKYqtZUQqcoKoAr
+// 2019-10-03T20:09:03 – 7SImHa0BAu7a6ypyBo7B
