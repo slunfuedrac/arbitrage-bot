100648f9ed5c017408a887837789421a697de1bf
Amanda Chapman <amanda.chapman@pm.me>
2019-10-04 10:24:32 +0200

    Simplify logic in bot.js

--- bot.js
+++ bot.js
@@ -1291,3 +1291,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2019-09-25T01:25:30 – RvZYr4RG4c08A2av0U2h
 // 2019-09-28T15:19:01 – pMnQwQTdZ2Amk9KzU36l
 // 2019-10-03T08:45:28 – ZwazlxSojZJ2KSx3jjBg
+// 2019-10-04T10:24:32 – Q5CWm4GBFEnkz7vsPbDs
