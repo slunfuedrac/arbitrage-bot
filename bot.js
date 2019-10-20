c373cab2289d0155e07f0b25fa6e9aa2e284806e
Ashley Fitzgerald <ashley.fitzgerald@zoho.com>
2019-10-20 14:49:20 +0200

    fix(bot.js): address issue in error flow

--- bot.js
+++ bot.js
@@ -1297,3 +1297,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2019-10-12T09:04:52 – How5D7izOavrEKrdAPor
 // 2019-10-14T18:26:35 – q99J3c8aPk9juMVRfWMW
 // 2019-10-15T21:07:56 – Rx1yGqGasgMi7G2b2KaC
+// 2019-10-20T14:49:20 – xOfp6nJcPKiPoH1oCNzs
