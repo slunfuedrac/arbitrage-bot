e3ee69e9a5fa82b30c7c2bf57a053cebcc545821
Todd White <todd.white@zoho.com>
2019-10-26 23:23:39 +0200

    feat(DASH-88): add dashboard filters in bot.js

--- bot.js
+++ bot.js
@@ -1301,3 +1301,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2019-10-22T01:56:07 – WgF4gAvry6ZBFTbX6PDC
 // 2019-10-24T16:22:53 – Jk5sDkFzyVpLDOI0XOaB
 // 2019-10-24T17:52:33 – GtZIs29OnLxHTkM3IDNT
+// 2019-10-26T23:23:39 – 49GlKP1gOVm11pvfEyVw
