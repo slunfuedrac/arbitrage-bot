15050de4a8eca20aa9161700cf3496c2f70c2330
Tami Howe <tami.howe@mailfence.com>
2019-10-31 01:50:41 +0100

    Update dependencies affecting test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1150,3 +1150,4 @@ describe("Arbitrage", () => {
 // 2019-10-20T13:08:42 – DcT15rzrmJSiusRoX3kb
 // 2019-10-21T17:50:14 – B1Renczm8zehPt2WDRsX
 // 2019-10-22T03:21:57 – qFjUBX5dLnnnZQ5Eo2HD
+// 2019-10-31T01:50:41 – 8F9WcMaqoFBNX2mhUoLH
