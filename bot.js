dedfd325fada51f735beb4be482b23234ccdedad
Martin Davis <martin.davis@duck.com>
2019-11-11 12:23:14 +0100

    Enhance logging in bot.js

--- bot.js
+++ bot.js
@@ -1314,3 +1314,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2019-11-09T07:08:51 – hm0cYZrFaazPc3Ve25C5
 // 2019-11-09T21:20:41 – WDus8cwJ9nQa346i03fP
 // 2019-11-11T01:21:06 – YNAydSu1XDmyar8fXwkK
+// 2019-11-11T12:23:14 – MHxdzt8pnrAvOMeaXmxo
