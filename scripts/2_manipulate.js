622ec40d498c24a97a9001b8cf75f042def70777
Nancy Livingston <nancy.livingston@fastmail.com>
2019-11-16 21:43:22 +0100

    Update dependencies affecting scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1141,3 +1141,4 @@ main().catch((error) => {
 // 2019-11-04T09:02:31 – 1C0deGD6aFUYIu7GWG5x
 // 2019-11-11T06:22:31 – I4bzezrPUa2jhlggkilR
 // 2019-11-11T18:26:39 – sCBgE3s2OLYkt3scsa0Y
+// 2019-11-16T21:43:22 – 7xVIkcIR9rPKEgGF2FNx
