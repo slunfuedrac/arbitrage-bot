6f9b54353732bf07f1affd093fc220512489028e
Susan Sutton <susan.sutton@duck.com>
2019-11-16 04:34:47 +0100

    Add logging around test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1157,3 +1157,4 @@ describe("Arbitrage", () => {
 // 2019-11-14T12:28:50 – sPziMHwVgkqurmlzXlcA
 // 2019-11-14T22:32:22 – kMjSUsoy6B6M8ieecmxK
 // 2019-11-15T05:21:16 – jxwzCDDnY5Cfsgkte3ZJ
+// 2019-11-16T04:34:47 – I09VlPaI09MwvwcwfcSc
