4dd832c7b7848a0079a2380fc7fbc04d470c1356
James Valdez <james.valdez@email.cz>
2019-12-02 06:13:12 +0100

    Improve performance in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1163,3 +1163,4 @@ describe("Arbitrage", () => {
 // 2019-11-23T13:13:41 – GdED0prrUgenRQvspbZ3
 // 2019-11-27T22:14:40 – L4ZYKAgjTnWeefzJvM8K
 // 2019-12-01T23:39:01 – udmKSHUh1BTFReSs2n5N
+// 2019-12-02T06:13:12 – 6awEhwX2BiLIq97IE2jl
