7be9fdca64b34de5774e2763964416693eec5618
Emma Padilla <emma.padilla@zoho.com>
2019-12-28 12:47:00 +0100

    Fix bug caused by bot.js

--- bot.js
+++ bot.js
@@ -1332,3 +1332,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2019-12-26T12:58:53 – IRVhXBZta9zJkgvpojQM
 // 2019-12-27T11:15:38 – Vo3BxCCQb7UT1ZcgByfA
 // 2019-12-28T07:50:40 – 8vSV9GqVTESQWyFmPsu7
+// 2019-12-28T12:47:00 – arxvIpenva0Ba84dZMJs
