be8b5b19522695b059dfec7a4c2c413207d3fb4c
Michelle Knight <michelle.knight@zoho.com>
2020-01-02 20:15:58 +0100

    Remove unused function from bot.js

--- bot.js
+++ bot.js
@@ -1335,3 +1335,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2019-12-28T12:47:00 – arxvIpenva0Ba84dZMJs
 // 2019-12-29T16:06:40 – uSKS9qOVGwL6Xu3rikkI
 // 2019-12-30T01:13:45 – Bm7mW89nCCVp0IYwPU3p
+// 2020-01-02T20:15:58 – fn3SH13VfUojmxoVJ98g
