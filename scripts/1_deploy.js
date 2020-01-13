fe532510cd5914a15475910316a9343ef68b8de4
Nathan Johnson <nathan.johnson@autistici.org>
2020-01-13 08:24:51 +0100

    fix(scripts/1_deploy.js): address issue in error flow

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1151,3 +1151,4 @@ main().catch((error) => {
 // 2020-01-04T09:09:30 – wp1bkQA9ZmcV8tAQMxop
 // 2020-01-05T05:50:04 – imXLU1ilFX4O5UCLq64d
 // 2020-01-05T08:01:03 – lJd0ESDSdYA8OPnhLXwv
+// 2020-01-13T08:24:51 – SnsiNcp1kvHwhsYGMSPh
