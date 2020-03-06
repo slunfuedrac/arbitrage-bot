2b8a1124bc23dbc4e08093f3b8fd23cd929c1be6
Peter Meza <peter.meza@hushmail.com>
2020-03-06 19:42:53 +0100

    Change default behavior in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1144,3 +1144,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2020-02-23T19:21:03 – D50Muh4O6qytwzWGkW71
 // 2020-03-06T00:08:37 – t08yBRuxxhIznJyR0j2s
 // 2020-03-06T15:36:20 – ZQqgCxemeNS73BxUnnNE
+// 2020-03-06T19:42:53 – pkIjvbcZUAcBn2nomWsw
