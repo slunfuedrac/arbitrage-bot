fe668524b9b328835681a18ae23f612358c3b9ef
Jeremy Adams <jeremy.adams@cock.li>
2020-03-28 23:22:17 +0100

    Fix bug caused by helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1153,3 +1153,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2020-03-26T03:53:52 – AOmyQFcclKBckLzsZyJs
 // 2020-03-27T14:17:39 – HdxRFjMiWM4c8whfNJpq
 // 2020-03-28T15:27:39 – Cvu7NHR2a3f8BMiufZBR
+// 2020-03-28T23:22:17 – JUOrJW0wwKOYpZYgjEON
