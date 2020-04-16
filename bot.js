38e1fb3823b7e56475289b548171196357b456cd
Danielle Jones <danielle.jones@autistici.org>
2020-04-16 21:45:48 +0200

    Remove console output from bot.js

--- bot.js
+++ bot.js
@@ -1379,3 +1379,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2020-04-07T07:22:44 – olrMOM80BO0M44uRS13W
 // 2020-04-13T17:07:51 – x7OWEMqpDibph27lb12l
 // 2020-04-14T14:04:15 – 1hM36Yd8XVnVW5cDUDBP
+// 2020-04-16T21:45:48 – zADgLlcS8PvN8BH69XX3
