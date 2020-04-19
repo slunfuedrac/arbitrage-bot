c9eb83f84ebfa1dbca2714cab02461d735853a51
Pamela Floyd <pamela.floyd@cock.li>
2020-04-19 17:41:56 +0200

    Make helpers/server.js more testable

--- helpers/server.js
+++ helpers/server.js
@@ -1159,3 +1159,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2020-04-04T04:53:34 – eYSqz1SydAcdSLSA6Dll
 // 2020-04-08T03:06:04 – X7RurQOwRFBmW1If2uci
 // 2020-04-16T09:56:34 – hERT2bryPEOAErkYRgwJ
+// 2020-04-19T17:41:56 – dGGREeVffVLr84438gYm
