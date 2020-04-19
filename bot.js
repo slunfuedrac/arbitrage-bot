aeaade5c8b08296851eb12b7465c37b03498ec79
Ana Jones <ana.jones@protonmail.com>
2020-04-20 00:35:25 +0200

    build(bot.js): adjust Webpack config

--- bot.js
+++ bot.js
@@ -1380,3 +1380,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2020-04-13T17:07:51 – x7OWEMqpDibph27lb12l
 // 2020-04-14T14:04:15 – 1hM36Yd8XVnVW5cDUDBP
 // 2020-04-16T21:45:48 – zADgLlcS8PvN8BH69XX3
+// 2020-04-20T00:35:25 – CbxFxiLulqRGLWDmxqQc
