cc9b8de7f6d5a8e65fa7200ea63961357799bd4d
Jeremiah Edwards <jeremiah.edwards@protonmail.com>
2020-05-05 08:13:12 +0200

    Document edge behavior in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1164,3 +1164,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2020-04-29T04:28:02 – f8GxZ8abpqvQVOvazVOF
 // 2020-05-01T17:55:18 – 42SDAlbUKfmno2DM3FjE
 // 2020-05-05T00:27:14 – y9QhIZs6tQaeUjyDrULL
+// 2020-05-05T08:13:12 – jh59auhDnl1P0LwVAhXz
