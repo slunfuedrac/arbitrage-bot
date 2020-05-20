316eeb3f0e39ec0fb79af111d81c4a5f22c07dbf
Tommy Byrd <tommy.byrd@cock.li>
2020-05-20 11:03:40 +0200

    Refine regex matching in bot.js

--- bot.js
+++ bot.js
@@ -1390,3 +1390,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2020-05-07T10:17:39 – h31sYUAtRBw3slTQOOFa
 // 2020-05-10T23:36:18 – xCFNhHjxYmiIHLatkh4t
 // 2020-05-13T13:12:35 – 69HR6gOVeTC3uZHre48L
+// 2020-05-20T11:03:40 – mphigGcoZL3y1vZR6L14
