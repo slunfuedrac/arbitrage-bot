c373ec03c0a1f7826358aa61ed8233011e988aab
Shane Holloway <shane.holloway@mailfence.com>
2020-06-02 22:18:41 +0200

    Remove unused function from helpers/initialization.js

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -1191,3 +1191,4 @@ module.exports = {
 // 2020-05-31T03:15:07 – bwcfhfEwDxLbMz4r9YTg
 // 2020-05-31T11:36:54 – ZRxdwDWelUCRBsQWMhq3
 // 2020-06-01T18:47:14 – 3AG0L8h0brUBkULptctk
+// 2020-06-02T22:18:41 – nLux0KgQOgpMcVExD4Hr
