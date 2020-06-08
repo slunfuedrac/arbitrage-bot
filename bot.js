8e1d352d56a1533576c890fc2c6dfdfe105e37ee
Nicole Hester <nicole.hester@duck.com>
2020-06-08 20:17:48 +0200

    Refine regex matching in bot.js

--- bot.js
+++ bot.js
@@ -1398,3 +1398,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2020-05-27T17:18:54 – Y7H9w8Lc53Obk0DD1krX
 // 2020-06-04T16:53:32 – ep6eaEOeXav9WyYeDk9t
 // 2020-06-07T16:29:29 – Mb6nJ9YFwZZYcVKmhWMV
+// 2020-06-08T20:17:48 – nms6uAQMz55wlk4X1Hcn
