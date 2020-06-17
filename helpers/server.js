4c29da1119464127ca5223e999f7953c709d8cca
Robin Gardner <robin.gardner@mailfence.com>
2020-06-17 13:32:39 +0200

    Backport changes to helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1181,3 +1181,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2020-06-15T20:20:06 – ZouzSK5YAZEPAnGVPCZl
 // 2020-06-17T04:45:35 – NadAGxAXGkBmjuz6tEkT
 // 2020-06-17T10:31:52 – jocMxa9ZPkrRODZ8TkqU
+// 2020-06-17T13:32:39 – 63IC3By1dEFxE1aOl6WA
