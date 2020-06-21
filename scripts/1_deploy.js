0f768f0326553067ef044c5202f134ef420275ff
Holly Newton <holly.newton@cock.li>
2020-06-21 18:25:11 +0200

    Improve readability of scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1225,3 +1225,4 @@ main().catch((error) => {
 // 2020-06-18T15:00:43 – 3wEaiq8Q8GpNbUbh3r5L
 // 2020-06-21T09:26:13 – XBt7pq1XribhQPHjMBuo
 // 2020-06-21T14:06:00 – EGMobsYKVdmAVL71sgmv
+// 2020-06-21T18:25:11 – Cbq2PHTSeRLDZVc0pKUv
