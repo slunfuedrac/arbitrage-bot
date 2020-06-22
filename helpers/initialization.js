450860454bccfa45cda6432786d6e93080c31dfb
Jamie Mayer <jamie.mayer@hey.com>
2020-06-23 00:19:47 +0200

    refactor(helpers/initialization.js): simplify control flow

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -1199,3 +1199,4 @@ module.exports = {
 // 2020-06-09T19:16:34 – egBQYDd7RSc66vS91op4
 // 2020-06-16T09:20:09 – 4yEc4ZpDryJmkz15tvtq
 // 2020-06-21T10:05:54 – JCZ3fpeKMDe0YhacCjdU
+// 2020-06-23T00:19:47 – y80FFRPCjnbgubUVlZNJ
