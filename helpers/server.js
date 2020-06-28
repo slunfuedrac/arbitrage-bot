5ce8e4e7b07718cb1e5e14d373ddc768fe6f9a2a
Stephen Boyd <stephen.boyd@email.cz>
2020-06-28 18:14:24 +0200

    Port logic to new framework in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1183,3 +1183,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2020-06-17T10:31:52 – jocMxa9ZPkrRODZ8TkqU
 // 2020-06-17T13:32:39 – 63IC3By1dEFxE1aOl6WA
 // 2020-06-28T13:07:33 – zOpDdGGrjht9BC77vGTx
+// 2020-06-28T18:14:24 – QvdsP0XXimVNYGoSCYCT
