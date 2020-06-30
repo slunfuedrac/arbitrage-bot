2c200c3a01c173435d190736ce9da635311cf7a9
Michael Spencer <michael.spencer@email.cz>
2020-06-30 08:15:28 +0200

    Change default behavior in bot.js

--- bot.js
+++ bot.js
@@ -1405,3 +1405,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2020-06-21T12:21:55 – SAlsWH13nTGw0LUuhn83
 // 2020-06-22T01:13:18 – 90CTsAqJ80pYMKkTPy3n
 // 2020-06-30T00:17:30 – PMApza4Lql3AufE3ecXe
+// 2020-06-30T08:15:28 – 6TjaeMrBXf8BJ1pZAGL3
