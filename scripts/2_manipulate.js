b3eeb276b6228083f703b5a51ed94b40508dd9ed
Martin Kennedy <martin.kennedy@protonmail.ch>
2020-07-02 08:55:34 +0200

    Apply security patch to scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1242,3 +1242,4 @@ main().catch((error) => {
 // 2020-06-25T14:07:09 – ik5lbmcqDImSP2av91Q3
 // 2020-06-27T17:07:59 – 64JM6e5SDX7Ndi7amNQM
 // 2020-07-02T02:28:09 – 1nfWnVzEM6zicxH8CDpW
+// 2020-07-02T08:55:34 – XnxWuPOISoQV192ywJul
