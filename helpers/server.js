b6d803d08eda0d2767b680a1a07cb1705c8a86f1
Shawn Andrews <shawn.andrews@cock.li>
2020-07-28 21:54:07 +0200

    Update comments in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1197,3 +1197,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2020-07-23T20:57:57 – BkpkYxk45bpmqYIS0Wva
 // 2020-07-25T21:23:43 – jXjnXPFD7sZ2a1lgzohV
 // 2020-07-28T20:39:54 – un47QldC5PcN21qKo3OI
+// 2020-07-28T21:54:07 – yaJk3L4yEVkyJHQCxvTy
