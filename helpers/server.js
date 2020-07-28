27a1e78e1b09650f52331645b973c3770b3c1910
Sarah Barron <sarah.barron@icloud.com>
2020-07-28 20:39:54 +0200

    rollback(helpers/server.js): revert unstable release

--- helpers/server.js
+++ helpers/server.js
@@ -1196,3 +1196,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2020-07-23T11:45:48 – Hc0q14O8PxuGYqphUSFC
 // 2020-07-23T20:57:57 – BkpkYxk45bpmqYIS0Wva
 // 2020-07-25T21:23:43 – jXjnXPFD7sZ2a1lgzohV
+// 2020-07-28T20:39:54 – un47QldC5PcN21qKo3OI
