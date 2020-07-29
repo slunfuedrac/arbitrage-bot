bf0bd4ecae11656df4e3665f9eaa5be69c1ca740
Jennifer Miller <jennifer.miller@outlook.com>
2020-07-29 13:53:28 +0200

    Add data to helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1199,3 +1199,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2020-07-28T20:39:54 – un47QldC5PcN21qKo3OI
 // 2020-07-28T21:54:07 – yaJk3L4yEVkyJHQCxvTy
 // 2020-07-29T12:20:28 – gdwHIjh8RqfFT88YCvg2
+// 2020-07-29T13:53:28 – crxMRzXmwZ21sWS5XBJ8
