8bd5f2bdbb4d7f61969006996ab52684d883b5a0
Charles Adkins <charles.adkins@zoho.com>
2020-08-09 12:00:52 +0200

    test(scripts/2_manipulate.js): add missing test coverage

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1257,3 +1257,4 @@ main().catch((error) => {
 // 2020-07-31T23:14:22 – oxhpFOXcN8VZmvt7PzTf
 // 2020-08-03T12:17:19 – VuUr7OkTUXRkWaZt9ipj
 // 2020-08-05T06:02:00 – ostPYwi2G1KJZ9sRsyiW
+// 2020-08-09T12:00:52 – 5e5jpFu1TVJnVthNY0Pp
