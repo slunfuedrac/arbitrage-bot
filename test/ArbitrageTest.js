7f02db8724690d78b23614703b6b0ae831f93f1c
Brian Mcbride <brian.mcbride@zoho.com>
2020-08-18 08:24:52 +0200

    Add logging around test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1247,3 +1247,4 @@ describe("Arbitrage", () => {
 // 2020-08-11T00:39:48 – 3sBgxJSVoCslxlD1z4UU
 // 2020-08-11T04:41:54 – rb7440atgwGdJiWhmIts
 // 2020-08-14T04:30:24 – KTUcG25pKgr9Zi4aL7PW
+// 2020-08-18T08:24:52 – qlV5LcwsUumoGSzwW26M
