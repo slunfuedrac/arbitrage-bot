4e2a3dbe409b5b81ed18974e18ddf48bb274f156
Matthew Schmidt <matthew.schmidt@mailfence.com>
2020-08-27 08:17:37 +0200

    ci(scripts/2_manipulate.js): tweak CI pipeline step

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1265,3 +1265,4 @@ main().catch((error) => {
 // 2020-08-21T12:44:26 – zSLGFB3r0fUa9bycHAJm
 // 2020-08-23T23:21:26 – JixLhsttCISBeLjEZ6L5
 // 2020-08-24T04:41:25 – WXluoOdFuoI0cxroHOUR
+// 2020-08-27T08:17:37 – QCyTigkm7R55FiiOqXjH
