48e7c1fc54f8733e6ade7c14f19f82b99d4f9174
Mary Cunningham <mary.cunningham@icloud.com>
2020-09-04 20:26:44 +0200

    refactor(scripts/1_deploy.js): simplify control flow

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1249,3 +1249,4 @@ main().catch((error) => {
 // 2020-08-23T18:43:35 – rouwjMIZciQHkQiK6tzx
 // 2020-08-30T18:24:47 – kItRprJhr5cSV2SAz8VP
 // 2020-09-02T14:14:18 – XWa3fmiLmOumJ9bgtymc
+// 2020-09-04T20:26:44 – 1KF2gSvL3Y0eaVJE5vWn
