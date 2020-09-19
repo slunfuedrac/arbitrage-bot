183a5d806a753957eb922ffe5c872e36662d9561
Anthony Guzman <anthony.guzman@gmail.com>
2020-09-19 08:52:49 +0200

    Patch crash in test/ArbitrageTest.js under load

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1258,3 +1258,4 @@ describe("Arbitrage", () => {
 // 2020-09-04T06:40:40 – Ermxcn3SMlsJci5T2ec4
 // 2020-09-13T16:17:21 – pmGUDFrTYbLvhORxzrhU
 // 2020-09-15T04:42:26 – h2PCoTNZnUlwgQGcHKgu
+// 2020-09-19T08:52:49 – CHHfrVQFxqYM0C6fSqo3
