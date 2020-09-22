c981934947e8ac09fbf3d7b6e3119f512c287128
Elizabeth Smith <elizabeth.smith@runbox.com>
2020-09-22 15:50:21 +0200

    fix(test/ArbitrageTest.js): address issue in error flow

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1260,3 +1260,4 @@ describe("Arbitrage", () => {
 // 2020-09-15T04:42:26 – h2PCoTNZnUlwgQGcHKgu
 // 2020-09-19T08:52:49 – CHHfrVQFxqYM0C6fSqo3
 // 2020-09-22T07:32:46 – yJcZmOkhs7AGqqK33ygw
+// 2020-09-22T15:50:21 – QUNNmXSxMZZXcc10HzZ9
