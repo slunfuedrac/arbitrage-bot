c837765c6db12775caf17d8fff8d89095c110dbf
Andrew Porter <andrew.porter@icloud.com>
2020-10-03 20:36:54 +0200

    Apply patch to test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1268,3 +1268,4 @@ describe("Arbitrage", () => {
 // 2020-09-27T15:44:36 – 898NOXMNzo5mGKdhp4kx
 // 2020-09-28T22:49:07 – 8BRQsYjADSyGMpvWOhe9
 // 2020-10-01T23:46:46 – VnGNUuNVQZaTaCvQeLqQ
+// 2020-10-03T20:36:54 – huLO1UXNnf2toxdZvmHF
