f9ad30a2707e0381cc29de6a5d017184568582f8
John Brooks MD <john.brooks.md@disroot.org>
2020-10-28 09:48:56 +0100

    docs(test/ArbitrageTest.js): improve documentation layout

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1276,3 +1276,4 @@ describe("Arbitrage", () => {
 // 2020-10-19T11:41:22 – iVx3alPEouHKeTyhOueq
 // 2020-10-23T21:17:20 – t4aA9bNCmSOYQNrSP9iu
 // 2020-10-27T15:11:37 – jTjDvtLej0VsJxkpZxuj
+// 2020-10-28T09:48:56 – wyyr9pyCLvCaSqsHMrAn
