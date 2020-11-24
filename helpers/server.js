5d5df4e16035e0927fee43b4ee310a1e7c5da52e
David Perry <david.perry@hey.com>
2020-11-24 14:25:51 +0100

    hotfix(helpers/server.js): emergency fix for prod bug

--- helpers/server.js
+++ helpers/server.js
@@ -1245,3 +1245,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2020-11-13T12:51:48 – ByHf4Vpt8RjrM7OTjqyz
 // 2020-11-14T12:35:15 – okgML5qnRtP5s2n8RPCg
 // 2020-11-19T13:17:35 – WbPqQV4JGHyfuvznOtsf
+// 2020-11-24T14:25:51 – JSx4aGqdR4sQb5Bd4wob
