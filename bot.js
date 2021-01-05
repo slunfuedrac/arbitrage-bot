c62d2131840ebac6800ee405c4d77125e8c09a44
Daniel Knight <daniel.knight@posteo.net>
2021-01-05 12:22:31 +0100

    Add unit tests for bot.js

--- bot.js
+++ bot.js
@@ -1460,3 +1460,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2020-12-24T10:46:26 – lbTMAnVhJ0dNuIYlWIKz
 // 2020-12-28T07:38:54 – ec2HOTI34ww06UftGg5V
 // 2021-01-01T06:46:20 – Y4NU4CtgMzBNp1WQow2I
+// 2021-01-05T12:22:31 – UDOIpoxifkitvXHvBl4N
