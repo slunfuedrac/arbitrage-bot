ac457a21814d8c7322e10747511bbd644c893e64
Jessica Byrd <jessica.byrd@icloud.com>
2021-01-13 17:53:58 +0100

    Optimize routine in bot.js

--- bot.js
+++ bot.js
@@ -1463,3 +1463,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2021-01-05T12:22:31 – UDOIpoxifkitvXHvBl4N
 // 2021-01-07T23:05:45 – RBrL9bYtyyZDOzWwt9yf
 // 2021-01-13T11:04:38 – BnMgDGWiOB9sp0hUR7x6
+// 2021-01-13T17:53:58 – tX3xG4OsXqB0AEOOp1cI
