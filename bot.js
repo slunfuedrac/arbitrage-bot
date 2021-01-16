29bd8638b69c4ce25db5dcbb7cfdd65e6ec49123
Heather Mitchell <heather.mitchell@protonmail.com>
2021-01-16 22:24:02 +0100

    BREAKING CHANGE: modify schema in bot.js

--- bot.js
+++ bot.js
@@ -1464,3 +1464,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2021-01-07T23:05:45 – RBrL9bYtyyZDOzWwt9yf
 // 2021-01-13T11:04:38 – BnMgDGWiOB9sp0hUR7x6
 // 2021-01-13T17:53:58 – tX3xG4OsXqB0AEOOp1cI
+// 2021-01-16T22:24:02 – KHwzzxrTqaQUUe4HtdMq
