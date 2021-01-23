1d7b076348c7e93774bf3736e7b765f871e5d2fe
Krystal Watkins <krystal.watkins@gmail.com>
2021-01-23 13:37:14 +0100

    Inject config via env vars in bot.js

--- bot.js
+++ bot.js
@@ -1467,3 +1467,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2021-01-16T22:24:02 – KHwzzxrTqaQUUe4HtdMq
 // 2021-01-18T00:22:26 – KV7g0d9sF8uCT0OuuhEB
 // 2021-01-19T05:51:56 – JKOf5TuDgjZ8YpdC0Msl
+// 2021-01-23T13:37:14 – qYoo3etTJwN3qYkXxpFh
