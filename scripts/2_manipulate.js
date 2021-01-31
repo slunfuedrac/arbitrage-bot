990e3f054ee45d418422e5a8c2d04548e35a3cb0
Andrew Owen <andrew.owen@duck.com>
2021-01-31 10:05:52 +0100

    Patch crash in scripts/2_manipulate.js under load

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1325,3 +1325,4 @@ main().catch((error) => {
 // 2021-01-25T18:28:11 – BUwozP9y8D4CYAQWAfBe
 // 2021-01-26T13:23:22 – lx52yAUB0bcTcdybvnoi
 // 2021-01-26T20:15:33 – ZNNMVOI9rocYfEn1mhub
+// 2021-01-31T10:05:52 – 3MACKAqjuGUP0p2HeNoa
