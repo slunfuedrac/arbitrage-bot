7a88baf2ad9f6585eec4572a2490edf7b670cee1
David Walsh <david.walsh@autistici.org>
2021-02-01 10:26:14 +0100

    Enhance logging in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1269,3 +1269,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2021-01-23T10:06:04 – ZLNkkROm3Ba5bNfHigwH
 // 2021-01-27T03:10:10 – u7DJtYrcC1Xx9fFTx0Hq
 // 2021-02-01T06:35:49 – VL0siv8XfPPpYSSO0rJg
+// 2021-02-01T10:26:14 – w36iIUTrnLJBc7A80TPA
