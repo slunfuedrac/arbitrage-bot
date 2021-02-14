efce0a5c283696a818b7f1b373344774af5cf8a2
Gina Lambert <gina.lambert@gmail.com>
2021-02-14 21:46:08 +0100

    Port logic to new framework in helpers/initialization.js

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -1289,3 +1289,4 @@ module.exports = {
 // 2021-02-02T02:19:09 – pocgHffOv6H1LnqU7ofO
 // 2021-02-07T12:23:14 – PDxiGJpPkodkz1qTB19j
 // 2021-02-11T18:56:55 – w7VpssFXn6Haq4TZT19o
+// 2021-02-14T21:46:08 – ogCtw5juJu5ngrrvjOsD
