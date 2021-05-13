87be7fd580450106cdade3be28318e54963ddae3
Tammy Rollins <tammy.rollins@zoho.com>
2021-05-13 23:16:14 +0200

    Unify formatting in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1323,3 +1323,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2021-05-12T04:35:19 – CTkEsnVTUpXIItKTgO6f
 // 2021-05-12T19:11:01 – BMrpFhz1UJuJwxOpoZ0L
 // 2021-05-13T06:37:07 – YWePIDHQ08D3Lbd9jbCe
+// 2021-05-13T23:16:14 – 6A1OTaOAWklESlgDrTNS
