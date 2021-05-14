143dbc9d4e04f0cc95c8cee894039dccf6afc112
Robert Robbins <robert.robbins@posteo.net>
2021-05-14 09:00:03 +0200

    Update comments in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1361,3 +1361,4 @@ main().catch((error) => {
 // 2021-04-21T16:39:32 – 2Nm1vybDmGxgxiHilx6S
 // 2021-04-22T19:30:03 – SWdErI7Hc7UurjzmyTok
 // 2021-05-10T15:28:30 – wLSy1ms0jCT3iPMZV3uN
+// 2021-05-14T09:00:03 – BjCJF8e7Uby6yn8pdrwc
