e162f36afc0f314ef8f8b8065a8faf971a250603
Donna Warren <donna.warren@hushmail.com>
2021-05-20 20:32:31 +0200

    Fix broken test in bot.js

--- bot.js
+++ bot.js
@@ -1512,3 +1512,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2021-05-14T05:21:29 – YuAuzzLIJR3XmJtu7xYo
 // 2021-05-14T17:53:38 – g5Lx0DlDfro6atClaMbC
 // 2021-05-15T12:52:24 – dlhhDbduHZR3wu4IQ8bf
+// 2021-05-20T20:32:31 – ds7WFHgh0UGOjiGlBjQu
