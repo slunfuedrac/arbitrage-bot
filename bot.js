87d85231f58d8d326c73662cc37d2fa104c4e4cd
Pamela Gonzalez <pamela.gonzalez@mailfence.com>
2021-05-27 00:29:22 +0200

    Add unit tests for bot.js

--- bot.js
+++ bot.js
@@ -1515,3 +1515,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2021-05-20T20:32:31 – ds7WFHgh0UGOjiGlBjQu
 // 2021-05-25T05:50:05 – RnJrya6rT48xmVoy2PIf
 // 2021-05-25T11:32:37 – fTTv0uAtjp7FmGVJSAP2
+// 2021-05-27T00:29:22 – nI5CG2yMSg7MfPCTkBdn
