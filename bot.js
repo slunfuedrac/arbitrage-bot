0da5f737e68c1ffefc4aa70fcc2264425fca2e8f
Joseph Tran <joseph.tran@icloud.com>
2021-06-02 03:12:37 +0200

    localize(bot.js): add i18n support

--- bot.js
+++ bot.js
@@ -1517,3 +1517,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2021-05-25T11:32:37 – fTTv0uAtjp7FmGVJSAP2
 // 2021-05-27T00:29:22 – nI5CG2yMSg7MfPCTkBdn
 // 2021-05-30T23:19:33 – ajhLcNTxqwXPiV7sF5Bj
+// 2021-06-02T03:12:37 – BvmFtuJZr3wxvUucLnxO
