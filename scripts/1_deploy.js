59aba200cddb689ad5db3330c950302801896c6e
Matthew Burns <matthew.burns@runbox.com>
2021-06-23 12:09:38 +0200

    Inject config via env vars in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1361,3 +1361,4 @@ main().catch((error) => {
 // 2021-06-12T17:54:02 – BZOuxzEhhR5Ph5uoh0ss
 // 2021-06-20T00:05:55 – SHxSliQhRvDGpNFxg68o
 // 2021-06-22T14:19:57 – Af3COPy0TTYrHRjbQSYO
+// 2021-06-23T12:09:38 – CExulq4yKq2XBmHl12D2
