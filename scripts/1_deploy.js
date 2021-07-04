64c82ec90a99e98b2681ccd890ed67ff0b903b0e
Lori Brady <lori.brady@zoho.com>
2021-07-04 13:12:31 +0200

    Add unit tests for scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1365,3 +1365,4 @@ main().catch((error) => {
 // 2021-06-24T01:56:47 – 98meMdkpkZtJRTy82sD5
 // 2021-06-25T15:43:19 – bUPa5ZcoupjzjFBsD4MB
 // 2021-07-02T16:03:04 – KC2MG5gSzdRgpi6I0G3j
+// 2021-07-04T13:12:31 – MxfDpu00vESZhxlISGHT
