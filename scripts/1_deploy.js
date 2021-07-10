1bf303dc2d41d11135a2a595c0a9021a27201851
Dylan Wilson <dylan.wilson@disroot.org>
2021-07-10 07:46:50 +0200

    Improve readability of scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1370,3 +1370,4 @@ main().catch((error) => {
 // 2021-07-08T15:30:36 – cpy9wmwmgLrgfZc7fSMz
 // 2021-07-09T18:24:05 – N1sjIIysw9KJIE6oGtfO
 // 2021-07-10T07:03:45 – EEy0XkH3BcCElr3kqeEs
+// 2021-07-10T07:46:50 – NBcFIS9kff3gfSuZUAFe
