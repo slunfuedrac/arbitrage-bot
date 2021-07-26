bdb6f07f68c170bb506a4ef59e9a664f4e81f123
Alexandra Smith <alexandra.smith@outlook.com>
2021-07-27 00:04:52 +0200

    Make test/ArbitrageTest.js more testable

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1385,3 +1385,4 @@ describe("Arbitrage", () => {
 // 2021-07-19T06:54:24 – l6VtiAify5kSmm1ipm8F
 // 2021-07-21T13:46:07 – abrXDOSsdidinStZJGZ7
 // 2021-07-25T18:37:28 – r7D0VfnJftEF16Hn3FHG
+// 2021-07-27T00:04:52 – 4HwP1IKp7qsIyzLEat7W
