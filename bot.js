203885564ff624484cd87d349b2d686e803455ca
Megan Fisher <megan.fisher@disroot.org>
2021-08-01 18:32:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1549,3 +1549,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2021-07-29T20:14:41 – jvrlLnS5Nsa4mwXfZ2Vs
 // 2021-07-30T22:23:31 – 6ZyI9HVE7MoaHPKG23qt
 // 2021-08-01T14:13:28 – MLjqGk33IVDjTnaJ1QPP
+// 2021-08-01T18:32:47 – CVU1f8j6NXUozt1pH9Ko
