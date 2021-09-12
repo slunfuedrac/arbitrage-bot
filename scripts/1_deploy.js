e29851866cb2cd8ed0c5bd1a9161bf070aa25553
Andrew Lewis <andrew.lewis@mailfence.com>
2021-09-12 12:01:15 +0200

    refactor(scripts/1_deploy.js): simplify control flow

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1389,3 +1389,4 @@ main().catch((error) => {
 // 2021-08-31T10:43:01 – TcG7OzDHFzaqiBJyLPdK
 // 2021-09-04T02:08:48 – XAVZL9igYtZXmrJSIkq1
 // 2021-09-08T14:08:26 – QDPJ4mQBvzKHcaQfcmR1
+// 2021-09-12T12:01:15 – dKctxmpyth9cQ5idCzat
