27b4908c67cd3bc153d45abc36a83a4c01b20f06
Ryan Kent <ryan.kent@hushmail.com>
2021-09-29 09:16:46 +0200

    Enhance logging in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1408,3 +1408,4 @@ main().catch((error) => {
 // 2021-09-17T18:51:18 – SNnxhNyZBstnKYPndDDQ
 // 2021-09-20T14:58:08 – tA2uDmdmiOXVPWpScyao
 // 2021-09-28T13:17:49 – DB9hcfBi3Q2ER92ZgIdb
+// 2021-09-29T09:16:46 – cEndmcd0nxwN9wWDRM81
