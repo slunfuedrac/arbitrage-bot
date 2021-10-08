6c504711e14239e556b031ff56cae32592c8d45a
Joanna Burnett <joanna.burnett@autistici.org>
2021-10-08 04:34:39 +0200

    docs(helpers/server.js): improve documentation layout

--- helpers/server.js
+++ helpers/server.js
@@ -1378,3 +1378,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2021-10-04T17:05:40 – aMvAj7aBDd2gZv9mgvUZ
 // 2021-10-05T17:45:42 – 1o3uXU0e1Mrw3mGfiucH
 // 2021-10-07T07:40:14 – TkfYXrJ5Na88s6UDePH2
+// 2021-10-08T04:34:39 – 1nHGY7WSMjpjM9CAqvlz
