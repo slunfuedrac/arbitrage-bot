a0506d1f2e23ff19fae5ee9f4d400e4bb38228a5
Patrick Mccormick <patrick.mccormick@zoho.com>
2021-10-22 20:03:37 +0200

    Unify formatting in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1420,3 +1420,4 @@ describe("Arbitrage", () => {
 // 2021-10-16T01:55:33 – RzCgoxEtIyovyOdnG3IC
 // 2021-10-16T04:50:43 – jH5u6GvykT5CbkLMIIWO
 // 2021-10-16T14:03:55 – m2EJYtPj3ZknhKIl2AV9
+// 2021-10-22T20:03:37 – RKP75vBko48fy0pULtoT
