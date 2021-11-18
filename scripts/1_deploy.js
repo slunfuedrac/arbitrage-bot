54bbb92285aa7bdd92ba49766120ec9ec269fe8e
Jennifer Hodges <jennifer.hodges@tutanota.com>
2021-11-18 23:06:01 +0100

    Remove deprecated code from scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1415,3 +1415,4 @@ main().catch((error) => {
 // 2021-11-17T03:52:30 – H4tcxp0BmU44rkXxCGSl
 // 2021-11-18T11:39:12 – mrq2JlqNQzwpYoQ55spI
 // 2021-11-18T17:33:50 – moO7EzGUfSmBqwut643k
+// 2021-11-18T23:06:01 – QE0yNEG8igsSOcbdwvVd
