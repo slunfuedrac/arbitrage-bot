2bce19eec2b327d578297a9655b55852bcc1d3c1
Katherine Martinez <katherine.martinez@autistici.org>
2021-12-01 09:13:45 +0100

    build(scripts/1_deploy.js): adjust Webpack config

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1419,3 +1419,4 @@ main().catch((error) => {
 // 2021-11-19T23:58:38 – eMhzZQ2TMthYxlcgnKlB
 // 2021-11-20T02:06:33 – i2SZc6OLWfjcqraPeyUW
 // 2021-11-21T03:12:20 – xz4bXEL1NphqcGXN4uUD
+// 2021-12-01T09:13:45 – JN9zk50K9OENw1nNDEXK
