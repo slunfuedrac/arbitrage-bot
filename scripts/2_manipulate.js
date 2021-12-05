86221e44959fcd43f84e50a289d44d95e1eee057
Melissa Higgins MD <melissa.higgins.md@runbox.com>
2021-12-05 13:39:50 +0100

    rollback(scripts/2_manipulate.js): revert unstable release

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1440,3 +1440,4 @@ main().catch((error) => {
 // 2021-12-01T02:17:30 – hzYDKWnjcWcc3ECEPELp
 // 2021-12-03T06:06:21 – atVyhhietEeSCjyLbpcd
 // 2021-12-04T09:38:43 – Lfj0bMom4aKUgGDenFT2
+// 2021-12-05T13:39:50 – VToS5RQgxcr5N1J6MsNC
