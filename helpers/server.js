a2e2c27adbf12d444be1047a1fc630c964d661fe
Lindsey Reeves <lindsey.reeves@posteo.net>
2021-12-12 11:16:49 +0100

    test(helpers/server.js): add missing test coverage

--- helpers/server.js
+++ helpers/server.js
@@ -1398,3 +1398,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2021-12-04T10:51:57 – 3gV48y8wkU7OBnV11tS9
 // 2021-12-06T05:38:27 – pjIWWtLQdpM2cAbfVvNE
 // 2021-12-11T20:11:56 – zNfT2P8FF80HRmQ88xfd
+// 2021-12-12T11:16:49 – NSMQr7vHmhg3TvHsPg4S
