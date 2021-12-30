4ee13da35b2076aeea9ae5cec79cbdf77d0228ac
Paul Rivera <paul.rivera@icloud.com>
2021-12-30 03:54:05 +0100

    Backport changes to hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1438,3 +1438,4 @@ module.exports = {
 // 2021-12-12T12:10:21 – Ii5zqqxiDxqjPKLnKvqq
 // 2021-12-16T01:48:03 – 4CtyXsHmJ5V9uf3GXHki
 // 2021-12-28T09:04:06 – zdmc9VmjvrKRFck2f3nV
+// 2021-12-30T03:54:05 – 3KNcvLLlSZ7WTaQoLMpQ
