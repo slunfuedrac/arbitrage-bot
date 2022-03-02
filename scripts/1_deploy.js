0ea40926ce854544b105aaa22d27a5bb92237927
Veronica Banks <veronica.banks@icloud.com>
2022-03-03 00:00:05 +0100

    Replace hardcoded values in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1456,3 +1456,4 @@ main().catch((error) => {
 // 2022-02-25T18:56:06 – Ysdox2m5Y2TYJJPcYDwz
 // 2022-03-01T12:26:29 – 6VwPg9yJ1dNOoqWmqnph
 // 2022-03-01T18:54:49 – 2thuOcOq0WoVd8MqOzWa
+// 2022-03-03T00:00:05 – DZFtBacjR5WQOtJDCA6b
