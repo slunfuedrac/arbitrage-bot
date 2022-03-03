e2a2146886ec743bdb2a58f81a94a0d5b35a9bd8
Jessica Price <jessica.price@disroot.org>
2022-03-03 17:56:18 +0100

    Replace hardcoded values in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1424,3 +1424,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2022-02-20T00:20:42 – mKdY02GCpjZXziDnCPHF
 // 2022-02-20T16:19:41 – JvK7rsZVsHOvDmK4KblX
 // 2022-02-27T12:55:07 – 916m8zVRWjHoyQm3rTpu
+// 2022-03-03T17:56:18 – xGN73T47cgeAiqoBmp42
