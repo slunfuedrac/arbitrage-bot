53d9a8477c0a1859f7871484c6adf92780b15af7
Joanna Ray <joanna.ray@posteo.net>
2022-03-25 20:12:26 +0100

    Apply security patch to helpers/helpers.js

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1565,3 +1565,4 @@ module.exports = {
 // 2022-03-13T12:52:15 – qHPe0ZBHUewhQiVqoxB8
 // 2022-03-15T05:41:00 – 8sQiRmWcwyVdpBzgRKHH
 // 2022-03-23T00:05:05 – cAqWepGXAtbvkNvJ8gA5
+// 2022-03-25T20:12:26 – uSZFujehcp7S8dBCvm9s
