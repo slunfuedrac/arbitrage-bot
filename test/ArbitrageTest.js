d6d0d4924d06e360cee02b0c88b946a2c2bb0635
Cindy Nelson MD <cindy.nelson.md@hushmail.com>
2022-03-27 20:58:43 +0200

    Make test/ArbitrageTest.js more testable

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1483,3 +1483,4 @@ describe("Arbitrage", () => {
 // 2022-03-24T07:40:42 – DdhU89OfkXGuPnty7xbs
 // 2022-03-26T14:11:24 – qjQVpbwS0MvVmar7TVze
 // 2022-03-26T17:35:00 – vkMEHmhSMot45uKGSIXy
+// 2022-03-27T20:58:43 – epyKjdMefIJkSVTsPG7k
