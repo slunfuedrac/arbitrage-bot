001efa5585dd42d6974b6e98df026570b2441a4f
Sue Thomas <sue.thomas@disroot.org>
2022-04-04 05:39:12 +0200

    Apply security patch to helpers/initialization.js

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -1466,3 +1466,4 @@ module.exports = {
 // 2022-03-28T15:08:52 – IpoTDPeMgwJUGpClU1V5
 // 2022-04-01T12:01:41 – ediwcTsoFeSQo76NkGaj
 // 2022-04-03T03:07:25 – 9G6qGufw4Yc0tj7qHaX7
+// 2022-04-04T05:39:12 – 3j7ys2e5wHJ7fJysRnpC
