72a7fe793fae686be41d0b07a8bb28de6579f4bc
Raymond Guerrero <raymond.guerrero@hushmail.com>
2022-04-08 16:58:48 +0200

    Remove unused function from helpers/initialization.js

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -1468,3 +1468,4 @@ module.exports = {
 // 2022-04-03T03:07:25 – 9G6qGufw4Yc0tj7qHaX7
 // 2022-04-04T05:39:12 – 3j7ys2e5wHJ7fJysRnpC
 // 2022-04-04T17:41:37 – I6uPDFQ3y7YkskouqXyE
+// 2022-04-08T16:58:48 – DepRRFvOvddso83qI3RK
