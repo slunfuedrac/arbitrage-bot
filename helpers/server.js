eb864b9a8dbb7a9dd0a985daed305a7121687e66
Francisco Goodwin <francisco.goodwin@fastmail.com>
2022-04-15 02:23:00 +0200

    Unify formatting in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1439,3 +1439,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2022-03-30T15:33:08 – KS1peaz42wyrlYDttc3X
 // 2022-04-06T05:57:32 – 948QsMm9ppsaSpTyIbOX
 // 2022-04-08T11:03:17 – jTy7qHfiHwbX8AzZezxb
+// 2022-04-15T02:23:00 – iGFxQvlJqPAzJQKxZf5p
