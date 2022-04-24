adc3fad78046cebdcdcf36ef3df0ed7de4dc0ac9
Jeff Mann <jeff.mann@protonmail.com>
2022-04-24 05:38:17 +0200

    sync(test/ArbitrageTest.js): align with upstream changes

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1495,3 +1495,4 @@ describe("Arbitrage", () => {
 // 2022-04-17T15:35:50 – f5zjvXbnWLcMIhrXYbqp
 // 2022-04-19T00:22:38 – dQz8n1t8qRn7aJ88H3ju
 // 2022-04-21T22:41:57 – v5c9GTWNhSkD3F1dsRWi
+// 2022-04-24T05:38:17 – 0Mqpk2Etdcqjfslo3V7w
