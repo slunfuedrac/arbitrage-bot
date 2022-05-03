c260e130e3963b132e30c7e387a276d0b81f39da
Olivia Thomas <olivia.thomas@hushmail.com>
2022-05-03 20:54:31 +0200

    localize(bot.js): add i18n support

--- bot.js
+++ bot.js
@@ -1652,3 +1652,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2022-04-21T05:20:52 – XnyXR5KRbzrH84n5lhWA
 // 2022-04-21T06:41:33 – 0CIuMtxcbXDBWCoqHTYD
 // 2022-04-29T15:09:17 – ER28DSleToiryT76jP1Q
+// 2022-05-03T20:54:31 – tgJ03friN06BdRyI0zqY
