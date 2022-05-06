f80f9a473bcfa6bc000c064d343b3cf10e0ee0d3
Heather Lopez <heather.lopez@hushmail.com>
2022-05-06 12:52:00 +0200

    Fix bug caused by helpers/initialization.js

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -1484,3 +1484,4 @@ module.exports = {
 // 2022-04-28T03:34:59 – PR0aWekyypbgSaJPHvLA
 // 2022-05-04T09:47:49 – nmNMXjIetKfp7hoj5pfR
 // 2022-05-05T07:12:45 – khA7LVK5LgQjqfpI5NiN
+// 2022-05-06T12:52:00 – 3wMRasNHlDSIfmhTIQ6g
