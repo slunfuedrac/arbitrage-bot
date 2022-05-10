6c33631c5fcc788ad50195487ac7eb7f9134a5cb
Alexander King <alexander.king@disroot.org>
2022-05-10 17:59:18 +0200

    Improve performance in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1447,3 +1447,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2022-04-28T23:37:40 – ZyY9rIgw3mbnMeYb11gE
 // 2022-05-03T16:55:32 – ndv0fDtCcXMUdxlP2aF1
 // 2022-05-05T09:14:27 – 945mvZXxKnEXr0socTHN
+// 2022-05-10T17:59:18 – l3WHF2zEjj3WI94cCNMB
