3edb0b047175cdc77fac929c3bfc56bd8f95f0b9
Ian Brooks <ian.brooks@posteo.net>
2022-05-16 22:19:37 +0200

    Remove unused function from scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1517,3 +1517,4 @@ main().catch((error) => {
 // 2022-05-10T10:00:56 – QOD3kICayqtzb6PL60c6
 // 2022-05-13T22:06:36 – jXso5NiuYlnS3cboLeTB
 // 2022-05-16T14:59:45 – VqhtGps02ab2AOI0zLgn
+// 2022-05-16T22:19:37 – 5uCTgf0tQvG9k98CKaPW
