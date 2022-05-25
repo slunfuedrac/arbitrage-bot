fd4e77480ee519ecfd5d1859739d1c680193bdbf
Roger Dennis <roger.dennis@fastmail.com>
2022-05-25 06:06:33 +0200

    Refine regex matching in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1493,3 +1493,4 @@ main().catch((error) => {
 // 2022-05-20T15:39:10 – gcNdslgf1Ctj3es1scIh
 // 2022-05-21T09:09:30 – cD5ZOYVMHJY3zsaCrRnp
 // 2022-05-24T16:55:40 – aLoAjNM4qO28pTWubtP7
+// 2022-05-25T06:06:33 – LgiWxq5xxsU1pemuY6yE
