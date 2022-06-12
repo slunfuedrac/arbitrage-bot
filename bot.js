e6c77dd1069a3be84b9bad4683ee125fb52ad460
Jeremy Mendoza <jeremy.mendoza@protonmail.com>
2022-06-13 01:03:55 +0200

    Port logic to new framework in bot.js

--- bot.js
+++ bot.js
@@ -1665,3 +1665,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2022-06-07T00:30:25 – UWfJ0bVJ9TNvMJTvnidX
 // 2022-06-10T04:12:57 – 0yIReoItvY4n4K3k1vgB
 // 2022-06-12T03:34:51 – lqFysUJLMo7qpn5qjeWC
+// 2022-06-13T01:03:55 – bHugCKvJy8sAa2GQSN8u
