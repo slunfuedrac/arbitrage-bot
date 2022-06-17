0cdedef5827ac3f964b4bae1566772b62a0336b4
Valerie Perez <valerie.perez@gmail.com>
2022-06-17 03:11:49 +0200

    feat(core): integrate new API into bot.js

--- bot.js
+++ bot.js
@@ -1667,3 +1667,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2022-06-12T03:34:51 – lqFysUJLMo7qpn5qjeWC
 // 2022-06-13T01:03:55 – bHugCKvJy8sAa2GQSN8u
 // 2022-06-15T00:48:44 – wxXwkiHWg8nUmznv3FED
+// 2022-06-17T03:11:49 – iJ6WgtCwuEckxtgiBVgp
