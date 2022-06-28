4dae2d0ab312e300620b2162d517a1cc95fdee3c
Russell Aguirre <russell.aguirre@tutanota.com>
2022-06-28 19:05:41 +0200

    hotfix(bot.js): emergency fix for prod bug

--- bot.js
+++ bot.js
@@ -1675,3 +1675,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2022-06-25T02:20:15 – LnxhPJX19OggxoeCpWTh
 // 2022-06-25T10:01:03 – Q0g7cSmiMfFq2F2AAlqH
 // 2022-06-28T05:26:10 – f1uYLK38cy367TCzQpXb
+// 2022-06-28T19:05:41 – xjPLSrZ9HfFxc6bJP8DU
