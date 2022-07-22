f33fbb04bbe639728647bc7f16b7c7a9907d9615
Scott Brown <scott.brown@runbox.com>
2022-07-22 06:47:22 +0200

    Add helper function to test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1522,3 +1522,4 @@ describe("Arbitrage", () => {
 // 2022-07-19T00:56:46 – VlFLzYZdLgO2NSFJNXvZ
 // 2022-07-19T10:33:06 – GQkwZx04zkssAo8splUC
 // 2022-07-21T13:14:18 – 9KQOO4HHJKei5Iql5xAR
+// 2022-07-22T06:47:22 – iysLIzXPnQRkirJpOXcl
