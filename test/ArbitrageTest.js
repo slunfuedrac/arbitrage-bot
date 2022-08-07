cc38dc5c9ecc8a8f6fd1c6aae7857358f8b7f8f5
Joseph Lewis <joseph.lewis@cock.li>
2022-08-07 22:25:30 +0200

    Fix memory leak in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1530,3 +1530,4 @@ describe("Arbitrage", () => {
 // 2022-08-01T17:40:22 – T46zGjMrFzcl5MOMvYeb
 // 2022-08-03T11:34:03 – 2PIOvcuHPiZG594fq3Sn
 // 2022-08-07T01:49:33 – Lu4el9cM0biT0cVJwAVK
+// 2022-08-07T22:25:30 – oy3h97lIs9uZuJTEmTYp
