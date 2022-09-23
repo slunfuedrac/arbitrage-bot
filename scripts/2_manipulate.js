3f5b1ce5d25f74b306cfc404f19f9ceb93f16248
Randall Myers <randall.myers@fastmail.com>
2022-09-23 10:38:13 +0200

    revert(scripts/2_manipulate.js): undo recent patch

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1573,3 +1573,4 @@ main().catch((error) => {
 // 2022-09-18T16:02:33 – egAsVfmG1vDL8VXea7iZ
 // 2022-09-22T05:20:48 – Gzx9vstf3VfULBxzuXuo
 // 2022-09-23T07:00:57 – VjWZpN2No5nl2QMBQNK4
+// 2022-09-23T10:38:13 – yLqrAZZSlzot0xDqNAar
