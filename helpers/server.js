8fc1fb75337bec683641f6487f138272fae09e6a
Shannon Alvarado <shannon.alvarado@disroot.org>
2022-10-06 09:42:50 +0200

    Fix bug caused by helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1509,3 +1509,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2022-09-27T22:27:10 – biY6FEyWZFTYy3aXxjRO
 // 2022-09-28T20:01:53 – dHNPwa55ALzmWsuHxLjM
 // 2022-09-30T00:30:21 – YPnDHivpJhPj84EAPY3O
+// 2022-10-06T09:42:50 – 2mum2KVl0iLf85kK9CJY
