d40d435bbd63c8ac03557b43951a1dfd09f20a3c
David Jones <david.jones@runbox.com>
2022-10-08 10:59:33 +0200

    Initial commit for scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1582,3 +1582,4 @@ main().catch((error) => {
 // 2022-10-04T02:49:39 – pWAmNzfwp4rpumb0MAkl
 // 2022-10-07T01:44:36 – UYNu3SJwxQ3Hd4ExcGGd
 // 2022-10-07T04:56:09 – qSTzZGS0dYuSdvW2t5Ff
+// 2022-10-08T10:59:33 – 8qN7w7imOwPE8mxrVo0j
