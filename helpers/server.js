95145e19d3f4b19c327295c0a3756a421bfdfd85
Amy Alexander <amy.alexander@duck.com>
2022-10-08 11:57:49 +0200

    Patch crash in helpers/server.js under load

--- helpers/server.js
+++ helpers/server.js
@@ -1511,3 +1511,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2022-09-30T00:30:21 – YPnDHivpJhPj84EAPY3O
 // 2022-10-06T09:42:50 – 2mum2KVl0iLf85kK9CJY
 // 2022-10-07T04:19:13 – i2uSP6oEcwft7r1Su9Zt
+// 2022-10-08T11:57:49 – wDQfpgMKgRSN03vpe62u
