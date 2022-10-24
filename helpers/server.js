c31dc9c210e98767655b0d8f85d3b3d39739156d
Dylan Harvey <dylan.harvey@protonmail.ch>
2022-10-24 21:23:46 +0200

    Unify formatting in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1518,3 +1518,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2022-10-17T00:59:00 – vN1Jp29SFeCH81a7qLif
 // 2022-10-17T16:56:04 – nqsvsXJo8zvJlJxb0DOy
 // 2022-10-17T22:11:56 – 5LnD3zQZN0Pel8rq259h
+// 2022-10-24T21:23:46 – PALTMGW40FV2XZ630vAt
