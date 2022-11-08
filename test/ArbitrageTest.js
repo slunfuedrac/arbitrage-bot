13409fe9c80dc3fb82d3a7eccc6ea7d13ce306df
Katherine Davis <katherine.davis@duck.com>
2022-11-08 23:53:34 +0100

    Apply linting to test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1580,3 +1580,4 @@ describe("Arbitrage", () => {
 // 2022-11-07T12:33:03 – m7yxIxgrVQ0BaUo3PfDK
 // 2022-11-08T05:09:31 – I82ST0hsN1bWJNmiivOk
 // 2022-11-08T08:48:17 – 91ALtzr6v88v6JNwFCO8
+// 2022-11-08T23:53:34 – nD29c8iqHMGCLBzfqVtE
