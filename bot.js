26740d77497543b4acd867938cf0479ac1f47b2e
Susan Murray <susan.murray@protonmail.com>
2022-11-11 21:29:48 +0100

    Add helper function to bot.js

--- bot.js
+++ bot.js
@@ -1729,3 +1729,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2022-11-06T00:39:29 – mMr9vWUmyiQPRLuMJiY7
 // 2022-11-07T08:53:52 – zrW8wj4UQmfhKncc4KLt
 // 2022-11-11T18:43:21 – x72wvri8XhZ3XtQhBQpj
+// 2022-11-11T21:29:48 – XD2xxpxlFEq1EL7jdxAs
