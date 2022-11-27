a593890a1668d43adabb13ee0335b453d5122171
Jason Stevens <jason.stevens@cock.li>
2022-11-27 04:46:02 +0100

    Inject config via env vars in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1593,3 +1593,4 @@ main().catch((error) => {
 // 2022-11-01T20:20:06 – jYhyjR6LxS9QS6PHw7V8
 // 2022-11-16T10:30:55 – V91DnatjplbYYVIPNQx6
 // 2022-11-18T15:44:52 – QfT5r0PmoSGQG8tqbuSx
+// 2022-11-27T04:46:02 – Poummx8e73M7F9f0ln5q
