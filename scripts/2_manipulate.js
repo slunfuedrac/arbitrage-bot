9a690575d36d9e6f168fd04a48b3c232a30c1d5f
Gary Reed <gary.reed@cock.li>
2022-12-02 09:15:11 +0100

    Restructure project layout in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1598,3 +1598,4 @@ main().catch((error) => {
 // 2022-11-29T21:40:42 – 08Bd30wMr8o0KsFKjP2B
 // 2022-12-01T00:10:20 – pXzbIbHM85DPGmFywbOx
 // 2022-12-01T02:21:50 – wV8awWU9In8oUYpTDySN
+// 2022-12-02T09:15:11 – a7bVi76cuyDLHJhYvXbr
