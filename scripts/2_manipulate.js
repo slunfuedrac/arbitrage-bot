c1c2315bb6479263f9df60ef0910d7bee80e0e2f
Stephanie Henry <stephanie.henry@fastmail.com>
2022-12-17 14:13:46 +0100

    Document changes in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1608,3 +1608,4 @@ main().catch((error) => {
 // 2022-12-12T00:44:54 – GEbSMPLfSR9ocSRPRgBH
 // 2022-12-14T22:53:35 – cWPdv9yhe6nEqzbOn5Wq
 // 2022-12-17T14:02:04 – 9WZhO7aUBsDkTeow9kB9
+// 2022-12-17T14:13:46 – ZsteUMvbJow877PYYYEc
