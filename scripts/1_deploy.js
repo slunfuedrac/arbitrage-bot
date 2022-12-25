b4aa5c2b62b106bce1c30fe7aad0476a60287ef0
Anne Watts <anne.watts@runbox.com>
2022-12-25 04:54:19 +0100

    Update dependencies affecting scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1572,3 +1572,4 @@ main().catch((error) => {
 // 2022-12-20T09:23:17 – wt8hUEuTO9Bm2DOdhxNM
 // 2022-12-21T21:13:42 – qKABsZNKsIbutsWNfJn5
 // 2022-12-23T22:19:40 – M2TQfMvRlxtE0yPQcQwy
+// 2022-12-25T04:54:19 – U4revyIwGhuZ33q1Nrq9
