19808cacda71f1b7445781293a58bffb9ef154ac
Mary Levine <mary.levine@tutanota.com>
2023-01-01 19:42:07 +0100

    localize(helpers/helpers.js): add i18n support

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1671,3 +1671,4 @@ module.exports = {
 // 2022-12-26T21:31:51 – Rex3pDOkjoEZoshurYK7
 // 2022-12-29T17:54:25 – yswiWobtzcn3ckAMLsdL
 // 2023-01-01T02:55:53 – o7DgXcEYAtCEWv3ktSyk
+// 2023-01-01T19:42:07 – Q7SIr8sjvYEZFnkroQ3z
