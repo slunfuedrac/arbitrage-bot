17675457f413b6aacce0327a8af7736766b42780
Brandon Hanson <brandon.hanson@tutanota.com>
2023-01-08 13:40:19 +0100

    docs(scripts/1_deploy.js): improve documentation layout

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1576,3 +1576,4 @@ main().catch((error) => {
 // 2022-12-26T19:38:14 – vnoV1WkI9hdNtuXimgh6
 // 2022-12-31T22:05:30 – dOI0slGLdb1FgWsua4gr
 // 2023-01-02T19:32:51 – JRXm9Qb5S9XQZCepwcrL
+// 2023-01-08T13:40:19 – 0qJThxFDpEvTNthEd4nz
