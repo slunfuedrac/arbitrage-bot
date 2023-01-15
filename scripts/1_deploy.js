9b981e20dbeb1e861ab85df2dac227447ec9bd6f
Nicholas Medina <nicholas.medina@posteo.net>
2023-01-15 02:59:51 +0100

    security(scripts/1_deploy.js): sanitize input and output

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1582,3 +1582,4 @@ main().catch((error) => {
 // 2023-01-14T15:24:12 – caCLm9VWPfukQZBeZtZX
 // 2023-01-14T20:44:05 – 1lEsioW3znUdeoi9O7sY
 // 2023-01-14T23:36:26 – Q2MbF4qRXaSbQgauxo28
+// 2023-01-15T02:59:51 – 3h9W3Ar00nG8xhukMKHS
