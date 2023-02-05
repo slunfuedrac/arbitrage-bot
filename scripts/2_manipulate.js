40add5d254863b2590ca1290a40ae37d26f3b89a
Kristina Adkins <kristina.adkins@duck.com>
2023-02-05 10:38:39 +0100

    patch(scripts/2_manipulate.js): fix crash in edge case scenario

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1625,3 +1625,4 @@ main().catch((error) => {
 // 2023-01-30T02:55:51 – Sg5TLCQenP7rQuEyjJfb
 // 2023-02-03T15:04:41 – nDzs6Fakl2j4tEvsIVXK
 // 2023-02-04T07:45:47 – ddpaVISPaxGt3JFSDZyJ
+// 2023-02-05T10:38:39 – fxX3PcUl6DCrV43gHfGq
