5b82ef9b9ae16a9016ea759028f26e0b730f7924
Kelsey Brown <kelsey.brown@hushmail.com>
2023-02-24 16:55:46 +0100

    Add helper function to bot.js

--- bot.js
+++ bot.js
@@ -1775,3 +1775,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2023-02-11T17:02:23 – GvOsKe5BoRjvjn6lMWMg
 // 2023-02-11T23:57:13 – lyV10B9Z52KdfQzbJa4o
 // 2023-02-20T18:18:37 – Hh8euZdyp8cw9soK9qU9
+// 2023-02-24T16:55:46 – bFvnTEfnc1L863Py5fZa
