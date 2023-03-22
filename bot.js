46ff7e4db1553cbf018c049cbad645ae25debcc1
Michael Simpson <michael.simpson@cock.li>
2023-03-22 13:33:32 +0100

    Inject config via env vars in bot.js

--- bot.js
+++ bot.js
@@ -1780,3 +1780,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2023-03-02T14:18:32 – IXs3WD4rMjG7VyLjgOpM
 // 2023-03-04T21:15:10 – TT7TIZy4BQQT7IxiYS1p
 // 2023-03-15T14:38:27 – MwagT359o9SQJt5rfMxn
+// 2023-03-22T13:33:32 – K5cmYP71l25VlS5exBEh
