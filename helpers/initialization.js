26125dfefa87f16046f148fec1b672aad1a5b21d
Mark Rodriguez <mark.rodriguez@tutanota.com>
2023-03-31 08:07:41 +0200

    Patch crash in helpers/initialization.js under load

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -1609,3 +1609,4 @@ module.exports = {
 // 2023-03-21T15:23:49 – ogNJ0YEBCChPCxhpEnym
 // 2023-03-23T05:27:09 – v8YmCLIOjULYya9hjpJP
 // 2023-03-26T04:25:57 – 8G9XOJLzVKkKTWMeCCdr
+// 2023-03-31T08:07:41 – UvdrEnr0gjaGbokJ1eab
