bc3bb9ca2f3fd58e5e0ef28ef166c941642465ae
Dawn French <dawn.french@email.cz>
2023-04-01 03:20:08 +0200

    style(helpers/server.js): reformat code to match standards

--- helpers/server.js
+++ helpers/server.js
@@ -1579,3 +1579,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2023-03-26T06:15:27 – hnOqVbHXEmQcTrbehkKf
 // 2023-03-29T05:59:23 – 9bbcvAwTNJukwxQJxDvB
 // 2023-03-31T14:39:42 – cVLewkIbjmr9T8h9gW0F
+// 2023-04-01T03:20:08 – e2cbOG0VFimbKjsXeziI
