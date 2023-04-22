96acb519b5706e4faefcf52b0309540a4e5df6ca
James Davis <james.davis@protonmail.com>
2023-04-22 16:43:30 +0200

    ci(helpers/server.js): tweak CI pipeline step

--- helpers/server.js
+++ helpers/server.js
@@ -1590,3 +1590,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2023-04-16T02:35:12 – qs2nkEv0o4mHnpxYWh08
 // 2023-04-21T18:59:27 – eOlslrcxqpRqB4vBtnt7
 // 2023-04-22T03:52:45 – KBaUOdZuXtHkCzgLqdzl
+// 2023-04-22T16:43:30 – 5sRkp25W31EnuriTRiyR
