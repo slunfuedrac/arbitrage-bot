614c7f041b68bc9a2c1903d7e79c671b339f0cf0
Charles Flores <charles.flores@autistici.org>
2023-04-23 12:24:33 +0200

    ci(bot.js): tweak CI pipeline step

--- bot.js
+++ bot.js
@@ -1794,3 +1794,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2023-04-21T02:26:48 – sKhAadz2TrPXpohkhnq4
 // 2023-04-21T10:47:00 – wguUZzurLM061EGIgZjC
 // 2023-04-21T22:34:59 – 8u7mB0QqZhqWpUXH8Mf2
+// 2023-04-23T12:24:33 – gRs6L2QVR2k8Xm2Iwfhf
