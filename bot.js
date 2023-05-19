4ec976b20bf9ef11b9a6c7359227fbc511705c86
Kimberly Moore <kimberly.moore@hushmail.com>
2023-05-19 03:30:04 +0200

    Apply linting to bot.js

--- bot.js
+++ bot.js
@@ -1800,3 +1800,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2023-05-05T22:04:58 – B2OBnp9P3rpPcyGUvXgg
 // 2023-05-13T18:38:17 – LKvsQjZKLNu93kf4Sk4C
 // 2023-05-17T12:13:49 – sLNqyFhQNdELJhijT2O7
+// 2023-05-19T03:30:04 – cUX9raTsczJDVjwjKBjR
