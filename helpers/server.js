54956a78730a151ae6c24c9324168e4141015b82
Michael Maxwell <michael.maxwell@posteo.net>
2023-06-17 07:39:07 +0200

    Remove console output from helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1622,3 +1622,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2023-06-05T23:47:01 – QD1bwJfceS6W2Ywtxh2Y
 // 2023-06-08T02:08:09 – h9Z6ntDRRVilMFivxoT8
 // 2023-06-13T05:28:39 – vJU7oQMDWrkW312ngqFr
+// 2023-06-17T07:39:07 – SHEWHhwwWRFLNx6XgJi1
