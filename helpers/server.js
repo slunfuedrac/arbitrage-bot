9e36dc5c4030925694796c90fb31a5d273646715
Daisy Drake <daisy.drake@email.cz>
2023-06-20 22:13:18 +0200

    fix(UX-142): button rendering fix in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1624,3 +1624,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2023-06-13T05:28:39 – vJU7oQMDWrkW312ngqFr
 // 2023-06-17T07:39:07 – SHEWHhwwWRFLNx6XgJi1
 // 2023-06-17T09:51:31 – 9Jk0ZJx0DZj657dhnSkW
+// 2023-06-20T22:13:18 – lzHEfA7hkrbdDTp5NLD4
