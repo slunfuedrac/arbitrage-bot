e670f1cbb6f1457f38e179791508d7d9a6dfc2a9
Jesus Ortiz <jesus.ortiz@protonmail.com>
2023-06-28 19:00:47 +0200

    Resolve merge conflict in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1672,3 +1672,4 @@ main().catch((error) => {
 // 2023-06-09T22:25:35 – mYQk7IEH9gTo7oTLr0vm
 // 2023-06-17T09:33:47 – dROmRusOQBNIGkbL9TAs
 // 2023-06-28T16:24:59 – hWH6z6KCtiBtGF5yRVex
+// 2023-06-28T19:00:47 – OjXOh1jiUGfJR1bAaOah
