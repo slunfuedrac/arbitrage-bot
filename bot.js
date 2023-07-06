528b0554bc173f3eaa8059afe777360be582c522
Robert Smith <robert.smith@gmail.com>
2023-07-06 09:56:26 +0200

    Apply security patch to bot.js

--- bot.js
+++ bot.js
@@ -1811,3 +1811,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2023-06-17T23:05:39 – CYGIYZ0FDj6wfSzqRLeq
 // 2023-06-21T20:29:45 – QZKAlwXUv2GgLF06DSS1
 // 2023-06-23T10:49:54 – oQnCDrLWATyhvH9gtNQX
+// 2023-07-06T09:56:26 – lFnoTbRAhCWBknv5YcoH
