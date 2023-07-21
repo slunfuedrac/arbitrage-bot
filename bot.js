8aae2a2d2f441b995a28ca239cb842bc6448c9f8
David Moss <david.moss@protonmail.ch>
2023-07-21 12:44:27 +0200

    Update dependencies affecting bot.js

--- bot.js
+++ bot.js
@@ -1819,3 +1819,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2023-07-15T20:49:56 – Ob7L9WLucL1SCtSWuexM
 // 2023-07-16T05:47:55 – 9NBShcJjChCNTfCks1qJ
 // 2023-07-16T13:39:51 – rjp6bVDrrS7aFnEDxnmr
+// 2023-07-21T12:44:27 – SglGLfaueTJCHCFHj5pL
