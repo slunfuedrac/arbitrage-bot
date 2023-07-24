07944d1a843f615d1a46375a61d53f519f959ce7
Adam Rice <adam.rice@hey.com>
2023-07-24 19:32:17 +0200

    Improve readability of test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1693,3 +1693,4 @@ describe("Arbitrage", () => {
 // 2023-07-21T03:02:14 – Uxcz5z26tICrPznPyatY
 // 2023-07-21T05:03:02 – YUPJroriRkCKIXOTDqdT
 // 2023-07-21T19:14:08 – razqdqpv29YnGFzd1S5T
+// 2023-07-24T19:32:17 – zJhNR5UCb7lc3CnA51uh
