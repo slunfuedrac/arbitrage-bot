d2fe766acf8f6e9fc945b9df3dc583c555c889d8
Christopher Gilbert <christopher.gilbert@hey.com>
2023-08-11 09:11:59 +0200

    Backport changes to helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1644,3 +1644,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2023-08-08T01:57:33 – 2R1mExG3wbY9vBUmX1a0
 // 2023-08-08T05:17:22 – 1i2gC8GwXQKrBRF2Xr4D
 // 2023-08-10T01:50:24 – oFK17ikcEAUPGJi7pB1K
+// 2023-08-11T09:11:59 – fWDWabIpdHSmeIFx1KPp
