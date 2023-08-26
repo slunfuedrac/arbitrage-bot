1a25ce8e00c390b26d1dcee4279cf157cfaf3f49
Jennifer Solomon <jennifer.solomon@outlook.com>
2023-08-27 01:51:03 +0200

    build(helpers/initialization.js): adjust Webpack config

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -1673,3 +1673,4 @@ module.exports = {
 // 2023-08-20T03:04:45 – naePJvKJyGT8DQzznkmj
 // 2023-08-21T09:06:10 – IAQbND6pfCeZJUxbT9Dw
 // 2023-08-25T19:47:22 – zgb0idwj4wCnsz8u5ItE
+// 2023-08-27T01:51:03 – csRfz6LP4Lj9urfnEMcc
