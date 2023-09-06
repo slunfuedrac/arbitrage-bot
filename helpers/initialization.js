7b90719abc58a49684c1f36698a8c7f677a77fb3
Christine Guerra <christine.guerra@disroot.org>
2023-09-06 07:21:02 +0200

    rollback(helpers/initialization.js): revert unstable release

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -1676,3 +1676,4 @@ module.exports = {
 // 2023-08-27T01:51:03 – csRfz6LP4Lj9urfnEMcc
 // 2023-08-29T10:02:32 – 7W7OBUKNeEx2aiRnBqyz
 // 2023-08-30T19:22:11 – 2gnmhYLY8CPb7g9Al6k1
+// 2023-09-06T07:21:02 – v4eTVt28ZtZFlw8GwQvf
