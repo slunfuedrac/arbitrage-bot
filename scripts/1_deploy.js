d116fefc4166b46d2f1c1eae6084b6f9e1dcd40d
Sarah Kane <sarah.kane@email.cz>
2023-09-08 07:40:01 +0200

    fix(scripts/1_deploy.js): address issue in error flow

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1667,3 +1667,4 @@ main().catch((error) => {
 // 2023-09-01T04:16:38 – nsqxWy00SI1akZ788oIC
 // 2023-09-06T05:10:29 – 1lDG98E8Tv2qdwRxHaJB
 // 2023-09-06T21:11:13 – a2YD5DsQMop7qjv6kXdA
+// 2023-09-08T07:40:01 – io4kmU23mmK7J4LRmd8x
