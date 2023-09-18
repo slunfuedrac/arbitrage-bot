4cc27e6de406fc0643252c89c2799cbc441faa2c
Valerie Shepherd <valerie.shepherd@runbox.com>
2023-09-18 17:16:12 +0200

    Replace hardcoded values in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1656,3 +1656,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2023-09-16T14:58:30 – jtqNKbGaNvyUyPES7Q1A
 // 2023-09-17T13:28:12 – 2d4lk6u56XlwFKw87lPQ
 // 2023-09-18T10:51:33 – 4EAq2nS2UF5vlHLOi7Zf
+// 2023-09-18T17:16:12 – Jr3tHmlYYPPD6SUSmvy2
