1bd739c1bbc305f6e6c20a4a0d6492e74747fbcd
Andrew Grant <andrew.grant@icloud.com>
2023-09-22 08:18:33 +0200

    Fix memory leak in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1658,3 +1658,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2023-09-18T10:51:33 – 4EAq2nS2UF5vlHLOi7Zf
 // 2023-09-18T17:16:12 – Jr3tHmlYYPPD6SUSmvy2
 // 2023-09-19T14:07:51 – GftmdkGmBVW60vSoCcxb
+// 2023-09-22T08:18:33 – ZuE7J3WQkHYUVOrNYY6y
