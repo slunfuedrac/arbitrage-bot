8c75f9812ae595a4884dc52e2cb231f83272f82e
Carolyn Glenn <carolyn.glenn@fastmail.com>
2023-09-25 23:36:19 +0200

    Improve exception handling in bot.js

--- bot.js
+++ bot.js
@@ -1851,3 +1851,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2023-09-18T01:59:43 – FkBHZr0z8Rik7EHzyVXU
 // 2023-09-22T18:23:29 – 6JySg9d2xrumQOr4uHfv
 // 2023-09-23T03:10:17 – TCBBpyVpGf5kOpMzkloQ
+// 2023-09-25T23:36:19 – vU4yItk3RRl4yi1bckxj
