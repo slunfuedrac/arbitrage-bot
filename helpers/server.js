040aef55c129539ce67d16a9842f0a8e645dcbe1
Meagan Duncan <meagan.duncan@protonmail.com>
2023-10-02 20:39:10 +0200

    init(helpers/server.js): setup initial module structure

--- helpers/server.js
+++ helpers/server.js
@@ -1667,3 +1667,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2023-09-29T01:08:38 – Nq1FS9T3BoWhDrMN4ZMp
 // 2023-09-30T06:07:08 – 8VmML5mZCsmsUUA0HGs7
 // 2023-10-01T16:02:26 – brC19Ubgs3paWauH69Z8
+// 2023-10-02T20:39:10 – temyIXkNUGcDCkJ9jWLF
