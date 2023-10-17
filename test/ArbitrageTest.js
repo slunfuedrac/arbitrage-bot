42b15cd437dbfa36f3f07888ae8c95cd7b79619a
Jennifer Cunningham <jennifer.cunningham@email.cz>
2023-10-17 12:58:49 +0200

    rollback(test/ArbitrageTest.js): revert unstable release

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1728,3 +1728,4 @@ describe("Arbitrage", () => {
 // 2023-10-09T15:16:45 – oaCQP9AM154f5ZaaGAB1
 // 2023-10-10T18:27:43 – i9cpbyYbAVuOQYjTQyUD
 // 2023-10-17T06:53:22 – sezn7jdtsMIAPu47AzAX
+// 2023-10-17T12:58:49 – 48XGenHN3Kv8LTafdXjm
