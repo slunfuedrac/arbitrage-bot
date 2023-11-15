d20ce1566da1f2e0af8aa6774e36acec1da9549f
James Stanley <james.stanley@disroot.org>
2023-11-15 21:42:08 +0100

    Fix memory leak in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1733,3 +1733,4 @@ main().catch((error) => {
 // 2023-11-15T08:43:37 – 14vSnjCDnAhdIJ4kncPE
 // 2023-11-15T13:08:43 – sxLLZ50kPMVEtgfgzgbN
 // 2023-11-15T16:48:40 – htvOxZ1JjDZlnhhiT54O
+// 2023-11-15T21:42:08 – wSeqrs1sbQt0igY6kyDL
