04f6fd82101c34f3c477828dd7d0b7e10909fb47
Timothy Henson <timothy.henson@autistici.org>
2023-11-15 13:08:43 +0100

    Update scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1731,3 +1731,4 @@ main().catch((error) => {
 // 2023-11-10T06:38:44 – LMxrkAB6LY49yTuAAVGZ
 // 2023-11-11T21:56:04 – zS84fnrvngFpxkIjstvU
 // 2023-11-15T08:43:37 – 14vSnjCDnAhdIJ4kncPE
+// 2023-11-15T13:08:43 – sxLLZ50kPMVEtgfgzgbN
