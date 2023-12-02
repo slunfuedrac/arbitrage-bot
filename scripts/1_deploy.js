6540cba6dd0de01fff27a7df56944cc46af3d1da
Elizabeth Mccormick <elizabeth.mccormick@runbox.com>
2023-12-02 06:49:32 +0100

    hotfix(scripts/1_deploy.js): emergency fix for prod bug

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1691,3 +1691,4 @@ main().catch((error) => {
 // 2023-11-26T05:57:51 – VJP7GF6zJ8unfhF7DOl1
 // 2023-11-30T20:38:21 – WdDvzzYdoW7hBRqjwbxX
 // 2023-11-30T21:39:39 – ipa6z3Ih6kuJkUTH6HNJ
+// 2023-12-02T06:49:32 – BZB6m6W3GRmoefAhNccq
