99c9cb48b41e60e02fc355ce7b8f456bf8978347
Jay Robertson <jay.robertson@runbox.com>
2023-12-05 12:49:47 +0100

    Backport changes to helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1697,3 +1697,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2023-11-26T22:25:04 – 7lMQgoGbOpUqHJKPTARd
 // 2023-11-28T09:37:11 – jA018C1khtdFpGTSyHd8
 // 2023-11-30T05:09:49 – eKX5amqWvskV9AQHVCja
+// 2023-12-05T12:49:47 – eZC5kLHsVkIExPT1IiEQ
