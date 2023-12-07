4b36a724b211d2f141f56987f0347b3ea59bc331
Stephanie Nelson <stephanie.nelson@posteo.net>
2023-12-07 01:50:16 +0100

    Simplify logic in bot.js

--- bot.js
+++ bot.js
@@ -1881,3 +1881,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2023-12-04T13:07:35 – NSseFjg5FuMV7ioqK0uN
 // 2023-12-05T09:43:50 – x0H5dOQCalRvVvUEcHbJ
 // 2023-12-06T08:33:35 – YY4qMZfW0NJNUZxULvVU
+// 2023-12-07T01:50:16 – q9uY99U26f5loNvwOZId
