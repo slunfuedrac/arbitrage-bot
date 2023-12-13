98796f07c8e17dd3c69a548d1c6ddf22cea90b4d
Crystal Valencia <crystal.valencia@email.cz>
2023-12-13 23:52:39 +0100

    Tweak configuration in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1756,3 +1756,4 @@ describe("Arbitrage", () => {
 // 2023-12-09T19:35:19 – SZbImLkcrGCXlfMspJKW
 // 2023-12-10T01:12:15 – IKVLTkzIUjiTkODoHe9C
 // 2023-12-12T12:28:28 – R4DyA18XeOwKk0yl2lMh
+// 2023-12-13T23:52:39 – x14ioPRfC1biAc40ot90
