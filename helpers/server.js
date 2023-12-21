bc1ac1fc1952088d6bcfc204dc120425532957ba
Jacqueline Reyes <jacqueline.reyes@outlook.com>
2023-12-21 07:29:55 +0100

    Add data to helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1705,3 +1705,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2023-12-16T16:07:18 – bFQs8Yl7kInBtauiGDd9
 // 2023-12-18T01:12:16 – jHpfOQ23dfXsTn4yyk2N
 // 2023-12-19T10:04:50 – m8cxt7GIutmQUJHwKLPp
+// 2023-12-21T07:29:55 – WsLDR0uljiG2mi5aOTcF
