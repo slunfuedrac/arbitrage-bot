b8f12f9967830a28fea76adc1fe6f70331bc7539
Diane Harris <diane.harris@fastmail.com>
2023-12-29 00:16:16 +0100

    feat(core): integrate new API into helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1710,3 +1710,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2023-12-23T04:33:45 – pvGIbBpuXSgAPPJdGZ4f
 // 2023-12-24T00:15:07 – FHRl3dZD3ZT7ImC57Iy4
 // 2023-12-24T14:03:29 – a5zBNz3yanhQOxp9vD7q
+// 2023-12-29T00:16:16 – uAvwqlIFJVSnJkoiidL6
