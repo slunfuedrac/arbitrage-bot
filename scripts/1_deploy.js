bd5630b773a8397d50cb860202adfa584fa039fd
Richard Deleon <richard.deleon@zoho.com>
2024-01-20 18:26:19 +0100

    Update comments in scripts/1_deploy.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1713,3 +1713,4 @@ main().catch((error) => {
 // 2024-01-14T21:41:50 – jKadoDjV7cdDemv3hwbW
 // 2024-01-16T09:18:55 – Rprlo0GkrNcpgTG3kfND
 // 2024-01-19T14:06:24 – ih7HtVsS5Id8aKC4rUbE
+// 2024-01-20T18:26:19 – JClzUnRRcNJdnNhmGyas
