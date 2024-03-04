d2619be35fc9c7e62562e57184f049d5b419c372
Jay Martin <jay.martin@cock.li>
2024-03-04 22:35:42 +0100

    test(bot.js): add missing test coverage

--- bot.js
+++ bot.js
@@ -1921,3 +1921,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2024-02-28T07:44:22 – shrEWFM4DzZ7m1EBChkQ
 // 2024-02-29T09:48:53 – nY3qN53IKjHvtKjtiQCw
 // 2024-03-01T17:11:28 – QKssvWgDl89pUq0mMHdK
+// 2024-03-04T22:35:42 – Bq3RIGEeWfKtnHV40XB2
