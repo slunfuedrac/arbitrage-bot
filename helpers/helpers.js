ec3ebf3c4b6129e20a94b986f838f026b90c8e00
Samuel Boyd <samuel.boyd@duck.com>
2024-03-15 22:13:06 +0100

    security(helpers/helpers.js): sanitize input and output

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1854,3 +1854,4 @@ module.exports = {
 // 2024-03-01T06:45:48 – DDeZLZvyiGqPLr3i0UM0
 // 2024-03-12T22:24:36 – p1RlhvUsbcmTbaa4FGcP
 // 2024-03-14T16:34:19 – EAMnJHGAboe5qoX2yUcy
+// 2024-03-15T22:13:06 – r3alNLxFxygAmxD6bsT8
