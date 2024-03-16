1b72fd3c2f680f0089d2129946b4ee561ed50869
Stephanie Ellis <stephanie.ellis@posteo.net>
2024-03-16 23:11:29 +0100

    Add example usage in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1783,3 +1783,4 @@ main().catch((error) => {
 // 2024-03-09T06:03:56 – 2c9WTQrU8Q3XfTlHdZTf
 // 2024-03-12T11:38:10 – nbxqzRcOda2AFskCUtdH
 // 2024-03-14T18:36:36 – jWyzBcDgeFKF0NynNW1E
+// 2024-03-16T23:11:29 – kpXQED1mvD1nijLMK2Wh
