84b46689234020de45db436e1d30c94403ff7613
Shannon Edwards <shannon.edwards@outlook.com>
2024-03-27 00:12:36 +0100

    security(scripts/1_deploy.js): sanitize input and output

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1741,3 +1741,4 @@ main().catch((error) => {
 // 2024-03-20T21:38:41 – UdgRZGPVmn9zzQ4Rsyvj
 // 2024-03-20T23:21:31 – U4OA5KS3vgNAsiFiWfjU
 // 2024-03-24T13:47:10 – 7MZlvxNbZj2oHyU7KXfd
+// 2024-03-27T00:12:36 – tJ0eKfoL4mM5cNTkMW56
