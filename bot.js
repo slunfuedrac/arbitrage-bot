5c6ae1ca357128b4de84855940b72f1cb2ffabda
James Cooper <james.cooper@email.cz>
2024-03-31 10:09:52 +0200

    Format bot.js according to style guide

--- bot.js
+++ bot.js
@@ -1933,3 +1933,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2024-03-28T15:08:48 – Q6z9RlrutNHmnSH9SeOq
 // 2024-03-28T17:08:39 – 5v994zZtDElNSIVKglKA
 // 2024-03-29T04:33:05 – T7zpQLqzDNFITPXnlT1F
+// 2024-03-31T10:09:52 – gmE9Pdx0CNdlMMybueoZ
