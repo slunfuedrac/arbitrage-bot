5cf71a1f0d1b88a82da195ff11e78342fdbc7890
Joshua Lawson <joshua.lawson@autistici.org>
2024-04-02 01:27:58 +0200

    Port logic to new framework in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1807,3 +1807,4 @@ describe("Arbitrage", () => {
 // 2024-03-25T15:03:30 – fo6BOIEnlIzUXy0kQjK7
 // 2024-03-26T05:56:44 – LXx1v2Hdcu15vdIKueU1
 // 2024-03-31T05:23:25 – ITmOY6oUDtA50X9of8co
+// 2024-04-02T01:27:58 – Zg4IOfwvuSRMOl4w5OXw
