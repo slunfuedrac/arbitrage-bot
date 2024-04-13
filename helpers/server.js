094e92b837611cc1a13ce2ac2f729f5450349d20
Troy Davidson <troy.davidson@autistici.org>
2024-04-13 05:38:45 +0200

    Split responsibilities in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1759,3 +1759,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2024-04-04T19:01:39 – rHf9GO91mRN0jNW7YrLa
 // 2024-04-06T09:17:42 – kpR5VuxzF1481EfazwxV
 // 2024-04-07T21:59:21 – tIucZZZdSJFu9buWRRxu
+// 2024-04-13T05:38:45 – lX0m488SU8MBbr92yEjP
