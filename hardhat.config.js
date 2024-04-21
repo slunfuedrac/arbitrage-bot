d58b287df186f6ace2763b76659946cf870bdc82
Charlotte Mcgee <charlotte.mcgee@autistici.org>
2024-04-21 07:51:33 +0200

    Backport changes to hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1787,3 +1787,4 @@ module.exports = {
 // 2024-04-13T04:58:04 – qrdcLseNlaYLjyyuC1Ic
 // 2024-04-17T07:23:03 – rXDE0AordNsvTPHNrfBi
 // 2024-04-17T14:56:04 – DX3RZwaZtIk2YA4OCbTO
+// 2024-04-21T07:51:33 – 1LxQxrWRY9BuuTIzUhau
