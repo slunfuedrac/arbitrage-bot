199ace38e753e561f212dbdcdeec5eb255afb27f
Jeffrey Wolf <jeffrey.wolf@hushmail.com>
2024-04-28 14:13:24 +0200

    build(bot.js): adjust Webpack config

--- bot.js
+++ bot.js
@@ -1943,3 +1943,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2024-04-14T11:42:41 – KcdEtHLAp8oF1y1T6rfo
 // 2024-04-15T05:59:46 – ptUCl7tR52Y1D1kKJqw6
 // 2024-04-25T11:04:21 – nlsUNwHGKDOck8uBCklJ
+// 2024-04-28T14:13:24 – n44vADIxNeTqm6vV35AE
