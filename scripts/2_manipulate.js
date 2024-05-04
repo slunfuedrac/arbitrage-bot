35baed77a41807199443a302a288e4f9eb9d713a
Robert Johnson <robert.johnson@fastmail.com>
2024-05-04 12:56:25 +0200

    Downgrade dependency used in scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1802,3 +1802,4 @@ main().catch((error) => {
 // 2024-04-22T11:57:12 – Sq4CCRnQUZu0tkMBa53L
 // 2024-04-22T23:36:29 – rygqKxUkMg2Bdsqyhjca
 // 2024-04-30T21:14:43 – fu910aBUuHpky2EKqzlJ
+// 2024-05-04T12:56:25 – zbNwzC9ZzYeYxUh6fZ6l
