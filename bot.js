11929b901356f6aa8b69fc78d90a8cc78734b677
Michael Owen <michael.owen@hey.com>
2024-05-09 09:26:24 +0200

    Simplify logic in bot.js

--- bot.js
+++ bot.js
@@ -1950,3 +1950,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2024-05-06T03:58:35 – J9S4L8LPG6Jee4JWINyY
 // 2024-05-07T05:19:12 – 7Hh6abzTv4t9ND5SJdJi
 // 2024-05-08T15:43:00 – EkmXK8yDfDQSIxTJ2yqk
+// 2024-05-09T09:26:24 – OySnc3m7aXu0iWfPotcn
