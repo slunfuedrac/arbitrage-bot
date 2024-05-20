58fbf14095ea1f69c33aa580debcf1e9f39afd42
Jordan Thornton <jordan.thornton@cock.li>
2024-05-20 13:01:28 +0200

    Backport changes to scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1809,3 +1809,4 @@ main().catch((error) => {
 // 2024-05-14T19:46:47 – wmQPBfiUfk9jjoCOledy
 // 2024-05-15T16:56:04 – bkjawCAp6GvXkYa7H1rm
 // 2024-05-17T05:50:43 – jVGYLyu1O8moTSoJoJEL
+// 2024-05-20T13:01:28 – 0YswaBjd77QhvxS5BQFR
