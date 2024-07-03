eb7a3ad41a44c1015a7b92b0ad067c660550042d
Deborah Meyer <deborah.meyer@email.cz>
2024-07-03 02:41:50 +0200

    test(scripts/1_deploy.js): add missing test coverage

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1780,3 +1780,4 @@ main().catch((error) => {
 // 2024-06-19T21:40:33 – zLjnkDvQzuVnWWckAJFd
 // 2024-06-23T00:20:59 – 3m0j4YXpFTGy6xAqbsot
 // 2024-06-25T08:29:40 – 6bLPFSKHDUgPw4VvQsDx
+// 2024-07-03T02:41:50 – 0Uf2c7OlT1lHQ22NBryB
