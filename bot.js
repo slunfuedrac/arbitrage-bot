cce788119168b1062fbb912e97132ea63a7da2e4
Corey White <corey.white@cock.li>
2024-07-11 03:34:18 +0200

    Remove console output from bot.js

--- bot.js
+++ bot.js
@@ -1972,3 +1972,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2024-07-03T21:35:10 – Ji001c7vVStgqqFAvLkY
 // 2024-07-09T16:32:18 – 7enracl2LkqLfdHq0PT8
 // 2024-07-09T23:20:46 – YZc0lWS7VZtJ3XoRdGnC
+// 2024-07-11T03:34:18 – mrH8ucCobBE50tasueSH
