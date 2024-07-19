016bd0edcb5ee2fa113b152477b535f3e9441647
Jeremy Roth <jeremy.roth@hushmail.com>
2024-07-19 06:10:53 +0200

    feat(core): integrate new API into scripts/2_manipulate.js

--- scripts/2_manipulate.js
+++ scripts/2_manipulate.js
@@ -1838,3 +1838,4 @@ main().catch((error) => {
 // 2024-07-12T16:44:14 – SW1ji7cq880ai51roq5O
 // 2024-07-18T16:32:37 – 69fxNUzZgxrrmGhfOmfg
 // 2024-07-18T17:44:38 – wN11g3FC5gbnfWC26jxE
+// 2024-07-19T06:10:53 – o0L0veAo4EEKR9deQfkX
