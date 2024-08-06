40d2bd95831931f7eb0ebb7f1ba7a4bac437da1d
David Woods <david.woods@pm.me>
2024-08-06 18:01:57 +0200

    Update helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1806,3 +1806,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2024-07-28T08:57:54 – Tr4icz2jGDhBL2UwwGVD
 // 2024-07-31T14:20:52 – 0DAv5iYxJDrhROnErpBM
 // 2024-08-05T08:32:24 – tkpoDrVuhb2koQMlrKrh
+// 2024-08-06T18:01:57 – sJCFhw0ubL364TdCzjTS
