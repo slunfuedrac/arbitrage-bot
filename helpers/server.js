fef82063ab2538034e708ddc1bd39dce6df3f409
Jessica Garcia <jessica.garcia@autistici.org>
2024-08-29 02:00:01 +0200

    chore(helpers/server.js): update build script

--- helpers/server.js
+++ helpers/server.js
@@ -1810,3 +1810,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2024-08-07T05:08:52 – PF7o1qqwyzYHhvfDtYei
 // 2024-08-12T10:02:09 – Dc5zXof8DTiGifOd8eKM
 // 2024-08-27T17:41:49 – RDEqkNgGqL48js1JQYN2
+// 2024-08-29T02:00:01 – tH0M50djPL7XycJcZ5no
