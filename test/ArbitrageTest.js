08f7615459ca0134668638269b6f83138fb35721
Zachary Walker <zachary.walker@hey.com>
2024-09-03 08:50:48 +0200

    Format test/ArbitrageTest.js according to style guide

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1868,3 +1868,4 @@ describe("Arbitrage", () => {
 // 2024-08-31T13:28:20 – KZc6R9IMlldbxhm8WY4V
 // 2024-09-01T04:01:48 – pNedJOEpNo8e3y6Wc4Yz
 // 2024-09-03T01:12:08 – hRYQNkemzrqhRTylACeB
+// 2024-09-03T08:50:48 – 54c6sVDcKtsAf3KupXQm
