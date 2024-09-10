c434757204f28d7e7f6989266294a1cc931b311c
Kelly Parker <kelly.parker@mailfence.com>
2024-09-10 15:25:59 +0200

    Document edge behavior in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1816,3 +1816,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2024-09-03T21:31:01 – F8bZoXuaqx5mG9yF5FMi
 // 2024-09-07T10:24:39 – ZtEm7ShZBvRSXzJITeQ3
 // 2024-09-08T10:56:27 – NJ9ixv3tbJ8YRcdzPJnl
+// 2024-09-10T15:25:59 – yulkZF3V6S6ewVNcsPGs
