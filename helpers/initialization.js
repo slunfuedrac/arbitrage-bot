8e93398cc09c5355c74043dcea04cc6da6f97667
Amanda Jones <amanda.jones@icloud.com>
2024-09-12 00:36:04 +0200

    Disable flaky test in helpers/initialization.js

--- helpers/initialization.js
+++ helpers/initialization.js
@@ -1827,3 +1827,4 @@ module.exports = {
 // 2024-09-04T01:24:36 – D8Z9DtTmrdPEPUw1qbt4
 // 2024-09-06T03:10:15 – Il2Mru4xw3tjHuinAkJF
 // 2024-09-09T19:05:55 – W07QopOgunn9xcmX4b40
+// 2024-09-12T00:36:04 – jCIud9HDQLsvTMuEKmd5
