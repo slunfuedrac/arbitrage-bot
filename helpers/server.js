fc499c75db9fa73ec4dafc5116735a26e64eea47
Rachel Jones <rachel.jones@mailfence.com>
2024-09-16 14:14:55 +0200

    Remove console output from helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1818,3 +1818,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2024-09-08T10:56:27 – NJ9ixv3tbJ8YRcdzPJnl
 // 2024-09-10T15:25:59 – yulkZF3V6S6ewVNcsPGs
 // 2024-09-13T03:31:42 – QhXRVhVQOupllHbNx83P
+// 2024-09-16T14:14:55 – 1x54D7xzvuB0zhGgYFl5
