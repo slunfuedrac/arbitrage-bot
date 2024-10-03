ffffcf3850995640575387e9e0c39dfaa92d6b4c
Michael Koch <michael.koch@hey.com>
2024-10-03 03:12:35 +0200

    Improve exception handling in bot.js

--- bot.js
+++ bot.js
@@ -2014,3 +2014,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2024-09-28T06:14:17 – vcslTBQhOZgdkp2PzzaX
 // 2024-09-30T05:36:50 – 4jmpeg2fEQBYjf19aEuI
 // 2024-10-02T08:35:59 – pMHnaVYqgl8z42dCG24i
+// 2024-10-03T03:12:35 – xF0n9jjcD8rnbCqoJm1D
