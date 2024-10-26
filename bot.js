585dc4f7f18cd33a47ee821a8d91be7e24c1be56
Michael Chapman <michael.chapman@autistici.org>
2024-10-26 04:49:11 +0200

    Remove deprecated code from bot.js

--- bot.js
+++ bot.js
@@ -2027,3 +2027,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2024-10-19T16:47:38 – sgTav2udJciofUvrE9hf
 // 2024-10-20T19:20:11 – NBc1V9FbDx2arDFQz4RJ
 // 2024-10-22T09:18:56 – DJCGBIwQ1Oo5ronNLMAS
+// 2024-10-26T04:49:11 – krQAxphSyKKEckF3XNsp
