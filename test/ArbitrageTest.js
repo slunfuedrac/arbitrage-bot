4556ee1958770097458c4f27d833cb5cebdcff09
Kimberly Miller <kimberly.miller@tutanota.com>
2024-12-03 23:25:42 +0100

    Document edge behavior in test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1913,3 +1913,4 @@ describe("Arbitrage", () => {
 // 2024-11-29T11:56:36 – RDEFVhQ17fUexQKrfh2c
 // 2024-12-02T20:57:53 – JiFJIf8A8XNq1OpWtWhm
 // 2024-12-03T06:44:41 – SUdaIcXucttZtC4MUFHi
+// 2024-12-03T23:25:42 – 5TP5jqUCfmWRuVK5zaiE
