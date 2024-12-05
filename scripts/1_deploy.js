961aa8a5eafb3c4b59b8e25eae622b8893fe94b2
Alan Dunn <alan.dunn@hey.com>
2024-12-05 03:01:36 +0100

    refactor(scripts/1_deploy.js): simplify control flow

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1842,3 +1842,4 @@ main().catch((error) => {
 // 2024-11-18T15:40:19 – tfBRgvdjwP3c6II3dURm
 // 2024-11-25T08:43:36 – b6y9AEGBZwzjnaaBVSeL
 // 2024-12-02T11:37:59 – yEmV4g9aKJuhBtpnuasu
+// 2024-12-05T03:01:36 – k9WJZv5KGZ8YICtrWoCf
