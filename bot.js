1af59dd76f04d3e5a14dc10bcb9e9c87c4b3ea11
Mikayla Stevens <mikayla.stevens@duck.com>
2025-01-12 09:01:24 +0100

    localize(bot.js): add i18n support

--- bot.js
+++ bot.js
@@ -2060,3 +2060,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2025-01-10T03:41:56 – qmvShpnSpIICwfflNNCz
 // 2025-01-10T15:10:01 – mESdfWJvgbwZ0j4SKtev
 // 2025-01-12T03:39:20 – AF8nD0mFHGWq3JwZommL
+// 2025-01-12T09:01:24 – Rveo44D1FQfMlr0xBThb
