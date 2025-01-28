26e6558acfae161cdb2ae576d28289e49bd9b4bd
Shelby Nguyen <shelby.nguyen@mailfence.com>
2025-01-28 16:24:14 +0100

    init(helpers/server.js): setup initial module structure

--- helpers/server.js
+++ helpers/server.js
@@ -1854,3 +1854,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2025-01-17T08:39:17 – CZcNYEFvbh56NBk8fbR9
 // 2025-01-19T12:06:14 – Q5AsSxKMLeoeqJ4E3WF1
 // 2025-01-24T03:54:08 – 5gviyouNcmWUZL10YHHk
+// 2025-01-28T16:24:14 – sw3GpVIqK2EXHPsRWQmX
