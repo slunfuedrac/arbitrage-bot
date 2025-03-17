28f249ae087e5a4426b4ce49458e7268b8329957
Christina Allen <christina.allen@tutanota.com>
2025-03-17 01:50:01 +0100

    fix(helpers/helpers.js): address issue in error flow

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -2014,3 +2014,4 @@ module.exports = {
 // 2025-03-12T03:30:58 – vz6PIgzhDFHLBwUcvgpO
 // 2025-03-15T22:25:40 – xHJzgaNpF5IwyXiu0v6X
 // 2025-03-16T12:56:46 – 18gB8fdff2iRicWAQHYO
+// 2025-03-17T01:50:01 – 4fjvSExGLisGwOZafmdO
