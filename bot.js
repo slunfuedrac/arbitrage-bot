4b7d67f9bf88732915b1f56f07e3ca60f4411c9e
Cameron Day <cameron.day@duck.com>
2025-03-21 04:21:30 +0100

    Improve exception handling in bot.js

--- bot.js
+++ bot.js
@@ -2082,3 +2082,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2025-03-04T07:55:20 – Gq2lrunxzUMd54irft5B
 // 2025-03-05T14:55:10 – 4Ih3WbU3bZ50sdVj0QEN
 // 2025-03-08T08:51:39 – Va5cRfa0CMBS8t8rxB14
+// 2025-03-21T04:21:30 – dSst50GlQnvmHeEupF04
