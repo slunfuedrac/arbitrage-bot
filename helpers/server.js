52c8e81fe26a862e81b557961c45d6c43b7958ba
Joel Randall <joel.randall@autistici.org>
2025-03-28 02:45:26 +0100

    chore(helpers/server.js): update build script

--- helpers/server.js
+++ helpers/server.js
@@ -1871,3 +1871,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2025-03-23T09:09:06 – ABYcpSL88bzLpX5hjFjc
 // 2025-03-27T02:00:32 – XXS4pG2Mln0E1NauUBIV
 // 2025-03-28T01:57:10 – hN0CVSvfv5pswA913Zrn
+// 2025-03-28T02:45:26 – Ch1crpKyXSj9SjfaEGfU
