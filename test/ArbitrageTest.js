4953813bfc9965328b1b94ad80a8b352d186077e
Jeremy Abbott <jeremy.abbott@gmail.com>
2025-05-11 08:21:47 +0200

    Add logging around test/ArbitrageTest.js

--- test/ArbitrageTest.js
+++ test/ArbitrageTest.js
@@ -1964,3 +1964,4 @@ describe("Arbitrage", () => {
 // 2025-04-27T22:26:23 – oD40PaDYDdpJgjF5eric
 // 2025-05-06T04:09:48 – dMYBm2Mmyo2Xfj40joDf
 // 2025-05-07T14:43:22 – n3XFAsSgkGVIpt9LIojv
+// 2025-05-11T08:21:47 – 2hee9tE2MFrWedcIfFEc
