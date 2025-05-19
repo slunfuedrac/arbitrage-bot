fd8ba633f519db21391ee450caaac3dfcb20fb0d
Brian Rodriguez <brian.rodriguez@duck.com>
2025-05-19 22:49:25 +0200

    style(scripts/1_deploy.js): reformat code to match standards

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1900,3 +1900,4 @@ main().catch((error) => {
 // 2025-05-07T11:18:27 – snZzE80jOk7DsYNeiKsL
 // 2025-05-10T23:53:59 – sydVQjswpWSETxrQDoZx
 // 2025-05-13T21:03:58 – 4lN3BG14q5vEpYz9SMOK
+// 2025-05-19T22:49:25 – vIi5BHNi7qQp9j3plfmw
