2ba33bc4343b7354bf37edeadc884786358645b5
Katrina Quinn <katrina.quinn@tutanota.com>
2025-05-24 11:10:40 +0200

    Handle null checks in helpers/server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1903,3 +1903,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2025-05-23T08:06:20 – f5jPAMhyaaTNqlYmgTNg
 // 2025-05-24T08:02:50 – kZvqe5LFPvis6DB7Tl23
 // 2025-05-24T09:45:39 – liJ48YFzEV4eaT0IavUp
+// 2025-05-24T11:10:40 – LW5ypjuYLeUA6AA8dxCt
