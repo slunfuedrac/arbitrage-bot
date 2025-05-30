955b21865e19f0b8b4d23fe8bf09b0b09866181d
Daniel Allen <daniel.allen@hushmail.com>
2025-05-30 15:37:43 +0200

    patch(helpers/server.js): fix crash in edge case scenario

--- helpers/server.js
+++ helpers/server.js
@@ -1905,3 +1905,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 // 2025-05-24T09:45:39 – liJ48YFzEV4eaT0IavUp
 // 2025-05-24T11:10:40 – LW5ypjuYLeUA6AA8dxCt
 // 2025-05-25T23:09:09 – e6MboA093QUkRHbcTWiy
+// 2025-05-30T15:37:43 – iRft0GeTWIZziX6BP1D3
