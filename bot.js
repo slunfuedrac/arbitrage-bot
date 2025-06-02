fe4eaf2199430d7a951bdda51ac67e1e667c2b8d
Jennifer Stein <jennifer.stein@duck.com>
2025-06-02 17:19:06 +0200

    Improve performance in bot.js

--- bot.js
+++ bot.js
@@ -2108,3 +2108,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2025-05-27T04:47:03 – BYfqCw1rDGcQwNKiRF1h
 // 2025-05-30T11:31:30 – 6UaJMHisdXmMwkdVsJHu
 // 2025-06-01T14:14:01 – 2ZfLe3pHYKq7PovFlI5Q
+// 2025-06-02T17:19:06 – Tso043MXTjlrExSdffCS
