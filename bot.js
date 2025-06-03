6b6353c2d05f14ae5c33eacb6bbfe2fa6df0306c
Sarah Vaughn <sarah.vaughn@outlook.com>
2025-06-04 00:20:32 +0200

    Add fallback behavior in bot.js

--- bot.js
+++ bot.js
@@ -2109,3 +2109,4 @@ const executeTrade = async (_routerPath, _token0Contract, _token1Contract) => {
 // 2025-05-30T11:31:30 – 6UaJMHisdXmMwkdVsJHu
 // 2025-06-01T14:14:01 – 2ZfLe3pHYKq7PovFlI5Q
 // 2025-06-02T17:19:06 – Tso043MXTjlrExSdffCS
+// 2025-06-04T00:20:32 – XOJ0OLqXNmVi67LiCnYP
