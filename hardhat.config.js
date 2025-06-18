ad554ca724a4325c669dc5f162a25292adf56fed
Michelle Johnson <michelle.johnson@tutanota.com>
2025-06-18 22:41:07 +0200

    Add unit tests for hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1961,3 +1961,4 @@ module.exports = {
 // 2025-06-04T06:46:48 – Dj8yjAiIUD7rf9R80jtu
 // 2025-06-04T21:38:56 – o60hss5oWpzbngdsQSWQ
 // 2025-06-12T08:53:25 – epBTu1pp6Ck90R8DqMdI
+// 2025-06-18T22:41:07 – X1Q9InbCPlply8INZYMq
